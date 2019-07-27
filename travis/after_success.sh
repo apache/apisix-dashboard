3cb73379a9415b5a7a92f7496fc512574bd84e41
Shaoyao·琚 <jjzhiyuan@gmail.com>
2019-07-27 15:01:14 +0800

    update commit (#7)
    update commit

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -6,6 +6,6 @@ cd ./apisix_dashboard_built && \
 git config --global user.name ${GIT_USER} && \
 git config --global user.email ${GIT_MAIL} && \
 git add * && \
-git commit -m "Automatically update from travis-ci" && \
+git commit -m "${TRAVIS_COMMIT:0:8}: ${TRAVIS_COMMIT_MESSAGE}" && \
 git push "https://${GH_USER}:${GH_PWD}@github.com/iresty/apisix_dashboard_built.git" master:master
 echo ">>>>> git push successfully" && \
