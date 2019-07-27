3c34fc68f84fb323cad46df32113e9a1cae69041
juzhiyuan <jjzhiyuan@gmail.com>
2019-07-27 14:45:45 +0800

    update

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -6,6 +6,6 @@ cd ./apisix_dashboard_built && \
 git config --global user.name ${GIT_USER} && \
 git config --global user.email ${GIT_MAIL} && \
 git add * && \
-git commit -m "${TRAVIS_COMMIT} ${TRAVIS_COMMIT_MESSAGE}" && \
+git commit -m "${TRAVIS_COMMIT_MESSAGE}" && \
 git push "https://${GH_USER}:${GH_PWD}@github.com/iresty/apisix_dashboard_built.git" master:master
 echo ">>>>> git push successfully" && \
