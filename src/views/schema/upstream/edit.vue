9580b179c6d81401267550ff57dcc63bea70ea86
juzhiyuan <jjzhiyuan@gmail.com>
2019-11-26 23:11:11 +0800

    added URL support for upstream

--- src/views/schema/upstream/edit.vue
+++ src/views/schema/upstream/edit.vue
@@ -151,7 +151,7 @@ import { TagsViewModule } from '@/store/modules/tags-view'
   name: 'RouterEdit'
 })
 export default class extends Vue {
-  private IPAndURLRegexp = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?|^((http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/g
+  private IPAndURLRegexp = /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3})$|^((([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9]))$/
   private form = {
     type: null,
     key: null,
