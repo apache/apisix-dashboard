8f59cd6a600b57ea5ae1b2106b559b3133a12000
juzhiyuan <jjzhiyuan@gmail.com>
2020-03-12 12:39:13 +0800

    feat: added SSL service

---
+++ src/services/ssl.ts
@@ -0,0 +1,19 @@
+import request from '@/utils/request';
+import { transformFetchListData } from '@/transforms/global';
+import { SSL } from '@/models/ssl';
+
+export const fetchList = () => request('/api/ssl').then(data => transformFetchListData<SSL>(data));
+
+export const fetchItem = (id: number) => request(`/api/ssl/${id}`);
+
+export const remove = (id: number) => request.delete(`/api/ssl/${id}`);
+
+export const create = (data: SSL) =>
+  request.post('/api/ssl', {
+    data,
+  });
+
+export const update = (id: number, data: SSL) =>
+  request.put(`/api/ssl/${id}`, {
+    data,
+  });
