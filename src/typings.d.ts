7f57329c28875e1f7c95216b32b5ff6ab7e4956b
bzp2010 <bzp20000818@gmail.com>
2020-04-18 23:41:18 +0800

    feature: add ADMIN_API and API_KEY define

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -37,7 +37,8 @@ declare let ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: 'site' | undefine
 
 declare const REACT_APP_ENV: 'test' | 'dev' | 'pre' | false;
 
-declare let ADMIN_API: '' | undefined;
+declare let ADMIN_API: '';
+declare let API_KEY: '';
 
 type PageMode = 'CREATE' | 'EDIT' | 'VIEW';
 
