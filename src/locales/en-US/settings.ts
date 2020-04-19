e0407f1215dcb51026773dad2c3827883f57f4f3
bzp2010 <bzp20000818@gmail.com>
2020-04-19 18:57:41 +0800

    fix: missing i18n files

--- src/locales/en-US/settings.ts
+++ src/locales/en-US/settings.ts
@@ -1,3 +1,5 @@
 export default {
-  'app.settings.description.invalid-admin-api': 'Invalid Admin API'
+  'app.settings.description.invalid-admin-api-schema': 'Invalid Admin API Schema',
+  'app.settings.description.invalid-admin-api-host': 'Invalid Admin API Host',
+  'app.settings.description.invalid-admin-api-path': 'Invalid Admin API Path',
 };
--- src/locales/zh-CN/settings.ts
+++ src/locales/zh-CN/settings.ts
@@ -1,3 +1,5 @@
 export default {
-  'app.settings.description.invalid-admin-api': '非法的管理API'
+  'app.settings.description.invalid-admin-api-schema': '非法的管理API协议',
+  'app.settings.description.invalid-admin-api-host': '非法的管理API地址',
+  'app.settings.description.invalid-admin-api-path': '非法的管理API路径',
 };
