041a8ee89c7d58c57e918e49ee128ec4266b3a75
juzhiyuan <jjzhiyuan@gmail.com>
2020-05-20 23:21:41 +0800

    feat: update request

--- src/app.tsx
+++ src/app.tsx
@@ -1,48 +1,5 @@
-import React from 'react';
-import { BasicLayoutProps, Settings as LayoutSettings } from '@ant-design/pro-layout';
-
 import { notification } from 'antd';
-import { history, RequestConfig } from 'umi';
-import RightContent from '@/components/RightContent';
-import Footer from '@/components/Footer';
-import { queryCurrent } from './services/user';
-
-import defaultSettings from '../config/defaultSettings';
-
-export async function getInitialState(): Promise<{
-  currentUser?: API.CurrentUser;
-  settings?: LayoutSettings;
-}> {
-  // 如果是登录页面，不执行
-  if (history.location.pathname !== '/user/login') {
-    try {
-      const currentUser = await queryCurrent();
-      return {
-        currentUser,
-        settings: defaultSettings,
-      };
-    } catch (error) {
-      history.push('/user/login');
-    }
-  }
-  return {
-    settings: defaultSettings,
-  };
-}
-
-export const layout = ({
-  initialState,
-}: {
-  initialState: { settings?: LayoutSettings };
-}): BasicLayoutProps => {
-  return {
-    rightContentRender: () => <RightContent />,
-    disableContentMargin: false,
-    footerRender: () => <Footer />,
-    menuHeaderRender: undefined,
-    ...initialState?.settings,
-  };
-};
+import { RequestConfig } from 'umi';
 
 const codeMessage = {
   200: '服务器成功返回请求的数据。',
