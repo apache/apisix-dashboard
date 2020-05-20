089a164e545a1b6ecf6d52e4cfdfc5e6b8a6ce07
juzhiyuan <jjzhiyuan@gmail.com>
2020-05-20 23:25:56 +0800

    feat: update plugin service

--- src/services/plugin.ts
+++ src/services/plugin.ts
@@ -1,7 +1,7 @@
-import request from '@/utils/request';
+import { request } from 'umi';
 import { transformSchemaFromAPI } from '@/transforms/plugin';
 
 export const fetchList = () => request('/api/plugins/list');
 
 export const fetchPluginSchema = (name: string): Promise<PluginSchema> =>
-  request(`/api/schema/plugins/${name}`).then(data => transformSchemaFromAPI(data, name));
+  request(`/api/schema/plugins/${name}`).then((data) => transformSchemaFromAPI(data, name));
