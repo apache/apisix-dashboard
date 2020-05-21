ca7440f63773f8c2a993b0f76a8eeaa09dd3893d
juzhiyuan <jjzhiyuan@gmail.com>
2020-05-21 13:54:44 +0800

    feat: remove unused files

--- src/components/RightContent/index.tsx
+++ src/components/RightContent/index.tsx
@@ -3,7 +3,6 @@ import { QuestionCircleOutlined } from '@ant-design/icons';
 import React from 'react';
 import { useModel, SelectLang } from 'umi';
 import Avatar from './AvatarDropdown';
-import HeaderSearch from '../HeaderSearch';
 import styles from './index.less';
 
 export type SiderTheme = 'light' | 'dark';
@@ -29,31 +28,11 @@ const GlobalHeaderRight: React.FC<{}> = () => {
   }
   return (
     <Space className={className}>
-      <HeaderSearch
-        className={`${styles.action} ${styles.search}`}
-        placeholder="站内搜索"
-        defaultValue="umi ui"
-        options={[
-          { label: <a href="https://umijs.org/zh/guide/umi-ui.html">umi ui</a>, value: 'umi ui' },
-          {
-            label: <a href="next.ant.design">Ant Design</a>,
-            value: 'Ant Design',
-          },
-          {
-            label: <a href="https://protable.ant.design/">Pro Table</a>,
-            value: 'Pro Table',
-          },
-          {
-            label: <a href="https://prolayout.ant.design/">Pro Layout</a>,
-            value: 'Pro Layout',
-          },
-        ]}
-      />
-      <Tooltip title="使用文档">
+      <Tooltip title="Documentation">
         <span
           className={styles.action}
           onClick={() => {
-            window.location.href = 'https://pro.ant.design/docs/getting-started';
+            window.location.href = 'https://github.com/apache/incubator-apisix';
           }}
         >
           <QuestionCircleOutlined />
