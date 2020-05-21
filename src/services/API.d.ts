8a892fd6b4f0c308da10f7a3e0992710bcc962ea
juzhiyuan <jjzhiyuan@gmail.com>
2020-05-21 11:51:00 +0800

    feat: update typings

--- src/services/API.d.ts
+++ src/services/API.d.ts
@@ -2,13 +2,6 @@ declare namespace API {
   export interface CurrentUser {
     avatar?: string;
     name?: string;
-    title?: string;
-    group?: string;
-    signature?: string;
-    tags?: {
-      key: string;
-      label: string;
-    }[];
     userid?: string;
     access?: 'user' | 'guest' | 'admin';
     unreadCount?: number;
