5d7d6661f6098cea3a12fa3e7fb601f0f8a2f38d
bzp2010 <bzp20000818@gmail.com>
2020-05-22 15:16:37 +0800

    fix: resolve the conflict

--- src/pages/ssl/service.ts
+++ src/pages/ssl/service.ts
@@ -2,18 +2,10 @@ import { request } from 'umi';
 import { transformFetchListData, transformFetchItemData } from '@/transforms/global';
 
 export const fetchList = () =>
-import { request } from 'umi';
-import { transformFetchListData, transformFetchItemData } from '@/transforms/global';
-
-export const fetchList = () =>
-  request('/ssl').then((data) => transformFetchListData<SSL>(data));
-  request('/api/ssl').then((data) => transformFetchListData<SSLModule.SSL>(data));
+  request('/ssl').then((data) => transformFetchListData<SSLModule.SSL>(data));
 
 export const fetchItem = (key: string) =>
-
-export const fetchItem = (key: string) =>
-  request(`/ssl/${key}`).then((data) => transformFetchItemData<SSL>(data));
-  request(`/api/ssl/${key}`).then((data) => transformFetchItemData<SSLModule.SSL>(data));
+  request(`/ssl/${key}`).then((data) => transformFetchItemData<SSLModule.SSL>(data));
 
 export const remove = (key: string) =>
   request(`/ssl/${key}`, {
@@ -21,7 +13,6 @@ export const remove = (key: string) =>
   });
 
 export const create = (data: SSLModule.SSL) =>
-  request('/api/ssl', {
   request('/ssl', {
     method: 'POST',
     data,
