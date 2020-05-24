17100cb87d4aa2a0ee71a0d8df4a1df3e176deb4
juzhiyuan <jjzhiyuan@gmail.com>
2020-05-25 06:16:31 +0800

    fix: setting

--- src/pages/Settings/constant.ts
+++
@@ -1,7 +0,0 @@
-import { getRequestPrefix } from './service';
-
-export const ADMIN_API_SCHEMA = 'http';
-export const ADMIN_API_HOST = '127.0.0.1:9080';
-export const ADMIN_API_PATH = '/apisix/admin';
-export const API_KEY = '';
-export const API_REQUEST_PREFIX = getRequestPrefix();
