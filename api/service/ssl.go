6e021c48dfdce91c7272e5ab6d2ba796999526a7
kv <gxthrj@163.com>
2020-06-11 17:25:57 +0800

    check ASF HEADER

--- api/service/ssl.go
+++ api/service/ssl.go
@@ -1,19 +1,19 @@
-//
-//Licensed to the Apache Software Foundation (ASF) under one or more
-//contributor license agreements.  See the NOTICE file distributed with
-//this work for additional information regarding copyright ownership.
-//The ASF licenses this file to You under the Apache License, Version 2.0
-//(the "License"); you may not use this file except in compliance with
-//the License.  You may obtain a copy of the License at
-//
-//http://www.apache.org/licenses/LICENSE-2.0
-//
-//Unless required by applicable law or agreed to in writing, software
-//distributed under the License is distributed on an "AS IS" BASIS,
-//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//See the License for the specific language governing permissions and
-//limitations under the License.
-//
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package service
 
 import (
@@ -260,12 +260,11 @@ func (req *SslRequest) DeleteFromApisix() (*ApisixSslResponse, error) {
 }
 
 func ParseCert(crt, key string) (*Ssl, error) {
-	//print private key 
+	// print private key
 	certDERBlock, _ := pem.Decode([]byte(crt))
 	if certDERBlock == nil {
 		return nil, errors.New("Certificate resolution failed")
 	}
-
 	// match
 	_, err := tls.X509KeyPair([]byte(crt), []byte(key))
 	if err != nil {
