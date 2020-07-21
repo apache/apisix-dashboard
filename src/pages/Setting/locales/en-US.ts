bf31aca654442289d3289e465e467c7643411f1d
jiangfeng <popular_style_k@qq.com>
2020-07-21 17:11:19 +0800

    i18n set

---
+++ src/pages/Setting/locales/en-US.ts
@@ -0,0 +1,8 @@
+export default {
+  'set.update.configuration.successfully': 'Update Configuration Successfully',
+  'set':'Set',
+  'set.api.address':'API Address',
+  'set.grafana.address':'Grafana Address',
+  'set.grafana.address.rule':'Grafana address should begin with HTTP or HTTPS',
+  'set.grafana.address.illegality':'Address is illegality',
+};
