f15e5df159ad8aeae640087059da031875b77eff
bzp2010 <bzp20000818@gmail.com>
2020-07-22 15:52:18 +0800

    feat(authentication): add centent to Login page

--- src/pages/User/Login.tsx
+++ src/pages/User/Login.tsx
@@ -1,7 +1,41 @@
 import React from 'react';
+import { Tabs } from 'antd';
+
+import { DefaultFooter } from '@ant-design/pro-layout';
+import { SelectLang } from '@@/plugin-locale/SelectLang';
+import { Link } from 'umi';
+import logo from '@/assets/logo.svg';
+import styles from './Login.less';
+
+const Tab = Tabs.TabPane;
 
 const Page: React.FC = () => {
-  return <div />;
+  return (
+    <div className={styles.container}>
+      <div className={styles.lang}>
+        <SelectLang />
+      </div>
+      <div className={styles.content}>
+        <div className={styles.top}>
+          <div className={styles.header}>
+            <Link to="/">
+              <img alt="logo" className={styles.logo} src={logo} />
+              <span className={styles.title}>APISIX Dashboard</span>
+            </Link>
+          </div>
+          <div className={styles.desc}>Cloud-Native Microservices API Gateway</div>
+        </div>
+        <div className={styles.main}>
+          <Tabs>
+            <Tab key="test" tab="Test">
+              Test
+            </Tab>
+          </Tabs>
+        </div>
+      </div>
+      <DefaultFooter />
+    </div>
+  );
 };
 
 export default Page;
