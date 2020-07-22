64e4072db2543d396751faea02e8cc99fd82b6c0
bzp2010 <bzp20000818@gmail.com>
2020-07-22 17:20:46 +0800

    feat(authentication): update Login page to add Password and Test method

--- src/pages/User/Login.tsx
+++ src/pages/User/Login.tsx
@@ -1,15 +1,38 @@
-import React from 'react';
-import { Tabs } from 'antd';
-
+import React, { useState } from 'react';
+import { Button, Tabs } from 'antd';
 import { DefaultFooter } from '@ant-design/pro-layout';
 import { SelectLang } from '@@/plugin-locale/SelectLang';
 import { Link } from 'umi';
+import LoginMethodPassword from '@/pages/User/components/LoginMethodPassword';
+import LoginMethodTest from '@/pages/User/components/LoginMethodTest';
+import { UserModule } from '@/pages/User/typing';
 import logo from '@/assets/logo.svg';
 import styles from './Login.less';
 
 const Tab = Tabs.TabPane;
 
+/**
+ * Login Methods List
+ */
+const loginMethods: UserModule.LoginMethod[] = [LoginMethodPassword, LoginMethodTest];
+
+/**
+ * User Login Page
+ * @constructor
+ */
 const Page: React.FC = () => {
+  const [loginType, setLoginType] = useState(loginMethods[0]);
+
+  const onTabChange = (activeKey: string) => {
+    loginMethods.forEach((item, index) => {
+      if (activeKey === item.id) setLoginType(loginMethods[index]);
+    });
+  };
+
+  const onSubmit = () => {
+    setLoginType(loginMethods[0]);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.lang}>
@@ -26,11 +49,16 @@ const Page: React.FC = () => {
           <div className={styles.desc}>Cloud-Native Microservices API Gateway</div>
         </div>
         <div className={styles.main}>
-          <Tabs>
-            <Tab key="test" tab="Test">
-              Test
-            </Tab>
+          <Tabs activeKey={loginType.id} onChange={onTabChange}>
+            {loginMethods.map((item) => (
+              <Tab key={item.id} tab={item.name}>
+                {item.render()}
+              </Tab>
+            ))}
           </Tabs>
+          <Button className={styles.submit} size="large" type="primary" onClick={onSubmit}>
+            登录
+          </Button>
         </div>
       </div>
       <DefaultFooter />
---
+++ src/pages/User/components/LoginMethodPassword.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { UserModule } from '@/pages/User/typing';
+import { Form, Input } from 'antd';
+import { FormInstance } from 'antd/lib/form';
+import { UserOutlined, LockTwoTone } from '@ant-design/icons';
+
+const formRef = React.createRef<FormInstance>();
+
+const LoginMethodPassword: UserModule.LoginMethod = {
+  id: 'password',
+  name: '账户密码登录',
+  render: () => {
+    return (
+      <Form ref={formRef} name="control-ref">
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: '请输入用户名!',
+            },
+          ]}
+        >
+          <Input
+            size="large"
+            type="text"
+            placeholder="用户名"
+            prefix={
+              <UserOutlined
+                style={{
+                  color: '#1890ff',
+                }}
+              />
+            }
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: '请输入密码！',
+            },
+          ]}
+        >
+          <Input size="large" type="password" placeholder="密码" prefix={<LockTwoTone />} />
+        </Form.Item>
+      </Form>
+    );
+  },
+  getData(): UserModule.LoginData {
+    if (formRef.current) {
+      const data = formRef.current.getFieldsValue();
+      return {
+        username: data.username,
+        password: data.password,
+      };
+    }
+    return {};
+  },
+};
+
+export default LoginMethodPassword;
