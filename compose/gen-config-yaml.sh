1b389f8be4b653cad131f3c64badf88b6c40357e
kv <gxthrj@163.com>
2020-07-28 15:26:31 +0800

    doc: sync config.yaml from the latest version of APISIX

---
+++ compose/gen-config-yaml.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+export etcd_url='http://192.17.5.10:2379'
+
+wget https://raw.githubusercontent.com/apache/incubator-apisix/master/conf/config.yaml
+
+sed -i -e ':a' -e 'N' -e '$!ba' -e "s/allow_admin[a-z: #\/._]*\n\( *- [0-9a-zA-Z: #\/._',]*\n*\)*//g" config.yaml
+
+sed -i -e "s%http://[0-9.]*:2379%`echo $etcd_url`%g" config.yaml
+
+mv config.yaml ./apisix_conf/config.yaml
