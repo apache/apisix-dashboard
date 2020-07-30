8e805f0c973a344b02f32a42e648b82b1a037ae0
Lien <liling@apache.org>
2020-07-30 13:07:02 +0800

    feature: support run in mac system (#349)

--- compose/gen-config-yaml.sh
+++ compose/gen-config-yaml.sh
@@ -2,10 +2,18 @@
 
 export etcd_url='http://192.17.5.10:2379'
 
+unamestr=`uname`
+
 wget https://raw.githubusercontent.com/apache/incubator-apisix/master/conf/config.yaml
 
-sed -i -e ':a' -e 'N' -e '$!ba' -e "s/allow_admin[a-z: #\/._]*\n\( *- [0-9a-zA-Z: #\/._',]*\n*\)*//g" config.yaml
+if [[ "$unamestr" == 'Darwin' ]]; then
+   sed -i '' -e ':a' -e 'N' -e '$!ba' -e "s/allow_admin[a-z: #\/._]*\n\( *- [0-9a-zA-Z: #\/._',]*\n*\)*//g" config.yaml
+   sed -i '' -e "s%http://[0-9.]*:2379%`echo $etcd_url`%g" config.yaml
+else
+	sed -i -e ':a' -e 'N' -e '$!ba' -e "s/allow_admin[a-z: #\/._]*\n\( *- [0-9a-zA-Z: #\/._',]*\n*\)*//g" config.yaml
+	sed -i -e "s%http://[0-9.]*:2379%`echo $etcd_url`%g" config.yaml
+fi
 
-sed -i -e "s%http://[0-9.]*:2379%`echo $etcd_url`%g" config.yaml
+
 
 mv config.yaml ./apisix_conf/config.yaml
