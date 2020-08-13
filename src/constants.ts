7543eaaa58d8d052e03a6085ece58eff1000e8b6
juzhiyuan <juzhiyuan@apache.org>
2020-08-13 22:11:11 +0800

    feat: update API

--- src/constants.ts
+++ src/constants.ts
@@ -33,4 +33,4 @@ export const codeMessage = {
 };
 
 // NOTE: This is the Manager API Address, e.g http://127.0.0.1:8080/apisix/admin
-export const DEFAULT_BASE_URL = 'https://api.apisix.dev/apisix/admin';
+export const DEFAULT_BASE_URL = '/apisix/admin';
