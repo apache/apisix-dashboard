ea8af82d340397bb5f9e17c2e4e344bcc5582cb3
bzp2010 <bzp20000818@gmail.com>
2020-08-14 10:17:41 +0800

    feat(authentication): change cors Access-Control-Allow-Headers header

--- api/filter/cors.go
+++ api/filter/cors.go
@@ -22,7 +22,7 @@ func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Authorization")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "*")
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
