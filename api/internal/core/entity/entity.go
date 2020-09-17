2b701c36362b82d502bcc3707c9892532968a597
nic-chen <johz@163.com>
2020-09-17 23:26:40 +0800

    add upstream struct

--- api/internal/core/entity/entity.go
+++ api/internal/core/entity/entity.go
@@ -11,7 +11,7 @@ type Route struct {
 	Host            string      `json:"host,omitempty"`
 	Hosts           []string    `json:"hosts,omitempty"`
 	RemoteAddr      string      `json:"remote_addr,omitempty"`
-	RemoteAddrs     string      `json:"remote_addrs,omitempty"`
+	RemoteAddrs     []string    `json:"remote_addrs,omitempty"`
 	Vars            string      `json:"vars,omitempty"`
 	FilterFunc      string      `json:"filter_func,omitempty"`
 	Script          string      `json:"script,omitempty"`
@@ -22,5 +22,94 @@ type Route struct {
 	ServiceProtocol string      `json:"service_protocol,omitempty"`
 }
 
+type Timeout struct {
+  Connect int `json:"connect,omitempty"`
+  Send int `json:"send,omitempty"`
+  Read int `json:"read,omitempty"`
+}
+
+type Node struct {
+  Host string `json:"host,omitempty"`
+  Port int `json:"port,omitempty"`
+  Weight int `json:"weight,omitempty"`
+  Metadata interface{} `json:"metadata,omitempty"`
+}
+
+type K8sInfo struct {
+  Namespace string `json:"namespace,omitempty"`
+  DeployName string `json:"deploy_name,omitempty"`
+  ServiceName string `json:"service_name,omitempty"`
+  Port int `json:"port,omitempty"`
+  BackendType string `json:"backend_type,omitempty"`
+
+}
+
+type Healthy struct {
+  HttpStatuses []int `json:"http_statuses,omitempty"`
+  Successes int `json:"successes,omitempty"`
+}
+
+type UnHealthy struct {
+  HttpStatuses []int `json:"http_statuses,omitempty"`
+  TcpFailures int `json:"tcp_failures,omitempty"`
+  Timeouts int `json:"timeouts,omitempty"`
+  HttpFailures int `json:"http_failures,omitempty"`
+}
+
+type Active struct {
+  Type string `json:"type,omitempty"`
+  Timeout int `json:"timeout,omitempty"`
+  Concurrency int `json:"concurrency,omitempty"`
+  Host string `json:"host,omitempty"`
+  Port int `json:"port,omitempty"`
+  HttpPath string `json:"http_path,omitempty"`
+  HttpsVerifyCertificate string `json:"https_verify_certificate,omitempty"`
+  Healthy Healthy `json:"healthy,omitempty"`
+  UnHealthy UnHealthy `json:"unhealthy,omitempty"`
+  ReqHeaders []string `json:"req_headers,omitempty"`
+}
+
+type Passive struct {
+  Type string `json:"type,omitempty"`
+  Healthy Healthy `json:"healthy,omitempty"`
+  UnHealthy UnHealthy `json:"unhealthy,omitempty"`
+}
+
+type HealthChecker struct {
+  Active Active `json:"active,omitempty"`
+  Passive Passive `json:"passive,omitempty"`
+}
+
 type Upstream struct {
+  Nodes []Node `json:"nodes,omitempty"`
+  Retries int `json:"retries,omitempty"`
+  Timeout  Timeout `json:"timeout,omitempty"`
+  K8sInfo K8sInfo `json:"k8s_deployment_info,omitempty"`
+  Type string `json:"type,omitempty"`
+  Checks  HealthChecker `json:"checks,omitempty"`
+  HashOn  string `json:"hash_on,omitempty"`
+  Key  string `json:"key,omitempty"`
+  EnableWebsocket  bool `json:"enable_websocket,omitempty"`
+  PassHost  string `json:"pass_host,omitempty"`
+  UpstreamHost  string `json:"upstream_host,omitempty"`
+  Name  string `json:"name,omitempty"`
+  Desc  string `json:"desc,omitempty"`
+  ServiceName  string `json:"service_name,omitempty"`
+  ID  string `json:"id,omitempty"`
+}
+
+type Consumer struct {
+  Username string `json:"username"`
+  Desc     string `json:"desc"`
+  Plugins  string `json:"plugins"`
+}
+
+type Ssl struct {
+  ValidityStart uint64 `json:"validity_start"`
+  ValidityEnd   uint64 `json:"validity_end"`
+  Snis          string `json:"snis"`
+  Status        uint64 `json:"status"`
+  PublicKey     string `json:"public_key,omitempty"`
+  PublicKeyHash string `json:"public_key_hash,omitempty"`
+  WorkspaceId   string `json:"workspace_id"`
 }
