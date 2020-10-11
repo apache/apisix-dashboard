5f5bb3fdcb2e5babec698c2cc6c563b7a9c285f8
nic-chen <johz@163.com>
2020-10-11 23:59:41 +0800

    test: complete consumer test cases

--- api/internal/handler/consumer/consumer_test.go
+++ api/internal/handler/consumer/consumer_test.go
@@ -7,6 +7,7 @@ import (
 	"github.com/apisix/manager-api/internal/core/store"
 	"github.com/shiningrush/droplet"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -43,6 +44,25 @@ func TestConsumer(t *testing.T) {
 	_, err = handler.Create(ctx)
 	assert.Nil(t, err)
 
+	//create consumer 2
+	consumer2 := &entity.Consumer{}
+	reqBody = `{
+      "username": "pony",
+      "plugins": {
+          "limit-count": {
+              "count": 2,
+              "time_window": 60,
+              "rejected_code": 503,
+              "key": "remote_addr"
+          }
+      },
+    "desc": "test description"
+  }`
+	json.Unmarshal([]byte(reqBody), consumer2)
+	ctx.SetInput(consumer2)
+	_, err = handler.Create(ctx)
+	assert.Nil(t, err)
+
 	//get consumer
 	input := &GetInput{}
 	reqBody = `{"username": "jack"}`
@@ -54,9 +74,67 @@ func TestConsumer(t *testing.T) {
 	assert.Equal(t, stored.ID, consumer.ID)
 	assert.Equal(t, stored.Username, consumer.Username)
 
+	//update consumer
+	consumer3 := &UpdateInput{}
+	consumer3.Username = "pony"
+	reqBody = `{
+      "username": "pony",
+      "plugins": {
+          "limit-count": {
+              "count": 2,
+              "time_window": 60,
+              "rejected_code": 503,
+              "key": "remote_addr"
+          }
+      },
+    "desc": "test description2"
+  }`
+	json.Unmarshal([]byte(reqBody), consumer3)
+	ctx.SetInput(consumer3)
+	_, err = handler.Update(ctx)
+	assert.Nil(t, err)
+
+	//
+	time.Sleep(time.Duration(1) * time.Second)
+
+	//check update
+	input3 := &GetInput{}
+	reqBody = `{"username": "pony"}`
+	json.Unmarshal([]byte(reqBody), input3)
+	ctx.SetInput(input3)
+	ret3, err := handler.Get(ctx)
+	stored3 := ret3.(*entity.Consumer)
+	assert.Nil(t, err)
+	assert.Equal(t, stored3.Desc, "test description2") //consumer3.Desc)
+	assert.Equal(t, stored3.Username, consumer3.Username)
+
+	//list page 1
+	listInput := &ListInput{}
+	reqBody = `{"page_size": 1, "page_number": 1}`
+	json.Unmarshal([]byte(reqBody), listInput)
+	ctx.SetInput(listInput)
+	retPage1, err := handler.List(ctx)
+	dataPage1 := retPage1.(*store.ListOutput)
+	assert.Equal(t, len(dataPage1.Rows), 1)
+
+	//list page 2
+	listInput2 := &ListInput{}
+	reqBody = `{"page_size": 1, "page_number": 2}`
+	json.Unmarshal([]byte(reqBody), listInput2)
+	ctx.SetInput(listInput2)
+	retPage2, err := handler.List(ctx)
+	dataPage2 := retPage2.(*store.ListOutput)
+	assert.Equal(t, len(dataPage2.Rows), 1)
+
 	//delete consumer
 	inputDel := &BatchDelete{}
 	reqBody = `{"usernames": "jack"}`
+	json.Unmarshal([]byte(reqBody), inputDel)
+	ctx.SetInput(inputDel)
+	_, err = handler.BatchDelete(ctx)
+	assert.Nil(t, err)
+
+	reqBody = `{"usernames": "pony"}`
 	json.Unmarshal([]byte(reqBody), inputDel)
 	ctx.SetInput(inputDel)
 	_, err = handler.BatchDelete(ctx)
