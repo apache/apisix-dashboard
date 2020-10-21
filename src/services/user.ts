29ea0ed6f73f3a9cfe0f117822261775c327a6d0
juzhiyuan <juzhiyuan@apache.org>
2020-10-21 09:08:36 +0800

    feat: remove unused codes

--- src/services/user.ts
+++ src/services/user.ts
@@ -14,13 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { request } from 'umi';
-
 import logo from '@/assets/logo.svg';
-
-export async function query() {
-  return request<API.CurrentUser[]>('/api/users');
-}
 
 export async function queryCurrent(): Promise<API.CurrentUser> {
   return Promise.resolve({
@@ -29,8 +23,4 @@ export async function queryCurrent(): Promise<API.CurrentUser> {
     userid: '00000001',
     access: 'admin',
   });
-}
-
-export async function queryNotices(): Promise<any> {
-  return request<{ data: API.NoticeIconData[] }>('/api/notices');
 }
