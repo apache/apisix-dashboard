2f567e5ee9e9823b00940dee197ded4694b87088
bzp2010 <bzp2010@apache.org>
2020-10-22 20:06:47 +0800

    feat: add go generate command for compile manually

--- api/main.go
+++ api/main.go
@@ -14,6 +14,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+//go:generate statik -src=./dashboard -f
+
 package main
 
 import (
