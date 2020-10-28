3db65456a63245ac4c21e3da629aa794bfdc33b3
litesun <7sunmiao@gmail.com>
2020-10-28 18:48:52 +0800

    feat: update Login.e2e.js

--- src/e2e/Login.e2e.js
+++ src/e2e/Login.e2e.js
@@ -1,28 +1,39 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const puppeteer = require('puppeteer');
 
+let browser;
+beforeAll(async () => {
+  browser = await puppeteer.launch({ headless: false });
+});
+
 describe('Login', () => {
-  it('should login with failure', async () => {
-    const browser = await puppeteer.launch({ headless: true });
+  test('Login fail', async () => {
     const page = await browser.newPage();
     await page.goto('http://localhost:8000');
     await page.type('#control-ref_username', 'admin');
     await page.type('#control-ref_password', 'wrong_password');
     await page.click('.ant-btn-lg');
     await page.waitForSelector('.ant-notification-notice-icon-error'); // should display error
+    const element = await page.$('.ant-notification-notice-description');
+    const text = await (await element.getProperty('textContent')).jsonValue();
+    expect(text).toBe('username or password error');
     await page.close();
-    browser.close();
-  });
+  }, 10000);
 
-  it('should login with succcess', async () => {
-    const browser = await puppeteer.launch({ headless: true });
+  test('Login success', async () => {
     const page = await browser.newPage();
     await page.goto('http://localhost:8000');
     await page.type('#control-ref_username', 'admin');
     await page.type('#control-ref_password', 'admin');
     await page.click('.ant-btn-lg');
     await page.waitForSelector('.ant-notification-notice-icon-success');
+    const element = await page.$('.ant-notification-notice-description');
+    const text = await (await element.getProperty('textContent')).jsonValue();
+    expect(text).toBe('Login Success');
     await page.close();
-    browser.close();
+  }, 10000);
+
+  afterAll(async () => {
+    await browser.close();
   });
 });
