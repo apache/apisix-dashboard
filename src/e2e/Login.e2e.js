47a42b5a7e82da09ee95c4619af447b3e0d5042c
litesun <7sunmiao@gmail.com>
2020-10-29 19:12:15 +0800

    feat: added  start-server-and-test package

--- src/e2e/Login.e2e.js
+++ src/e2e/Login.e2e.js
@@ -20,7 +20,7 @@ const puppeteer = require('puppeteer');
 
 let browser;
 beforeAll(async () => {
-  browser = await puppeteer.launch({ headless: false });
+  browser = await puppeteer.launch({ headless: true });
 });
 
 describe('Login', () => {
