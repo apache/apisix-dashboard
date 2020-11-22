b7f6d4a55d9be54e5f3c60163914f6bfd173708b
nic-chen <johz@163.com>
2020-11-22 23:42:44 +0800

    test: add test cases

--- api/internal/core/entity/format_test.go
+++ api/internal/core/entity/format_test.go
@@ -23,19 +23,81 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestConsumer(t *testing.T) {
-	nodesStr := `{
-    "127.0.0.1:8080": 1
-  }`
-	nodesMap := map[string]float64{}
-	err := json.Unmarshal([]byte(nodesStr), &nodesMap)
+func TestFormat(t *testing.T) {
+
+	// map value for nodes
+	str := `{
+		"id": "1",
+		"nodes": {"host:80": 100},
+		"type":"roundrobin"
+	}`
+	var upstream Upstream
+	err := json.Unmarshal([]byte(str), &upstream)
 	assert.Nil(t, err)
 
-	res := NodesFormat(nodesMap)
+	res := NodesFormat(upstream.Nodes)
 	nodes := res.([]*Node)
-
 	assert.Equal(t, 1, len(nodes))
-	assert.Equal(t, "127.0.0.1", nodes[0].Host)
+	assert.Equal(t, "host", nodes[0].Host)
+	assert.Equal(t, 80, nodes[0].Port)
+	assert.Equal(t, 100, nodes[0].Weight)
+
+	// map value for nodes on route
+	str = `{
+		"uri": "/index.html",
+		"upstream": {
+			"type": "roundrobin",
+			"nodes": {
+				"39.97.63.215:8080": 1
+			}
+		}
+	}`
+	var route Route
+	err = json.Unmarshal([]byte(str), &route)
+	assert.Nil(t, err)
+
+	res = NodesFormat(route.Upstream.Nodes)
+	nodes = res.([]*Node)
+	assert.Equal(t, 1, len(nodes))
+	assert.Equal(t, "39.97.63.215", nodes[0].Host)
 	assert.Equal(t, 8080, nodes[0].Port)
 	assert.Equal(t, 1, nodes[0].Weight)
+
+	// array value for nodes
+	str = `{
+		"uri": "/index.html",
+		"upstream": {
+			"type": "roundrobin",
+			"nodes": [{
+				"host": "10.1.1.1",
+				"port": 8888,
+				"weight": 1
+			}]
+		}
+	}`
+	err = json.Unmarshal([]byte(str), &route)
+	assert.Nil(t, err)
+
+	res = NodesFormat(route.Upstream.Nodes)
+	nodes = res.([]*Node)
+	assert.Equal(t, 1, len(nodes))
+	assert.Equal(t, "10.1.1.1", nodes[0].Host)
+	assert.Equal(t, 8888, nodes[0].Port)
+	assert.Equal(t, 1, nodes[0].Weight)
+
+	// empty value for nodes
+	str = `{
+		"uri": "/index.html",
+		"upstream": {
+			"type": "roundrobin",
+			"nodes": ""
+		}
+	}`
+	err = json.Unmarshal([]byte(str), &route)
+	assert.Nil(t, err)
+
+	res = NodesFormat(route.Upstream.Nodes)
+	nodes, ok := res.([]*Node)
+	assert.Equal(t, false, ok)
+
 }
