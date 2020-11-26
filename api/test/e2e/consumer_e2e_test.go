34c5b8264237fce3ae0c168ca70067d2a1c85326
idbeta <idbeta@gmail.com>
2020-11-26 11:00:36 +0800

    update testcase

--- api/test/e2e/consumer_e2e_test.go
+++ api/test/e2e/consumer_e2e_test.go
@@ -17,8 +17,14 @@
 package e2e
 
 import (
+	"io/ioutil"
 	"net/http"
+	"strings"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/tidwall/gjson"
 )
 
 //case 1: add consumer with username
@@ -153,6 +159,24 @@ func TestConsumer_delete_consumer(t *testing.T) {
 			ExpectStatus: http.StatusUnauthorized,
 			ExpectBody:   "Missing API key found in request",
 			Sleep:        sleepTime,
+		},
+	}
+
+	for _, tc := range tests {
+		testCaseCheck(tc)
+	}
+}
+
+//case 4: delete consumer(id: not_found)
+func TestConsumer_delete_notexit_consumer(t *testing.T) {
+	tests := []HttpTestCase{
+		{
+			caseDesc:     "delete notexit consumer",
+			Object:       MangerApiExpect(t),
+			Method:       http.MethodDelete,
+			Path:         "/apisix/admin/consumers/notexit",
+			Headers:      map[string]string{"Authorization": token},
+			ExpectStatus: http.StatusNotFound,
 		},
 	}
 
@@ -321,6 +345,72 @@ func TestConsumer_add_consumer_with_labels(t *testing.T) {
 		testCaseCheck(tc)
 	}
 }
+
+//case 8: update consumer, check if updatetime is updated
+func TestConsumer_create_consumer_with_createtime_updatetime(t *testing.T) {
+	//create consumer, save the result_A ( create_time and update_time )
+	data := `{
+		"username":"case_8",
+		"desc": "new consumer"
+   }`
+	request, _ := http.NewRequest("PUT", "http://127.0.0.1:8080/apisix/admin/consumers", strings.NewReader(data))
+	request.Header.Add("Authorization", token)
+	resp, _ := http.DefaultClient.Do(request)
+	defer resp.Body.Close()
+	respBody, _ := ioutil.ReadAll(resp.Body)
+	code := gjson.Get(string(respBody), "code")
+	assert.Equal(t, code.String(), "0")
+
+	time.Sleep(1 * time.Second)
+
+	request, _ = http.NewRequest("GET", "http://127.0.0.1:8080/apisix/admin/consumers/case_8", nil)
+	request.Header.Add("Authorization", token)
+	resp, _ = http.DefaultClient.Do(request)
+	defer resp.Body.Close()
+	respBody, _ = ioutil.ReadAll(resp.Body)
+	createtime := gjson.Get(string(respBody), "data.create_time")
+	updatetime := gjson.Get(string(respBody), "data.update_time")
+
+	//create consumer again, compare the new result and the result_A
+	data = `{
+		"username":"case_8",
+		"desc": "new consumer haha"
+   }`
+	request, _ = http.NewRequest("PUT", "http://127.0.0.1:8080/apisix/admin/consumers", strings.NewReader(data))
+	request.Header.Add("Authorization", token)
+	resp, _ = http.DefaultClient.Do(request)
+	defer resp.Body.Close()
+	respBody, _ = ioutil.ReadAll(resp.Body)
+	code = gjson.Get(string(respBody), "code")
+	assert.Equal(t, code.String(), "0")
+
+	time.Sleep(1 * time.Second)
+
+	request, _ = http.NewRequest("GET", "http://127.0.0.1:8080/apisix/admin/consumers/case_8", nil)
+	request.Header.Add("Authorization", token)
+	resp, _ = http.DefaultClient.Do(request)
+	defer resp.Body.Close()
+	respBody, _ = ioutil.ReadAll(resp.Body)
+	createtime2 := gjson.Get(string(respBody), "data.create_time")
+	updatetime2 := gjson.Get(string(respBody), "data.update_time")
+
+	assert.Equal(t, createtime.String(), createtime2.String())
+	assert.NotEqual(t, updatetime.String(), updatetime2.String())
+
+	//deletea consumer
+	request, _ = http.NewRequest("DELETE", "http://127.0.0.1:8080/apisix/admin/consumers/case_8", nil)
+	request.Header.Add("Authorization", token)
+	_, err := http.DefaultClient.Do(request)
+	assert.Nil(t, err)
+
+}
+
+// TODO it's a bug here, see: https://github.com/apache/apisix-dashboard/issues/852
+
+// TODO it's a bug here, see: https://github.com/apache/apisix-dashboard/issues/568
+
+// TODO it's a bug here, see: https://github.com/apache/apisix-dashboard/issues/870
+
 
 //Teardown
 func TestConsumer_teardown(t *testing.T) {
