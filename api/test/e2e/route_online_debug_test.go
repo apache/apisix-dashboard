199d5e86b2993d8294820fbfbb9cb59be738509d
liuxiran <belovedxixi@126.com>
2020-12-11 18:48:59 +0800

    fix: e2e

--- api/test/e2e/route_online_debug_test.go
+++ api/test/e2e/route_online_debug_test.go
@@ -21,6 +21,7 @@ import (
 	"net/http"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/tidwall/gjson"
@@ -302,7 +303,7 @@ func TestRoute_Online_Debug_Route_With_Basic_Auth(t *testing.T) {
 		testCaseCheck(tc)
 	}
 
-	// online debug without auth
+	// online debug without basic-auth
 	basepath := "http://127.0.0.1:9000/apisix/admin/debug-request-forwarding"
 	request, _ := http.NewRequest("POST", basepath, strings.NewReader(`{"url": "http://172.16.238.30:9080/hello","method": "GET"}`))
 	request.Header.Add("Authorization", token)
@@ -316,53 +317,53 @@ func TestRoute_Online_Debug_Route_With_Basic_Auth(t *testing.T) {
 	assert.Equal(t, `{"code":401,"message":"401 Unauthorized","data":{"message":"Missing authorization in request"}}`, realBody.String())
 }
 
-/*func TestRoute_Online_Debug_Route_With_Jwt_Auth(t *testing.T) {
-    tests := []HttpTestCase{
-        {
-            caseDesc:     "make sure the route is not created ",
-            Object:       APISIXExpect(t),
-            Method:       http.MethodGet,
-            Path:         "/hello",
-            ExpectStatus: http.StatusNotFound,
-            ExpectBody:   `{"error_msg":"404 Route Not Found"}`,
-        },
-        {
-            caseDesc: "create route enable basic-auth plugin",
-            Object:   ManagerApiExpect(t),
-            Method:   http.MethodPut,
-            Path:     "/apisix/admin/routes/r1",
-            Body: `{
-                    "uri": "/hello",
-                    "methods": ["GET"],
-                    "plugins": {
-                        "jwt-auth": {}
-                    },
-                    "upstream": {
-                        "type": "roundrobin",
-                        "nodes": [{
-                            "host": "172.16.238.20",
-                            "port": 1980,
-                            "weight": 1
-                        }]
-                    }
-                }`,
-            Headers:      map[string]string{"Authorization": token},
-            ExpectStatus: http.StatusOK,
-        },
-        {
-            caseDesc:     "make sure the consumer is not created",
-            Object:       ManagerApiExpect(t),
-            Method:       http.MethodGet,
-            Path:         "/apisix/admin/consumers/jack",
-            Headers:      map[string]string{"Authorization": token},
-            ExpectStatus: http.StatusNotFound,
-        },
-        {
-            caseDesc: "create consumer",
-            Object:   ManagerApiExpect(t),
-            Path:     "/apisix/admin/consumers",
-            Method:   http.MethodPut,
-            Body: `{
+func TestRoute_Online_Debug_Route_With_Jwt_Auth(t *testing.T) {
+	tests := []HttpTestCase{
+		{
+			caseDesc:     "make sure the route is not created ",
+			Object:       APISIXExpect(t),
+			Method:       http.MethodGet,
+			Path:         "/hello",
+			ExpectStatus: http.StatusNotFound,
+			ExpectBody:   `{"error_msg":"404 Route Not Found"}`,
+		},
+		{
+			caseDesc: "create route enable basic-auth plugin",
+			Object:   ManagerApiExpect(t),
+			Method:   http.MethodPut,
+			Path:     "/apisix/admin/routes/r1",
+			Body: `{
+                "uri": "/hello",
+                "methods": ["GET"],
+                "plugins": {
+                    "jwt-auth": {}
+                },
+                "upstream": {
+                    "type": "roundrobin",
+                    "nodes": [{
+                        "host": "172.16.238.20",
+                        "port": 1980,
+                        "weight": 1
+                    }]
+                }
+            }`,
+			Headers:      map[string]string{"Authorization": token},
+			ExpectStatus: http.StatusOK,
+		},
+		{
+			caseDesc:     "make sure the consumer is not created",
+			Object:       ManagerApiExpect(t),
+			Method:       http.MethodGet,
+			Path:         "/apisix/admin/consumers/jack",
+			Headers:      map[string]string{"Authorization": token},
+			ExpectStatus: http.StatusNotFound,
+		},
+		{
+			caseDesc: "create consumer",
+			Object:   ManagerApiExpect(t),
+			Path:     "/apisix/admin/consumers",
+			Method:   http.MethodPut,
+			Body: `{
                 "username": "jack",
                 "plugins": {
                     "jwt-auth": {
@@ -373,103 +374,107 @@ func TestRoute_Online_Debug_Route_With_Basic_Auth(t *testing.T) {
                 },
                 "desc": "test description"
             }`,
-            Headers:      map[string]string{"Authorization": token},
-            ExpectStatus: http.StatusOK,
-        },
-    }
+			Headers:      map[string]string{"Authorization": token},
+			ExpectStatus: http.StatusOK,
+		},
+	}
 
-    // sign jwt token
-    body, status, err := httpGet("http://127.0.0.1:9080/apisix/plugin/jwt/sign?key=user-key")
-    assert.Nil(t, err)
-    assert.Equal(t, http.StatusOK, status)
-    jwtToken := string(body)
+	// sign jwt token
+	body, status, err := httpGet("http://127.0.0.1:9080/apisix/plugin/jwt/sign?key=user-key")
+	assert.Nil(t, err)
+	assert.Equal(t, http.StatusOK, status)
+	jwtToken := string(body)
 
-    for _, tc := range tests {
-        testCaseCheck(tc)
-    }
+	for _, tc := range tests {
+		testCaseCheck(tc)
+	}
 
-    time.Sleep(sleepTime)
+	time.Sleep(sleepTime)
 
-    // verify token and clean test data
-    tests = []HttpTestCase{
-        {
-            caseDesc: "online debug route with jwt token",
-            Object:   ManagerApiExpect(t),
-            Method:   http.MethodPost,
-            Path:     "/apisix/admin/debug-request-forwarding",
-            Body: `{
-                "uri": "http://127.0.0.1:9080/hello",
+	// verify token and clean test data
+	tests = []HttpTestCase{
+		{
+			caseDesc: "online debug route with jwt token",
+			Object:   ManagerApiExpect(t),
+			Method:   http.MethodPost,
+			Path:     "/apisix/admin/debug-request-forwarding",
+			Body: `{
+                "url": "http://172.16.238.30:9080/hello",
                 "method": "GET",
                 "headerParams": {
-                    "Authorization":`+ jwtToken +`,
+                    "Authorization":[` + jwtToken + `],
                 }
             }`,
-            Headers:      map[string]string{"Authorization": token},
-            ExpectStatus: http.StatusOK,
-        },
-        {
-            caseDesc:     "verify route without jwt token",
-            Object:       APISIXExpect(t),
-            Method:       http.MethodGet,
-            Path:         "/hello",
-            ExpectStatus: http.StatusOK,
-            ExpectBody:   `{"message":"Missing JWT token in request"}`,
-            Sleep:        sleepTime,
-        },
-    }
+			Headers:      map[string]string{"Authorization": token},
+			ExpectStatus: http.StatusOK,
+		},
+	}
 
-    for _, tc := range tests {
-        testCaseCheck(tc)
-    }
+	for _, tc := range tests {
+		testCaseCheck(tc)
+	}
+
+	// online debug without jwt-auth
+	basepath := "http://127.0.0.1:9000/apisix/admin/debug-request-forwarding"
+	request, _ := http.NewRequest("POST", basepath, strings.NewReader(`{"url": "http://172.16.238.30:9080/hello","method": "GET"}`))
+	request.Header.Add("Authorization", token)
+	resp, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	respBody, _ := ioutil.ReadAll(resp.Body)
+	realBody := gjson.Get(string(respBody), "data")
+	assert.Equal(t, `{"code":401,"message":"401 Unauthorized","data":{"message":"Missing authorization in request"}}`, realBody.String())
 }
 
 func TestRoute_Online_Debug_Route_With_Key_Auth(t *testing.T) {
-    tests := []HttpTestCase{
-        {
-            caseDesc:     "make sure the route is not created ",
-            Object:       APISIXExpect(t),
-            Method:       http.MethodGet,
-            Path:         "/hello",
-            ExpectStatus: http.StatusNotFound,
-            ExpectBody:   `{"error_msg":"404 Route Not Found"}`,
-        },
-        {
-            caseDesc: "create route enable key-auth plugin",
-            Object:   ManagerApiExpect(t),
-            Method:   http.MethodPut,
-            Path:     "/apisix/admin/routes/r1",
-            Body: `{
-                    "uri": "/hello",
-                    "methods": ["GET"],
-                    "plugins": {
-                        "key-auth": {}
-                    },
-                    "upstream": {
-                        "type": "roundrobin",
-                        "nodes": [{
-                            "host": "172.16.238.20",
-                            "port": 1980,
-                            "weight": 1
-                        }]
-                    }
-                }`,
-            Headers:      map[string]string{"Authorization": token},
-            ExpectStatus: http.StatusOK,
-        },
-        {
-            caseDesc:     "make sure the consumer is not created",
-            Object:       ManagerApiExpect(t),
-            Method:       http.MethodGet,
-            Path:         "/apisix/admin/consumers/jack",
-            Headers:      map[string]string{"Authorization": token},
-            ExpectStatus: http.StatusNotFound,
-        },
-        {
-            caseDesc: "create consumer",
-            Object:   ManagerApiExpect(t),
-            Path:     "/apisix/admin/consumers",
-            Method:   http.MethodPut,
-            Body: `{
+	tests := []HttpTestCase{
+		{
+			caseDesc:     "make sure the route is not created ",
+			Object:       APISIXExpect(t),
+			Method:       http.MethodGet,
+			Path:         "/hello",
+			ExpectStatus: http.StatusNotFound,
+			ExpectBody:   `{"error_msg":"404 Route Not Found"}`,
+		},
+		{
+			caseDesc: "create route enable key-auth plugin",
+			Object:   ManagerApiExpect(t),
+			Method:   http.MethodPut,
+			Path:     "/apisix/admin/routes/r1",
+			Body: `{
+                "uri": "/hello",
+                "methods": ["GET"],
+                "plugins": {
+                    "key-auth": {}
+                },
+                "upstream": {
+                    "type": "roundrobin",
+                    "nodes": [{
+                        "host": "172.16.238.20",
+                        "port": 1980,
+                        "weight": 1
+                    }]
+                }
+            }`,
+			Headers:      map[string]string{"Authorization": token},
+			ExpectStatus: http.StatusOK,
+		},
+		{
+			caseDesc:     "make sure the consumer is not created",
+			Object:       ManagerApiExpect(t),
+			Method:       http.MethodGet,
+			Path:         "/apisix/admin/consumers/jack",
+			Headers:      map[string]string{"Authorization": token},
+			ExpectStatus: http.StatusNotFound,
+		},
+		{
+			caseDesc: "create consumer",
+			Object:   ManagerApiExpect(t),
+			Path:     "/apisix/admin/consumers",
+			Method:   http.MethodPut,
+			Body: `{
                 "username": "jack",
                 "plugins": {
                     "key-auth": {
@@ -478,94 +483,94 @@ func TestRoute_Online_Debug_Route_With_Key_Auth(t *testing.T) {
                 },
                 "desc": "test description"
             }`,
-            Headers:      map[string]string{"Authorization": token},
-            ExpectStatus: http.StatusOK,
-        },
-        {
-            caseDesc: "online debug route with apikey",
-            Object:   ManagerApiExpect(t),
-            Method:   http.MethodPost,
-            Path:     "/apisix/admin/debug-request-forwarding",
-            Body: `{
-                "uri": "http://127.0.0.1:9080/hello",
+			Headers:      map[string]string{"Authorization": token},
+			ExpectStatus: http.StatusOK,
+		},
+		{
+			caseDesc: "online debug route with apikey",
+			Object:   ManagerApiExpect(t),
+			Method:   http.MethodPost,
+			Path:     "/apisix/admin/debug-request-forwarding",
+			Body: `{
+                "uri": "http://172.16.238.30:9080/hello",
                 "method": "GET",
                 "headerParams": {
-                    "apikey": "user-key",
+                    "apikey": ["user-key"],
                 }
             }`,
-            Headers:      map[string]string{"Authorization": token},
-            ExpectStatus: http.StatusOK,
-        },
-        {
-            caseDesc: "online debug route without apikey",
-            Object:   ManagerApiExpect(t),
-            Method:   http.MethodPost,
-            Path:     "/apisix/admin/debug-request-forwarding",
-            Body: `{
-                "uri": "http://127.0.0.1:9080/hello",
-                "method": "GET"
-            }`,
-            Headers:      map[string]string{"Authorization": token},
-            ExpectStatus: http.StatusOK,
-            ExpectBody:   `{"message":"Missing JWT token in request"}`,
-        },
-    }
+			Headers:      map[string]string{"Authorization": token},
+			ExpectStatus: http.StatusOK,
+		},
+	}
 
-    for _, tc := range tests {
-        testCaseCheck(tc)
-    }
+	for _, tc := range tests {
+		testCaseCheck(tc)
+	}
+
+	// online debug without key-auth
+	basepath := "http://127.0.0.1:9000/apisix/admin/debug-request-forwarding"
+	request, _ := http.NewRequest("POST", basepath, strings.NewReader(`{"url": "http://172.16.238.30:9080/hello","method": "GET"}`))
+	request.Header.Add("Authorization", token)
+	resp, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	respBody, _ := ioutil.ReadAll(resp.Body)
+	realBody := gjson.Get(string(respBody), "data")
+	assert.Equal(t, `{"code":401,"message":"401 Unauthorized","data":{"message":"Missing authorization in request"}}`, realBody.String())
 }
 
 func TestRoute_Online_Debug_Route_With_Query_Params_Key_Auth(t *testing.T) {
-    tests := []HttpTestCase{
-        {
-            caseDesc:     "make sure the route is not created ",
-            Object:       APISIXExpect(t),
-            Method:       http.MethodGet,
-            Path:         "/hello",
-            ExpectStatus: http.StatusNotFound,
-            ExpectBody:   `{"error_msg":"404 Route Not Found"}`,
-        },
-        {
-            caseDesc: "create route enable key-auth plugin",
-            Object:   ManagerApiExpect(t),
-            Method:   http.MethodPut,
-            Path:     "/apisix/admin/routes/r1",
-            Body: `{
-                    "uri": "/hello",
-                    "methods": ["GET"],
-                    "vars": [
-                        ["arg_name","==","aaa"]
-                    ],
-                    "plugins": {
-                        "key-auth": {}
-                    },
-                    "upstream": {
-                        "type": "roundrobin",
-                        "nodes": [{
-                            "host": "172.16.238.20",
-                            "port": 1980,
-                            "weight": 1
-                        }]
-                    }
-                }`,
-            Headers:      map[string]string{"Authorization": token},
-            ExpectStatus: http.StatusOK,
-        },
-        {
-            caseDesc:     "make sure the consumer is not created",
-            Object:       ManagerApiExpect(t),
-            Method:       http.MethodGet,
-            Path:         "/apisix/admin/consumers/jack",
-            Headers:      map[string]string{"Authorization": token},
-            ExpectStatus: http.StatusNotFound,
-        },
-        {
-            caseDesc: "create consumer",
-            Object:   ManagerApiExpect(t),
-            Path:     "/apisix/admin/consumers",
-            Method:   http.MethodPut,
-            Body: `{
+	tests := []HttpTestCase{
+		{
+			caseDesc:     "make sure the route is not created ",
+			Object:       APISIXExpect(t),
+			Method:       http.MethodGet,
+			Path:         "/hello",
+			ExpectStatus: http.StatusNotFound,
+			ExpectBody:   `{"error_msg":"404 Route Not Found"}`,
+		},
+		{
+			caseDesc: "create route enable key-auth plugin",
+			Object:   ManagerApiExpect(t),
+			Method:   http.MethodPut,
+			Path:     "/apisix/admin/routes/r1",
+			Body: `{
+                "uri": "/hello",
+                "methods": ["GET"],
+                "vars": [
+                    ["arg_name","==","aaa"]
+                ],
+                "plugins": {
+                    "key-auth": {}
+                },
+                "upstream": {
+                    "type": "roundrobin",
+                    "nodes": [{
+                        "host": "172.16.238.20",
+                        "port": 1980,
+                        "weight": 1
+                    }]
+                }
+            }`,
+			Headers:      map[string]string{"Authorization": token},
+			ExpectStatus: http.StatusOK,
+		},
+		{
+			caseDesc:     "make sure the consumer is not created",
+			Object:       ManagerApiExpect(t),
+			Method:       http.MethodGet,
+			Path:         "/apisix/admin/consumers/jack",
+			Headers:      map[string]string{"Authorization": token},
+			ExpectStatus: http.StatusNotFound,
+		},
+		{
+			caseDesc: "create consumer",
+			Object:   ManagerApiExpect(t),
+			Path:     "/apisix/admin/consumers",
+			Method:   http.MethodPut,
+			Body: `{
                 "username": "jack",
                 "plugins": {
                     "key-auth": {
@@ -574,40 +579,40 @@ func TestRoute_Online_Debug_Route_With_Query_Params_Key_Auth(t *testing.T) {
                 },
                 "desc": "test description"
             }`,
-            Headers:      map[string]string{"Authorization": token},
-            ExpectStatus: http.StatusOK,
-        },
-        {
-            caseDesc: "online debug route with apikey",
-            Object:   ManagerApiExpect(t),
-            Method:   http.MethodPost,
-            Path:     "/apisix/admin/debug-request-forwarding",
-            Body: `{
-                "uri": "http://127.0.0.1:9080/hello?name=aaa",
+			Headers:      map[string]string{"Authorization": token},
+			ExpectStatus: http.StatusOK,
+		},
+		{
+			caseDesc: "online debug route with apikey",
+			Object:   ManagerApiExpect(t),
+			Method:   http.MethodPost,
+			Path:     "/apisix/admin/debug-request-forwarding",
+			Body: `{
+                "uri": "http://172.16.238.30:9080/hello?name=aaa",
                 "method": "GET",
                 "headerParams": {
-                    "apikey": "user-key",
+                    "apikey": ["user-key"],
                 }
             }`,
-            Headers:      map[string]string{"Authorization": token},
-            ExpectStatus: http.StatusOK,
-        },
-        {
-            caseDesc: "online debug route without apikey",
-            Object:   ManagerApiExpect(t),
-            Method:   http.MethodPost,
-            Path:     "/apisix/admin/debug-request-forwarding",
-            Body: `{
-                "uri": "http://127.0.0.1:9080/hello?name=aaa",
-                "method": "GET"
-            }`,
-            Headers:      map[string]string{"Authorization": token},
-            ExpectStatus: http.StatusOK,
-            ExpectBody:   `{"message":"Missing JWT token in request"}`,
-        },
-    }
+			Headers:      map[string]string{"Authorization": token},
+			ExpectStatus: http.StatusOK,
+		},
+	}
 
-    for _, tc := range tests {
-        testCaseCheck(tc)
-    }
-} */
+	for _, tc := range tests {
+		testCaseCheck(tc)
+	}
+
+	// online debug without key-auth
+	basepath := "http://127.0.0.1:9000/apisix/admin/debug-request-forwarding"
+	request, _ := http.NewRequest("POST", basepath, strings.NewReader(`{"url": "http://172.16.238.30:9080/hello?name=aaa","method": "GET"}`))
+	request.Header.Add("Authorization", token)
+	resp, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	respBody, _ := ioutil.ReadAll(resp.Body)
+	realBody := gjson.Get(string(respBody), "data")
+	assert.Equal(t, `{"code":401,"message":"401 Unauthorized","data":{"message":"Missing authorization in request"}}`, realBody.String())
+}
