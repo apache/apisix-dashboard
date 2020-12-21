5a7bcd62281482ddb584fcbe6a760ea8596defd2
starsz <starszcan@gmail.com>
2020-12-21 16:23:49 +0800

    update: delete empty line

--- api/filter/authentication.go
+++ api/filter/authentication.go
@@ -30,7 +30,6 @@ import (
 func Authentication() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		if c.Request.URL.Path != "/apisix/admin/user/login" && strings.HasPrefix(c.Request.URL.Path, "/apisix") {
-
 			tokenStr := c.GetHeader("Authorization")
 
 			// verify token
