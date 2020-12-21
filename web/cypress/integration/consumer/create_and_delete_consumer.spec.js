26d1eee210818d2129672b7b9cafcfe3d6f11789
idbeta <idbeta@gmail.com>
2020-12-21 16:25:01 +0800

    test: modify indentation, modify method for access consumer page, add JSON.stringify

--- web/cypress/integration/consumer/create_and_delete_consumer.spec.js
+++ web/cypress/integration/consumer/create_and_delete_consumer.spec.js
@@ -17,72 +17,83 @@
 /* eslint-disable no-undef */
 
 context('Create and Delete Consumer', () => {
-        const root_name = `root_${new Date().valueOf()}`;
-      
-        beforeEach(() => {
-          // init login 
-          cy.login();
-        })
-      
-        it('create consumer with key-auth', () => {
-          // go to consumer create page
-          cy.visit('/consumer/create');
-          cy.contains('Consumer').click();
-          cy.wait(500);
-          cy.contains('Create').click();
+  const name = `consumerName${new Date().valueOf()}`;
+  const sleepTime = 100;
+  const domSelectors = {
+    notification: '.ant-notification-notice-message',
+    pluginsCard: '.ant-card'
+  };
 
-          // Basic Information
-          cy.get('#username').type('autotest_'+root_name);
-          cy.get('#desc').type('desc_by_autotest');
-          cy.contains('Next').click();
-          cy.wait(300);
+  beforeEach(() => {
+    // init login 
+    cy.login();
+  })
 
-          // Plugin Config
-          cy.contains('.ant-card', 'key-auth').within(($form) => {
-            cy.get('button').first().click();
-          })
-          // edit CodeMirror
-          cy.get('.CodeMirror')
-            .first()
-            .then((editor) => {
-              editor[0].CodeMirror.setValue('{"key":"test"}');
-              cy.contains('button', 'Submit').click();
-          });
-          cy.contains('button', 'Next').click();
-          cy.contains('button', 'Submit').click();
-        });
-      
-        it('delete the consumer', () => {
-          cy.visit('/consumer/list');
-          cy.contains(root_name).siblings().contains('Delete').click();
-          cy.contains('button', 'Confirm').click();
-          cy.get('.ant-notification-notice-message').should('contain', 'Delete Consumer Successfully');
-        });
+  it('create consumer with key-auth', () => {
+    // go to consumer create page
+    cy.visit('/');
+    cy.contains('Consumer').click();
+    cy.wait(sleepTime*5);
+    cy.contains('Create').click();
 
-        it('create consumer with wrong json', () => {
-          // go to consumer create page
-          cy.visit('/consumer/create');
-          cy.contains('Consumer').click();
-          cy.wait(500);
-          cy.contains('Create').click();
+    // Basic Information
+    cy.get('#username').type(name);
+    cy.get('#desc').type('desc_by_autotest');
+    cy.contains('Next').click();
+    cy.wait(sleepTime*3);
 
-          // Basic Information
-          cy.get('#username').type('autotest_'+root_name);
-          cy.get('#desc').type('desc_by_autotest');
-          cy.contains('Next').click();
-          cy.wait(300);
+    // Plugin Config
+    cy.contains(domSelectors.pluginsCard, 'key-auth').within(() => {
+      cy.get('button').first().click();
+    })
+    // edit CodeMirror
+    cy.get('.CodeMirror')
+      .first()
+      .then((editor) => {
+        editor[0].CodeMirror.setValue(JSON.stringify({
+          key: "test"
+        }));
+        cy.contains('button', 'Submit').click();
+      });
+    cy.contains('button', 'Next').click();
+    cy.contains('button', 'Submit').click();
+  });
 
-          // Plugin Config
-          cy.contains('.ant-card', 'key-auth').within(($form) => {
-            cy.get('button').first().click();
-          })
-          // edit CodeMirror
-          cy.get('.CodeMirror')
-            .first()
-            .then((editor) => {
-              editor[0].CodeMirror.setValue('{"key_not_exst":"test"}');
-              cy.contains('button', 'Submit').click();
-          });
-          cy.get('.ant-notification-notice-message').should('contain', 'Invalid plugin data');
-        });
-      })
+  it('delete the consumer', () => {
+    cy.visit('/');
+    cy.contains('Consumer').click();
+    cy.wait(sleepTime*5)
+    cy.contains(name).siblings().contains('Delete').click();
+    cy.contains('button', 'Confirm').click();
+    cy.get(domSelectors.notification).should('contain', 'Delete Consumer Successfully');
+  });
+
+  it('create consumer with wrong json', () => {
+    // go to consumer create page
+    cy.visit('/');
+    cy.contains('Consumer').click();
+    cy.wait(sleepTime*5);
+    cy.contains('Create').click();
+
+    // Basic Information
+    cy.get('#username').type(name);
+    cy.get('#desc').type('desc_by_autotest');
+    cy.contains('Next').click();
+    cy.wait(sleepTime*3);
+
+    // Plugin Config
+    cy.contains(domSelectors.pluginsCard, 'key-auth').within(($form) => {
+      cy.get('button').first().click();
+    })
+    // edit CodeMirror
+    cy.get('.CodeMirror')
+      .first()
+      .then((editor) => {
+        editor[0].CodeMirror.setValue(JSON.stringify({
+          key_not_exst: "test"
+        }));
+        cy.contains('button', 'Submit').click();
+    });
+    cy.get(domSelectors.notification).should('contain', 'Invalid plugin data');
+  });
+})
