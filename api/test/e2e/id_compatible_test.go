fb1d4856fc51e11811d951f2fb8a1a4175da3c2c
nic-chen <johz@163.com>
2020-12-25 11:06:14 +0800

    test: add test cases for create route by POST

--- api/test/e2e/id_compatible_test.go
+++ api/test/e2e/id_compatible_test.go
@@ -17,8 +17,14 @@
 package e2e
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/tidwall/gjson"
 )
 
 func TestID_Using_Int(t *testing.T) {
@@ -401,9 +407,69 @@ func TestID_Not_In_Body(t *testing.T) {
 			ExpectStatus: http.StatusNotFound,
 			Sleep:        sleepTime,
 		},
+		{
+			Desc:   "create route that has no ID in request body by admin api (POST)",
+			Object: APISIXExpect(t),
+			Method: http.MethodPost,
+			Path:   "/apisix/admin/routes",
+			Body: `{
+				"uri": "/hello",
+				"upstream": {
+					"type": "roundrobin",
+					"nodes": {
+						"172.16.238.20:1980": 1
+					}
+				}
+			}`,
+			Headers:      map[string]string{"X-API-KEY": "edd1c9f034335f136f87ad84b625c8f1"},
+			ExpectStatus: http.StatusCreated,
+			Sleep:        sleepTime,
+		},
+		{
+			Desc:         "verify that the route is available for manager api",
+			Object:       ManagerApiExpect(t),
+			Method:       http.MethodGet,
+			Path:         "/apisix/admin/routes",
+			Headers:      map[string]string{"Authorization": token},
+			ExpectStatus: http.StatusOK,
+			ExpectBody:   `"uri":"/hello"`,
+			Sleep:        sleepTime,
+		},
+		{
+			Desc:         "hit the route just created",
+			Object:       APISIXExpect(t),
+			Method:       http.MethodGet,
+			Path:         "/hello",
+			ExpectStatus: http.StatusOK,
+			ExpectBody:   "hello world",
+			Sleep:        sleepTime,
+		},
 	}
 
 	for _, tc := range tests {
+		testCaseCheck(tc, t)
+	}
+
+	// delete the route created by POST
+	time.Sleep(time.Duration(100) * time.Millisecond)
+	request, _ := http.NewRequest("GET", ManagerAPIHost+"/apisix/admin/routes", nil)
+	request.Header.Add("Authorization", token)
+	resp, err := http.DefaultClient.Do(request)
+	assert.Nil(t, err)
+	defer resp.Body.Close()
+	respBody, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("string(respBody)", string(respBody))
+	list := gjson.Get(string(respBody), "data.rows").Value().([]interface{})
+	for _, item := range list {
+		route := item.(map[string]interface{})
+		tc := HttpTestCase{
+			Desc:         "delete the route",
+			Object:       ManagerApiExpect(t),
+			Method:       http.MethodDelete,
+			Path:         "/apisix/admin/routes/" + route["id"].(string),
+			Headers:      map[string]string{"Authorization": token},
+			ExpectStatus: http.StatusOK,
+		}
 		testCaseCheck(tc, t)
 	}
 }
