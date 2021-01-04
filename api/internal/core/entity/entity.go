4fffe6125ea88e097e968930e626889df15f306f
nic-chen <33000667+nic-chen@users.noreply.github.com>
2021-01-04 16:48:07 +0800

    fix: allow empty plugins for global rule (#1180)
    Co-authored-by: 琚致远 <juzhiyuan@apache.org>

--- api/internal/core/entity/entity.go
+++ api/internal/core/entity/entity.go
@@ -238,7 +238,7 @@ type Script struct {
 // swagger:model GlobalPlugins
 type GlobalPlugins struct {
 	ID      interface{}            `json:"id"`
-	Plugins map[string]interface{} `json:"plugins,omitempty"`
+	Plugins map[string]interface{} `json:"plugins"`
 }
 
 type ServerInfo struct {
