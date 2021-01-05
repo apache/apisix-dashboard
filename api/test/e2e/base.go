63740638abcb5161437fc6c3f73cb4d00c9f6b77
nic-chen <33000667+nic-chen@users.noreply.github.com>
2021-01-05 16:07:29 +0800

    chore: sync json schema from Apache APISIX 2.2 (#1177)
    * chore: sync json schema from Apache APISIX 2.2
    
    * fix: remove schema of plugins that not enable by default
    
    * fix test cases for plugin skywalking which is not enable by default
    
    * chore: expose port for control API
    
    * fix: control API config
    
    * fix yaml format
    
    * fix CI failed
    
    * fix: log path
    
    * fix: log path
    
    Co-authored-by: 琚致远 <juzhiyuan@apache.org>

--- api/test/e2e/base.go
+++ api/test/e2e/base.go
@@ -20,10 +20,12 @@ import (
 	"bytes"
 	"context"
 	"crypto/tls"
-
+	"fmt"
 	"io/ioutil"
 	"net"
 	"net/http"
+	"os/exec"
+	"strings"
 	"testing"
 	"time"
 
@@ -32,12 +34,15 @@ import (
 	"github.com/tidwall/gjson"
 )
 
-var token string
+var (
+	token string
+	Token string
 
-var APISIXHost = "http://127.0.0.1:9080"
-var APISIXInternalUrl = "http://172.16.238.30:9080"
-var APISIXSingleWorkerHost = "http://127.0.0.1:9081"
-var ManagerAPIHost = "http://127.0.0.1:9000"
+	APISIXHost             = "http://127.0.0.1:9080"
+	APISIXInternalUrl      = "http://172.16.238.30:9080"
+	APISIXSingleWorkerHost = "http://127.0.0.1:9081"
+	ManagerAPIHost         = "http://127.0.0.1:9000"
+)
 
 func init() {
 	//login to get auth token
@@ -67,6 +72,7 @@ func init() {
 
 	respond := gjson.ParseBytes(body)
 	token = respond.Get("data.token").String()
+	Token = token
 }
 
 func httpGet(url string) ([]byte, int, error) {
@@ -245,4 +251,35 @@ func testCaseCheck(tc HttpTestCase, t *testing.T) {
 		}
 
 	})
+}
+
+func RunTestCases(tc HttpTestCase, t *testing.T) {
+	testCaseCheck(tc, t)
+}
+
+func ReadAPISIXErrorLog(t *testing.T) string {
+	cmd := exec.Command("pwd")
+	pwdByte, err := cmd.CombinedOutput()
+	pwd := string(pwdByte)
+	pwd = strings.Replace(pwd, "\n", "", 1)
+	pwd = pwd[:strings.Index(pwd, "/e2e")]
+	bytes, err := ioutil.ReadFile(pwd + "/docker/apisix_logs/error.log")
+	assert.Nil(t, err)
+	logContent := string(bytes)
+
+	return logContent
+}
+
+func CleanAPISIXErrorLog(t *testing.T) {
+	cmd := exec.Command("pwd")
+	pwdByte, err := cmd.CombinedOutput()
+	pwd := string(pwdByte)
+	pwd = strings.Replace(pwd, "\n", "", 1)
+	pwd = pwd[:strings.Index(pwd, "/e2e")]
+	cmd = exec.Command("sudo", "echo", " > ", pwd + "/docker/apisix_logs/error.log")
+	_, err = cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("cmd error:", err.Error())
+	}
+	assert.Nil(t, err)
 }
