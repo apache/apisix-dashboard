fa9ed4a22c452e9ebd1fb31d47f3121b6610658d
nic-chen <johz@163.com>
2021-01-11 00:51:46 +0800

    test: add test case

---
+++ api/test/e2e/import_test.go
@@ -0,0 +1,91 @@
+package e2e
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/tidwall/gjson"
+)
+
+func TestImport_default(t *testing.T) {
+	path, err := filepath.Abs("../testdata/import-test-default.yaml")
+	assert.Nil(t, err)
+
+	headers := map[string]string{
+		"Authorization": token,
+	}
+	files := []UploadFile{
+		{Name: "file", Filepath: path},
+	}
+	PostFile(ManagerAPIHost+"/apisix/admin/import", nil, files, headers)
+
+	request, _ := http.NewRequest("GET", ManagerAPIHost+"/apisix/admin/routes", nil)
+	request.Header.Add("Authorization", token)
+	resp, err := http.DefaultClient.Do(request)
+	assert.Nil(t, err)
+	defer resp.Body.Close()
+	respBody, _ := ioutil.ReadAll(resp.Body)
+	list := gjson.Get(string(respBody), "data.rows").Value().([]interface{})
+
+	fmt.Println("list:", string(respBody))
+
+	tests := []HttpTestCase{}
+	for _, item := range list {
+		route := item.(map[string]interface{})
+		tc := HttpTestCase{
+			Desc:         "route patch for update status(online)",
+			Object:       ManagerApiExpect(t),
+			Method:       http.MethodPatch,
+			Path:         "/apisix/admin/routes/" + route["id"].(string),
+			Body:         `{"status":1}`,
+			Headers:      map[string]string{"Authorization": token},
+			ExpectStatus: http.StatusOK,
+			Sleep:        sleepTime,
+		}
+		tests = append(tests, tc)
+	}
+
+	tests = append(tests, HttpTestCase{
+		Desc:         "verify the route just imported",
+		Object:       APISIXExpect(t),
+		Method:       http.MethodGet,
+		Path:         "/hello",
+		ExpectStatus: http.StatusOK,
+		ExpectBody:   "hello world",
+		Sleep:        sleepTime,
+	})
+
+	for _, tc := range tests {
+		testCaseCheck(tc, t)
+	}
+
+}
+
+func TestImport_delete_test_data(t *testing.T) {
+	// delete the route created by POST
+	time.Sleep(time.Duration(100) * time.Millisecond)
+	request, _ := http.NewRequest("GET", ManagerAPIHost+"/apisix/admin/routes", nil)
+	request.Header.Add("Authorization", token)
+	resp, err := http.DefaultClient.Do(request)
+	assert.Nil(t, err)
+	defer resp.Body.Close()
+	respBody, _ := ioutil.ReadAll(resp.Body)
+	list := gjson.Get(string(respBody), "data.rows").Value().([]interface{})
+	for _, item := range list {
+		route := item.(map[string]interface{})
+		tc := HttpTestCase{
+			Desc:         "delete the route",
+			Object:       ManagerApiExpect(t),
+			Method:       http.MethodDelete,
+			Path:         "/apisix/admin/routes/" + route["id"].(string),
+			Headers:      map[string]string{"Authorization": token},
+			ExpectStatus: http.StatusOK,
+		}
+		testCaseCheck(tc, t)
+	}
+}
