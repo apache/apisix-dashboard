80b9bf58e11f844cb7019b4535e7e67c37cec072
ShiningRush <277040271@qq.com>
2021-01-11 22:12:47 +0800

    chore:add e2e test coverage

---
+++ api/cmd/manager/main_test.go
@@ -0,0 +1,35 @@
+package main
+import (
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"testing"
+)
+func TestMain(m *testing.M) {
+	var (
+		args []string
+	)
+	for _, arg := range os.Args {
+		switch {
+		case strings.HasPrefix(arg, "DEVEL"):
+		case strings.HasPrefix(arg, "-test"):
+		default:
+			args = append(args, arg)
+		}
+	}
+	waitCh := make(chan int, 1)
+	os.Args = args
+	go func() {
+		main()
+		close(waitCh)
+	}()
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGHUP)
+	select {
+	case <-signalCh:
+		return
+	case <-waitCh:
+		return
+	}
+}
