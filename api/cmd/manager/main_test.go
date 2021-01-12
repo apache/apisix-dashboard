8a8e6a2408adfd552f03c699480bf787402b5de0
ShiningRush <277040271@qq.com>
2021-01-12 17:08:22 +0800

    fix: skip test main when unit testing

--- api/cmd/manager/main_test.go
+++ api/cmd/manager/main_test.go
@@ -1,3 +1,5 @@
+// +build
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -23,6 +25,10 @@ import (
 	"testing"
 )
 func TestMainWrapper(t *testing.T) {
+	if os.Getenv("ENV") == "test" {
+		t.Skip("skipping build binary when execute unit test")
+	}
+
 	var (
 		args []string
 	)
