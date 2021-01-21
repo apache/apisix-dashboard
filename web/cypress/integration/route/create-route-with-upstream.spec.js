2b62ca1c46c5729344072ba2672c04ff554cc05e
琚致远 <juzhiyuan@apache.org>
2021-01-21 22:42:05 +0800

    Update create-route-with-upstream.spec.js

--- web/cypress/integration/route/create-route-with-upstream.spec.js
+++ web/cypress/integration/route/create-route-with-upstream.spec.js
@@ -92,7 +92,6 @@ context('Create Route with Upstream', () => {
     cy.contains('Search').click();
     cy.contains(data.routeName).siblings().contains('Edit').click();
 
-    // fetching Resource
     cy.get(domSelector.name).click().should('value', data.routeName);
     cy.contains('Next').click();
 
