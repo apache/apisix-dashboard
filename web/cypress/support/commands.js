ab5b8699bbcff566726b86cd9f2a9960ca3be47a
guoqqqi <979918879@qq.com>
2021-01-24 20:47:55 +0800

    test: update code

--- web/cypress/support/commands.js
+++ web/cypress/support/commands.js
@@ -65,8 +65,7 @@ Cypress.Commands.add('configurePlugins', (cases) => {
           });
 
         // NOTE: wait for the Drawer to appear on the DOM
-        cy.get({ timeout });
-        cy.get(domSelectors.drawer).within(() => {
+        cy.get(domSelectors.drawer, { timeout }).within(() => {
           cy.get(domSelectors.switch).click({
             force: true,
             timeout,
