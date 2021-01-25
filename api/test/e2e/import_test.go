8f60f0255fc0729597c314db34627109c14a168d
nic-chen <johz@163.com>
2021-01-25 14:58:10 +0800

    test: more test cases

--- api/test/e2e/import_test.go
+++ api/test/e2e/import_test.go
@@ -17,13 +17,14 @@
 package e2e
 
 import (
-	"github.com/stretchr/testify/assert"
-	"github.com/tidwall/gjson"
 	"io/ioutil"
 	"net/http"
 	"path/filepath"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/tidwall/gjson"
 )
 
 func TestImport_default(t *testing.T) {
@@ -235,6 +236,124 @@ func TestImport_with_plugins(t *testing.T) {
 			Body:         `{"status": "1"}`,
 			ExpectStatus: http.StatusUnauthorized,
 			ExpectBody:   `{"message":"Missing authorization in request"}`,
+			Sleep:        sleepTime,
+		},
+	}
+	tests = append(tests, verifyTests...)
+
+	// delete test data
+	for _, item := range list {
+		route := item.(map[string]interface{})
+		tc := HttpTestCase{
+			Desc:         "delete route",
+			Object:       ManagerApiExpect(t),
+			Method:       http.MethodDelete,
+			Path:         "/apisix/admin/routes/" + route["id"].(string),
+			Headers:      map[string]string{"Authorization": token},
+			ExpectStatus: http.StatusOK,
+		}
+		tests = append(tests, tc)
+	}
+
+	for _, tc := range tests {
+		testCaseCheck(tc, t)
+	}
+}
+
+func TestImport_with_multi_routes(t *testing.T) {
+	path, err := filepath.Abs("../testdata/import/multi-routes.yaml")
+	assert.Nil(t, err)
+
+	headers := map[string]string{
+		"Authorization": token,
+	}
+	files := []UploadFile{
+		{Name: "file", Filepath: path},
+	}
+	PostFile(ManagerAPIHost+"/apisix/admin/import", nil, files, headers)
+
+	// sleep for data sync
+	time.Sleep(sleepTime)
+
+	request, _ := http.NewRequest("GET", ManagerAPIHost+"/apisix/admin/routes", nil)
+	request.Header.Add("Authorization", token)
+	resp, err := http.DefaultClient.Do(request)
+	assert.Nil(t, err)
+	defer resp.Body.Close()
+	respBody, _ := ioutil.ReadAll(resp.Body)
+	list := gjson.Get(string(respBody), "data.rows").Value().([]interface{})
+
+	assert.Equal(t, 2, len(list))
+
+	var tests []HttpTestCase
+	for _, item := range list {
+		route := item.(map[string]interface{})
+		tc := HttpTestCase{
+			Desc:         "route patch for update status(online)",
+			Object:       ManagerApiExpect(t),
+			Method:       http.MethodPatch,
+			Path:         "/apisix/admin/routes/" + route["id"].(string),
+			Body:         `{"status":1}`,
+			Headers:      map[string]string{"Authorization": token},
+			ExpectStatus: http.StatusOK,
+			Sleep:        sleepTime,
+		}
+		tests = append(tests, tc)
+
+		// verify route data
+		if route["uri"].(string) == "/get" {
+			tcDataVerify := HttpTestCase{
+				Desc:         "verify data of route",
+				Object:       ManagerApiExpect(t),
+				Method:       http.MethodGet,
+				Path:         "/apisix/admin/routes/" + route["id"].(string),
+				Headers:      map[string]string{"Authorization": token},
+				ExpectStatus: http.StatusOK,
+				ExpectBody: []string{`"methods":["GET","POST","HEAD","PUT","PATCH","DELETE"]`,
+					`"proxy-rewrite":{"disable":false,"scheme":"https"}`,
+					`"labels":{"API_VERSION":"v2","dev":"test"}`,
+					`"upstream":{"nodes":[{"host":"httpbin.org","port":443,"weight":1}],"timeout":{"connect":6000,"read":6000,"send":6000},"type":"roundrobin","pass_host":"node"}`,
+				},
+				Sleep: sleepTime,
+			}
+			tests = append(tests, tcDataVerify)
+		} else {
+			tcDataVerify := HttpTestCase{
+				Desc:         "verify data of route2",
+				Object:       ManagerApiExpect(t),
+				Method:       http.MethodGet,
+				Path:         "/apisix/admin/routes/" + route["id"].(string),
+				Headers:      map[string]string{"Authorization": token},
+				ExpectStatus: http.StatusOK,
+				ExpectBody: []string{`"methods":["POST"]`,
+					`"proxy-rewrite":{"disable":false,"scheme":"https"}`,
+					`"labels":{"API_VERSION":"v1","version":"v1"}`,
+					`"upstream":{"nodes":[{"host":"httpbin.org","port":443,"weight":1}],"timeout":{"connect":6000,"read":6000,"send":6000},"type":"roundrobin","pass_host":"node"}`,
+				},
+				Sleep: sleepTime,
+			}
+			tests = append(tests, tcDataVerify)
+		}
+	}
+
+	// verify route
+	verifyTests := []HttpTestCase{
+		{
+			Desc:         "verify the route just imported",
+			Object:       APISIXExpect(t),
+			Method:       http.MethodPost,
+			Path:         "/get",
+			ExpectStatus: http.StatusOK,
+			ExpectBody:   `"url": "https://127.0.0.1/get"`,
+			Sleep:        sleepTime,
+		},
+		{
+			Desc:         "verify the route just imported",
+			Object:       APISIXExpect(t),
+			Method:       http.MethodPost,
+			Path:         "/post",
+			ExpectStatus: http.StatusOK,
+			ExpectBody:   `"url": "https://127.0.0.1/post"`,
 			Sleep:        sleepTime,
 		},
 	}
