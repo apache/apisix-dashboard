a396a8cec356946c99bd22fb4bce3d7b10e9cdaa
Jaycean <jinchen_jacean@163.com>
2021-01-25 12:33:00 +0800

    Add unit test

--- api/internal/handler/data_loader/route_export_test.go
+++ api/internal/handler/data_loader/route_export_test.go
@@ -29,6 +29,7 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
+// 1.Export data as the route of URIs Hosts
 func TestExportRoutes1(t *testing.T) {
 	input := &ExportInput{IDs: "1"}
 	//*entity.Route
@@ -156,6 +157,7 @@ func TestExportRoutes1(t *testing.T) {
 	assert.NotNil(t, ret1)
 }
 
+// 2.Export data as the route of URI host
 func TestExportRoutes2(t *testing.T) {
 	input := &ExportInput{IDs: "1"}
 	//*entity.Route
@@ -283,6 +285,7 @@ func TestExportRoutes2(t *testing.T) {
 	assert.NotNil(t, ret1)
 }
 
+// 3.Create a service that contains complete data and use the service_id create route
 func TestExportRoutesCreateByServiceId(t *testing.T) {
 	input := &ExportInput{IDs: "1"}
 	//*entity.Route
@@ -389,6 +392,7 @@ func TestExportRoutesCreateByServiceId(t *testing.T) {
 	assert.NotNil(t, _ret)
 }
 
+// 4.Create a service containing plugin and a route containing plugin to test the fusion of exported data
 func TestExportRoutesCreateByServiceId2(t *testing.T) {
 	input := &ExportInput{IDs: "1"}
 	//*entity.Route
@@ -513,6 +517,7 @@ func TestExportRoutesCreateByServiceId2(t *testing.T) {
 	assert.NotNil(t, _ret)
 }
 
+// 5.Create a service according to the upstream ID and a route according to the service ID
 func TestExportRoutesCreateByServiceId3(t *testing.T) {
 	input := &ExportInput{IDs: "1"}
 	us := `{
@@ -649,6 +654,7 @@ func TestExportRoutesCreateByServiceId3(t *testing.T) {
 	assert.NotNil(t, _ret)
 }
 
+// 6.Create and export route according to upstream ID
 func TestExportRoutesCreateByUpstreamId(t *testing.T) {
 	input := &ExportInput{IDs: "1"}
 	us := `{
@@ -740,6 +746,7 @@ func TestExportRoutesCreateByUpstreamId(t *testing.T) {
 	assert.NotNil(t, _ret)
 }
 
+// 7.Create route according to upstream ID and service ID
 func TestExportRoutesCreateByUpstreamIdandServiceId(t *testing.T) {
 	input := &ExportInput{IDs: "1"}
 	us := `{
@@ -884,6 +891,7 @@ func TestExportRoutesCreateByUpstreamIdandServiceId(t *testing.T) {
 	assert.NotNil(t, _ret)
 }
 
+// 8.Creating route using service ID does not contain upstream data
 func TestExportRoutesCreateByServiceIdNoUpstream(t *testing.T) {
 	input := &ExportInput{IDs: "1"}
 	us := `{
@@ -1007,6 +1015,8 @@ func TestExportRoutesCreateByServiceIdNoUpstream(t *testing.T) {
 	assert.NotNil(t, _ret)
 }
 
+// 9.Create a service with label data and a route with label data, and export the route.
+// Label is the original data of the route
 func TestExportRoutesCreateByLabel(t *testing.T) {
 	input := &ExportInput{IDs: "1"}
 	s := `{
@@ -1099,6 +1109,8 @@ func TestExportRoutesCreateByLabel(t *testing.T) {
 	assert.NotNil(t, _ret)
 }
 
+// 10.Create a service with label data and a route without label data, and export the route.
+//  Label is the data of the service
 func TestExportRoutesCreateByLabel2(t *testing.T) {
 	input := &ExportInput{IDs: "1"}
 	s := `{
@@ -1183,6 +1195,7 @@ func TestExportRoutesCreateByLabel2(t *testing.T) {
 	assert.NotNil(t, _ret)
 }
 
+// 11.Test export route request_ validation data correctness
 func TestExportRoutesCreateByRequestValidation(t *testing.T) {
 	input := &ExportInput{IDs: "1"}
 	r := `{
@@ -1292,6 +1305,7 @@ func TestExportRoutesCreateByRequestValidation(t *testing.T) {
 	assert.NotNil(t, _ret)
 }
 
+// 12.Export route create by jwt-auth plugin
 func TestExportRoutesCreateByJWTAuth(t *testing.T) {
 	input := &ExportInput{IDs: "1"}
 	r := `{
@@ -1367,6 +1381,7 @@ func TestExportRoutesCreateByJWTAuth(t *testing.T) {
 	assert.NotNil(t, _ret)
 }
 
+// 13.Export route create by apikey-auth plugin  basic-auth plugin
 func TestExportRoutesCreateByKeyAuthAndBasicAuth(t *testing.T) {
 	input := &ExportInput{IDs: "1"}
 	r := `{
@@ -1450,10 +1465,294 @@ func TestExportRoutesCreateByKeyAuthAndBasicAuth(t *testing.T) {
 	assert.NotNil(t, _ret)
 }
 
+// 14.Export all routes
+func TestExportRoutesAll(t *testing.T) {
+	input := &store.ListInput{}
+	//*entity.Route
+	r1 := `{
+		"name": "aaaa",
+		"status": 1,
+		"uri": "/hello_",
+		"host":  "*.bar.com",
+		"methods": [ "POST"],
+		"upstream": {
+			"nodes": {
+				"172.16.238.20:1980": 1
+			},
+			"type": "roundrobin"
+		}
+	}`
+
+	r2 := `{
+		"name": "aaaa2",
+		"status": 1,
+		"uris": ["/hello_2"],
+		"hosts": ["foo.com", "*.bar.com"],
+		"methods": ["GET"],
+		"upstream": {
+			"nodes": {
+				"172.16.238.20:1980": 1
+			},
+			"type": "roundrobin"
+		}
+	}`
+
+	exportR1 := `{
+		"components": {},
+		"info": {
+			"title": "RoutesExport",
+			"version": "3.0.0"
+		},
+		"openapi": "3.0.0",
+		"paths": {
+			"/hello_": {
+				"post": {
+					"operationId": "aaaaPOST",
+					"requestBody": {},
+					"responses": {
+						"default": {
+							"description": ""
+						}
+					},
+					"x-apisix-enable_websocket": false,
+					"x-apisix-host": "*.bar.com",
+					"x-apisix-plugins": {},
+					"x-apisix-priority": 0,
+					"x-apisix-status": 1,
+					"x-apisix-upstream": {
+						"nodes": {
+							"172.16.238.20:1980": 1
+						},
+						"type": "roundrobin"
+					}
+				}
+			},
+			"/hello_2": {
+				"get": {
+					"operationId": "aaaa2GET",
+					"requestBody": {},
+					"responses": {
+						"default": {
+							"description": ""
+						}
+					},
+					"x-apisix-enable_websocket": false,
+					"x-apisix-hosts": ["foo.com", "*.bar.com"],
+					"x-apisix-plugins": {},
+					"x-apisix-priority": 0,
+					"x-apisix-status": 1,
+					"x-apisix-upstream": {
+						"nodes": {
+							"172.16.238.20:1980": 1
+						},
+						"type": "roundrobin"
+					}
+				}
+			}
+		}
+	}`
+	var route *entity.Route
+	var route2 *entity.Route
+	var routes []*entity.Route
+	err := json.Unmarshal([]byte(r1), &route)
+	err = json.Unmarshal([]byte(r2), &route2)
+	mStore := &store.MockInterface{}
+	getCalled := false
+
+	routes = append(routes, route)
+	routes = append(routes, route2)
+
+	mStore.On("List", mock.Anything).Run(func(args mock.Arguments) {
+		getCalled = true
+	}).Return(func(input store.ListInput) *store.ListOutput {
+		var returnData []interface{}
+		for _, c := range routes {
+			returnData = append(returnData, c)
+		}
+		return &store.ListOutput{
+			Rows:      returnData,
+			TotalSize: len(returnData),
+		}
+	}, nil)
+
+	h := Handler{routeStore: mStore}
+	ctx := droplet.NewContext()
+	ctx.SetInput(input)
+
+	ret, err := h.ExportAllRoutes(ctx)
+	assert.Nil(t, err)
+	ret1, err := json.Marshal(ret)
+	if err != nil {
+	}
+	assert.Equal(t, replaceStr(exportR1), string(ret1))
+	assert.NotNil(t, ret1)
+	assert.True(t, getCalled)
+}
+
+//15.Create service according to upstream1 ID
+// Create route according to upstream2 ID and service ID
+func TestExportRoutesCreateByUpstreamIDAndServiceID2(t *testing.T) {
+	input := &ExportInput{IDs: "1"}
+	us := `{
+		"id": "u1",
+		"nodes": [
+			{
+				"host": "172.16.238.20",
+				"port": 1980,
+				"weight": 1
+			}
+		],
+		"type": "roundrobin"
+	}`
+
+	us2 := `{
+		"id": "u2",
+		"nodes": [
+			{
+				"host": "172.16.238.20",
+				"port": 1981,
+				"weight": 1
+			}
+		],
+		"type": "roundrobin"
+	}`
+
+	s := `{
+		"id": "s1",
+		"name": "testservice",
+		"desc": "testservice_desc",
+		"enable_websocket":true,
+		"upstream_id": "u2"
+	}`
+
+	r := `{
+		"name": "route_all",
+		"desc": "所有",
+		"status": 1,
+		"methods": ["GET"],
+		"priority": 0,
+		"service_id": "s1",
+		"labels": {
+			"test": "1",
+			"API_VERSION": "v1"
+		},
+		"vars": [
+			["arg_name", "==", "test"]
+		],
+		"uri": "/hello",
+		"enable_websocket":false,
+		"plugins": { 
+			"prometheus": {
+				"disable": false
+			}
+		},
+		"upstream_id": "u1"
+	}`
+
+	exportR := `{
+		"components": {},
+		"info": {
+			"title": "RoutesExport",
+			"version": "3.0.0"
+		},
+		"openapi": "3.0.0",
+		"paths": {
+			"/hello": {
+				"get": {
+					"operationId": "route_allGET",
+					"requestBody": {},
+					"responses": {
+						"default": {
+							"description": ""
+						}
+					},
+					"security": [],
+					"summary": "所有",
+					"x-apisix-enable_websocket": false,
+					"x-apisix-labels": {
+						"API_VERSION": "v1",
+						"test": "1"
+					},
+					"x-apisix-plugins": {
+						"prometheus": {
+							"disable": false
+						}
+					},
+					"x-apisix-priority": 0,
+					"x-apisix-status": 1,
+					"x-apisix-upstream": {
+						"id": "u1",
+						"nodes": [{
+							"host": "172.16.238.20",
+							"port": 1980,
+							"weight": 1
+						}],
+						"type": "roundrobin"
+					},
+					"x-apisix-vars": [
+						["arg_name", "==", "test"]
+					]
+				}
+			}
+		}
+	}`
+	var route *entity.Route
+	var service *entity.Service
+	var upstream *entity.Upstream
+	var upstream2 *entity.Upstream
+	var upstreams []*entity.Upstream
+
+	err := json.Unmarshal([]byte(r), &route)
+	err = json.Unmarshal([]byte(s), &service)
+	err = json.Unmarshal([]byte(us), &upstream)
+	err = json.Unmarshal([]byte(us2), &upstream2)
+
+	mStore := &store.MockInterface{}
+	mStore.On("Get", mock.Anything).Run(func(args mock.Arguments) {
+	}).Return(route, nil)
+
+	upstreams = append(upstreams, upstream)
+	upstreams = append(upstreams, upstream2)
+	getCalled := true
+
+	mStoreUpstream := &store.MockInterface{}
+	mStoreUpstream.On("Get", mock.Anything).Run(func(args mock.Arguments) {
+	}).Return(upstream, nil)
+
+	mStoreUpstream.On("List", mock.Anything).Run(func(args mock.Arguments) {
+		getCalled = true
+	}).Return(func(input store.ListInput) *store.ListOutput {
+		var returnData []interface{}
+		for _, c := range upstreams {
+			returnData = append(returnData, c)
+		}
+		return &store.ListOutput{
+			Rows:      returnData,
+			TotalSize: len(returnData),
+		}
+	}, nil)
+
+	mStoreService := &store.MockInterface{}
+	mStoreService.On("Get", mock.Anything).Run(func(args mock.Arguments) {
+	}).Return(service, nil)
+
+	h := Handler{routeStore: mStore, upstreamStore: mStoreUpstream, serviceStore: mStoreService}
+	ctx := droplet.NewContext()
+	ctx.SetInput(input)
+
+	ret, err := h.ExportRoutes(ctx)
+	assert.Nil(t, err)
+	_ret, err := json.Marshal(ret)
+	if err != nil {
+	}
+	assert.Equal(t, replaceStr(exportR), string(_ret))
+	assert.NotNil(t, _ret)
+	assert.True(t, getCalled)
+}
+
 func replaceStr(str string) string {
 	str = strings.Replace(str, "\n", "", -1)
 	str = strings.Replace(str, "\t", "", -1)
 	str = strings.Replace(str, " ", "", -1)
 	return str
 }
-
