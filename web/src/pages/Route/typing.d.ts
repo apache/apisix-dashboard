25e74768f7378ea5515c44831201d38d6a30ec1b
guoqqqi <979918879@qq.com>
2021-01-27 21:49:02 +0800

    feat: route vars support IN

--- web/src/pages/Route/typing.d.ts
+++ web/src/pages/Route/typing.d.ts
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 declare namespace RouteModule {
-  type Operator = '==' | '~=' | '>' | '<' | '~~';
+  type Operator = '==' | '~=' | '>' | '<' | '~~' | 'IN';
 
   type VarPosition = 'arg' | 'http' | 'cookie';
 
