4700d6f483d880eb9b19253f24903ddc6adbc09f
guoqqqi <72343596+guoqqqi@users.noreply.github.com>
2021-01-28 22:42:51 +0800

    docs: remove commit-convention.md (#1382)

--- web/scripts/verifyCommit.js
+++ web/scripts/verifyCommit.js
@@ -33,12 +33,11 @@ if (!commitRE.test(msg)) {
   console.log();
   console.error(
     `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(`invalid commit message format.`)}\n\n` +
-      chalk.red(
-        `  Proper commit message format is required for automated changelog generation. Examples:\n\n`,
-      ) +
-      `    ${chalk.green(`feat(compiler): add 'comments' option`)}\n` +
-      `    ${chalk.green(`fix(v-model): handle events on blur (close #28)`)}\n\n` +
-      chalk.red(`  See .github/commit-convention.md for more details.\n`),
+    chalk.red(
+      `  Proper commit message format is required for automated changelog generation. Examples:\n\n`,
+    ) +
+    `    ${chalk.green(`feat(compiler): add 'comments' option`)}\n` +
+    `    ${chalk.green(`fix(v-model): handle events on blur (close #28)`)}\n\n`
   );
   process.exit(1);
 }
