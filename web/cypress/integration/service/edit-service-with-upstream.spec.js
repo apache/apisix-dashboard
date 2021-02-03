3e36a9a5cc35356b8356f2ac768126344f063ac0
guoqqqi <979918879@qq.com>
2021-02-03 19:29:09 +0800

    fix: update code

--- web/cypress/integration/service/edit-service-with-upstream.spec.js
+++ web/cypress/integration/service/edit-service-with-upstream.spec.js
@@ -16,14 +16,14 @@
  */
 /* eslint-disable no-undef */
 
-context('edit service with upstream', () => {
+context('Edit Service with Upstream', () => {
   const domSelector = {
     name: '#name',
     desc: '#desc',
     nodes_0_host: '#nodes_0_host',
     notification: '.ant-notification-notice-message',
     search_name: '[title=Name]',
-    custom_upstream: '[title=Custom]',
+    upstream_selector: '[title=Custom]',
     test_upstream: '[title=test_upstream]',
     input: ':input',
   };
@@ -32,8 +32,10 @@ context('edit service with upstream', () => {
     test_upstream: 'test_upstream',
     desc: 'desc',
     ip: '127.0.0.1',
+    ip2: '127.0.0.2',
     create_service_success: 'Create Service Successfully',
     create_upstream_success: 'Create Upstream Successfully',
+    edit_service_success: 'Edit Service Successfully',
     delete_service_success: 'Delete Service Successfully',
     delete_upstream_success: 'Delete Upstream Successfully',
   };
@@ -42,7 +44,7 @@ context('edit service with upstream', () => {
     cy.login();
   });
 
-  it('should create test upstream', () => {
+  it('should create a test upstream', () => {
     cy.visit('/');
     cy.contains('Upstream').click();
     cy.contains('Create').click();
@@ -55,13 +57,13 @@ context('edit service with upstream', () => {
     cy.url().should('contains', 'upstream/list');
   });
 
-  it('should create test service', () => {
+  it('should create a test service', () => {
     cy.visit('/');
     cy.contains('Service').click();
     cy.contains('Create').click();
     cy.get(domSelector.name).type(data.service_name);
     cy.get(domSelector.desc).type(data.desc);
-    cy.get(domSelector.custom_upstream).click();
+    cy.get(domSelector.upstream_selector).click();
     cy.contains(data.test_upstream).click();
     cy.get(domSelector.input).should('be.disabled');
 
@@ -79,15 +81,19 @@ context('edit service with upstream', () => {
     cy.contains('Search').click();
     cy.contains(data.service_name).siblings().contains('Edit').click();
 
-    cy.get(domSelector.nodes_0_host).click().should('value', data.ip);
+    cy.get(domSelector.nodes_0_host).click({ force: true }).should('value', data.ip);
     cy.get(domSelector.input).should('be.disabled');
 
     cy.get(domSelector.test_upstream).click();
     cy.contains('Custom').click();
-    cy.get(domSelector.nodes_0_host).should('not.be.disabled');
+    cy.get(domSelector.nodes_0_host).should('not.be.disabled').clear().type(data.ip2);
+    cy.contains('Next').click();
+    cy.contains('Next').click();
+    cy.contains('Submit').click();
+    cy.get(domSelector.notification).should('contain', data.edit_service_success);
   });
 
-  it('should delete service and upstream', () => {
+  it('should delete this service and upstream', () => {
     cy.visit('/service/list');
     cy.get(domSelector.search_name).type(data.service_name);
     cy.contains('Search').click();
