19a388cc31630b9b6a62a0a936d1c97587a7aaaf
guoqqqi <979918879@qq.com>
2021-02-04 15:44:26 +0800

    fix: Frontend e2e test

--- web/cypress/integration/route/create-route-with-upstream.spec.js
+++ web/cypress/integration/route/create-route-with-upstream.spec.js
@@ -24,7 +24,6 @@ context('Create Route with Upstream', () => {
     name: '#name',
     description: '#desc',
     search_name: '[title=Name]',
-    custom_upstream: '[title=Custom]',
     upstream_selector: '[data-cy=upstream_selector]'
   };
 
@@ -66,14 +65,12 @@ context('Create Route with Upstream', () => {
 
   it('should disable Upstream input boxes after selecting an existing upstream', () => {
     cy.get(domSelector.upstream_selector).click();
-    cy.contains('Custom').click();
-    cy.get(domSelector.upstream_selector).click();
     cy.contains(data.upstream_name).click();
     cy.get(domSelector.input).should('be.disabled');
   });
 
   it('should enable Upstream input boxes after selecting Custom mode', () => {
-    cy.contains(data.upstream_name).click();
+    cy.get(domSelector.upstream_selector).click();
     cy.contains('Custom').click();
     cy.get(domSelector.input).should('not.be.disabled');
   });
@@ -97,12 +94,12 @@ context('Create Route with Upstream', () => {
     cy.contains(data.route_name).siblings().contains('Edit').click();
 
     cy.get(domSelector.name).should('value', data.route_name);
-    cy.contains('Next').click({force: true});
+    cy.contains('Next').click({ force: true });
 
     // check if the changes have been saved
     cy.get(domSelector.node_0_host).should('value', data.ip1);
 
-    cy.get(domSelector.custom_upstream).click();
+    cy.get(domSelector.upstream_selector).click();
     cy.contains(data.upstream_name).click();
     cy.get(domSelector.input).should('be.disabled');
 
