58bbaab25eada4c7e499ab9b19dd8b5be683b7c1
litesun <7sunmiao@gmail.com>
2021-02-05 16:09:29 +0800

    feat: update i18n

--- web/src/pages/ServerInfo/locales/en-US.ts
+++ web/src/pages/ServerInfo/locales/en-US.ts
@@ -16,8 +16,8 @@
  */
 export default {
   'page.systemStatus.pageContainer.title': 'System Status',
-  'page.systemStatus.select.placeholder': 'Please select node',
-  'page.systemStatus.desc': 'The relevant plugin need to be enabled to report server info.',
+  'page.systemStatus.select.placeholder': 'Please select a apache apisix node',
+  'page.systemStatus.desc': 'The relevant plugin needs to be enabled to report server info.',
   'page.systemStatus.link': 'How to enable?',
   'page.systemStatus.dashboardInfo': 'Dashboard',
   'page.systemStatus.nodeInfo': 'APISIX Nodes',
