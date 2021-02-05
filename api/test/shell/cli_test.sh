eaa6a73d58b4a6f607cc45227f567273a8a83d52
Peter Zhu <starszcan@gmail.com>
2021-02-05 12:44:23 +0800

    feat: support -v command to show version (#1431)

--- api/test/shell/cli_test.sh
+++ api/test/shell/cli_test.sh
@@ -193,6 +193,19 @@ if [[ `grep -c "${HOST}:${PORT}" ${STDOUT}` -ne '1' ]]; then
     exit 1
 fi
 
+# test -v command
+out=$(./manager-api -v 2>&1 || true)
+if [[ `echo $out | grep -c $VERSION` -ne '1' ]]; then
+    echo "failed: the manager server didn't show version info"
+    exit 1
+fi
+
+if [[ `echo $out | grep -c $GITHASH` -ne '1' ]]; then
+    echo "failed: the manager server didn't show git hash info"
+    exit 1
+fi
+
+
 # set an invalid etcd endpoint
 
 clean_up
