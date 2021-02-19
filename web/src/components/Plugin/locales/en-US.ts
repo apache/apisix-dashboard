2b9ff4837abc0cedcd439b71ecca3e25916f3d6e
琚致远 <juzhiyuan@apache.org>
2021-02-19 09:51:42 +0800

    feat: added tip for Plugin module (#1481)

--- web/src/components/Plugin/locales/en-US.ts
+++ web/src/components/Plugin/locales/en-US.ts
@@ -14,30 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { ActionBarEnUS } from '@/components/ActionBar';
-
-import component from './en-US/component';
-import globalHeader from './en-US/globalHeader';
-import menu from './en-US/menu';
-import pwa from './en-US/pwa';
-import settingDrawer from './en-US/settingDrawer';
-import settings from './en-US/setting';
-import PluginOrchestration from '../components/PluginOrchestration/locales/en-US';
-import Plugin from '../components/Plugin/locales/en-US';
-
 export default {
-  'navBar.lang': 'Languages',
-  'layout.user.link.help': 'Help',
-  'layout.user.link.privacy': 'Privacy',
-  'layout.user.link.terms': 'Terms',
-  'app.preview.down.block': 'Download this page to your local project',
-  ...globalHeader,
-  ...menu,
-  ...settingDrawer,
-  ...settings,
-  ...pwa,
-  ...component,
-  ...ActionBarEnUS,
-  ...PluginOrchestration,
-  ...Plugin,
+  'component.plugin.tip1': 'NOTE: After customizing the plugin, you need to update schema.json.',
+  'component.plugin.tip2': 'How to update?',
 };
