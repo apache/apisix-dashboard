be91a89c9c16283a16f97fefbea05dbed7d877fb
JinChen <36916582+Jaycean@users.noreply.github.com>
2021-03-01 12:53:46 +0800

    fix ci failed
    fix ci failed

--- api/test/e2enew/ssl/ssl_test.go
+++ api/test/e2enew/ssl/ssl_test.go
@@ -68,7 +68,7 @@ var _ = ginkgo.Describe("SSL Basic", func() {
 		}
 		_, err = http.Get("https://www.test2.com:9443")
 		assert.NotNil(t, err)
-		assert.EqualError(t, err, "Get \"https://www.test2.com:9443\": remote error: tls: internal error")
+		assert.EqualError(t, err, "Get https://www.test2.com:9443: remote error: tls: internal error")
 		//create ssl fail - key and cert not match
 		base.RunTestCase(base.HttpTestCase{
 			Object:       base.ManagerApiExpect(),
@@ -166,7 +166,7 @@ var _ = ginkgo.Describe("SSL Basic", func() {
 		time.Sleep(time.Duration(500) * time.Millisecond)
 		_, err := http.Get("https://www.test2.com:9443")
 		assert.NotNil(t, err)
-		assert.EqualError(t, err, "Get \"https://www.test2.com:9443\": remote error: tls: internal error")
+		assert.EqualError(t, err, "Get https://www.test2.com:9443: remote error: tls: internal error")
 	})
 	ginkgo.It("enable SSL", func() {
 		base.RunTestCase(base.HttpTestCase{
