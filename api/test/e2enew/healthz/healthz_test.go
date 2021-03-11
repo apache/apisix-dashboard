61c9b4acc6179147d99c1eefdcf0ec052fbc8fdc
Peter Zhu <starszcan@gmail.com>
2021-03-11 18:25:35 +0800

    chore: add health e2e test (#1572)

---
+++ api/test/e2enew/healthz/healthz_test.go
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package healthz
+
+import (
+	"net/http"
+
+	"github.com/onsi/ginkgo"
+
+	"e2enew/base"
+)
+
+var _ = ginkgo.Describe("Healthy check", func() {
+	ginkgo.It("ping manager-api", func() {
+		base.RunTestCase(base.HttpTestCase{
+			Object:       base.ManagerApiExpect(),
+			Method:       http.MethodGet,
+			Path:         "/ping",
+			ExpectStatus: http.StatusOK,
+			ExpectBody:   "pong",
+			Sleep:        base.SleepTime,
+		})
+	})
+})
