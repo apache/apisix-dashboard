2d6339e6c51478c5ab675aef81b432564f978657
Peter Zhu <starszcan@gmail.com>
2021-03-13 01:05:01 +0800

    chore: add github prefix at e2enew/base package (#1578)

--- api/test/e2enew/route/route_with_vars_test.go
+++ api/test/e2enew/route/route_with_vars_test.go
@@ -23,7 +23,7 @@ import (
 	"github.com/onsi/ginkgo"
 	"github.com/stretchr/testify/assert"
 
-	"e2enew/base"
+	"github.com/apisix/manager-api/test/e2enew/base"
 )
 
 var upstream map[string]interface{} = map[string]interface{}{
