a78fa6791ed53bffeba6dc879e1b2ad0ae9d9524
iamayushdas <ayush24das@gmail.com>
2021-03-23 06:12:38 +0530

    feat: added github link to header #1632

--- web/src/pages/User/Login.tsx
+++ web/src/pages/User/Login.tsx
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import logo from '@/assets/logo.svg';
 import React, { useState } from 'react';
 import { Button, notification, Tabs } from 'antd';
 import { SelectLang } from '@@/plugin-locale/SelectLang';
@@ -21,7 +22,6 @@ import { Link, useIntl, history } from 'umi';
 import LoginMethodPassword from '@/pages/User/components/LoginMethodPassword';
 import LoginMethodExample from '@/pages/User/components/LoginMethodExample';
 import type { UserModule } from '@/pages/User/typing';
-import logo from '@/assets/logo.svg';
 import { getUrlQuery } from '@/helpers';
 import Footer from '@/components/Footer';
 import styles from './Login.less';
@@ -82,8 +82,10 @@ const Page: React.FC = () => {
   return (
     <div className={styles.container}>
       <div className={styles.lang}>
-      <div className={styles.github} >
-          <a target="_blank" className="githubIcon" href="https://apisix.apache.org"><GithubOutlined /></a> 
+        <div className={styles.github} >
+          <a target="_blank" href="https://apisix.apache.org">
+            <GithubOutlined />
+          </a>
         </div>
         <SelectLang />
       </div>
