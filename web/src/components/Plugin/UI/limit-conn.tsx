8debc280f00e28e762fe3ad95e46d0c6a73103ff
litesun <7sunmiao@gmail.com>
2021-04-08 11:27:25 +0800

    feat: update limit-conn plugin

--- web/src/components/Plugin/UI/limit-conn.tsx
+++ web/src/components/Plugin/UI/limit-conn.tsx
@@ -23,16 +23,26 @@ type Props = {
   ref?: any;
 };
 
+export const FORM_ITEM_LAYOUT = {
+  labelCol: {
+    span: 6,
+  },
+  wrapperCol: {
+    span: 8
+  },
+};
+
 const LimitConn: React.FC<Props> = ({ form }) => {
   return (
     <Form
       form={form}
-      labelCol={{ span: 5 }}
+      {...FORM_ITEM_LAYOUT}
     >
       <Form.Item
         label="conn"
         required
         name="conn"
+        tooltip='the maximum number of concurrent requests allowed. Requests exceeding this ratio (and below conn + burst) will get delayed(the latency seconds is configured by default_conn_delay) to conform to this threshold.'
       >
         <InputNumber min={1} required></InputNumber>
       </Form.Item>
@@ -40,6 +50,7 @@ const LimitConn: React.FC<Props> = ({ form }) => {
         label="burst"
         required
         name="burst"
+        tooltip='the number of excessive concurrent requests (or connections) allowed to be delayed.'
       >
         <InputNumber min={0} required></InputNumber>
       </Form.Item>
@@ -47,14 +58,18 @@ const LimitConn: React.FC<Props> = ({ form }) => {
         label="default_conn_delay"
         required
         name="default_conn_delay"
+        tooltip='the latency seconds of request when concurrent requests exceeding conn but below (conn + burst).'
       >
-        <InputNumber min={1} required></InputNumber>
+        <InputNumber step={0.001} min={0.001} required></InputNumber>
       </Form.Item>
 
       <Form.Item
         label="key"
         required
         name="key"
+        tooltip="to limit the concurrency level.
+        For example, one can use the host name (or server zone) as the key so that we limit concurrency per host name. Otherwise, we can also use the client address as the key so that we can avoid a single client from flooding our service with too many parallel connections or requests.
+        Now accept those as key: 'remote_addr'(client's IP), 'server_addr'(server's IP), 'X-Forwarded-For/X-Real-IP' in request header, 'consumer_name'(consumer's username)."
       >
         <Select>
           {["remote_addr", "server_addr", "http_x_real_ip", "http_x_forwarded_for", "consumer_name"].map(item => {
@@ -66,8 +81,10 @@ const LimitConn: React.FC<Props> = ({ form }) => {
       <Form.Item
         label="rejected_code"
         name="rejected_code"
+        initialValue={503}
+        tooltip='returned when the request exceeds conn + burst will be rejected.'
       >
-        <InputNumber min={200} max={509} required></InputNumber>
+        <InputNumber min={200} max={599} required></InputNumber>
       </Form.Item>
     </Form>
   );
