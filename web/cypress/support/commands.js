2b743e1aeb8f83c6a68841f727fcf7b00b5d3b76
litesun <7sunmiao@gmail.com>
2021-04-09 19:28:22 +0800

    fix: ci

--- web/cypress/support/commands.js
+++ web/cypress/support/commands.js
@@ -69,6 +69,8 @@ Cypress.Commands.add('configurePlugins', (cases) => {
 
         cy.get(domSelector.codeMirrorMode).invoke('text').then(text => {
           if (text === 'Form') {
+            // FIXME: https://github.com/cypress-io/cypress/issues/7306
+            cy.wait(500);
             cy.get(domSelector.codeMirrorMode).should('be.visible');
             cy.get(domSelector.codeMirrorMode).click();
             cy.get(domSelector.selectDropdown).should('be.visible');
