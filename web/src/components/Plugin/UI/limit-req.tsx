6b9c3f920c4a4e0c94c96cfae434eb049bdcd082
guoqqqi <979918879@qq.com>
2021-04-10 00:26:11 +0800

    feat: added limit-req plugin form

---
+++ web/src/components/Plugin/UI/limit-req.tsx
@@ -0,0 +1,77 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import React from 'react';
+import type { FormInstance } from 'antd/es/form';
+import { Form, InputNumber, Select } from 'antd';
+
+type Props = {
+  form: FormInstance;
+  ref?: any;
+};
+
+export const FORM_ITEM_LAYOUT = {
+  labelCol: {
+    span: 4,
+  },
+  wrapperCol: {
+    span: 8
+  },
+};
+
+const LimitReq: React.FC<Props> = ({ form }) => {
+  return (
+    <Form
+      form={form}
+      {...FORM_ITEM_LAYOUT}
+    >
+      <Form.Item
+        label="rate"
+        name="rate"
+        required
+      >
+        <InputNumber min={1} required></InputNumber>
+      </Form.Item>
+      <Form.Item
+        label="burst"
+        name="burst"
+        required
+      >
+        <InputNumber min={0} required></InputNumber>
+      </Form.Item>
+      <Form.Item
+        label="key"
+        required
+        name="key"
+      >
+        <Select>
+          {["remote_addr", "server_addr", "http_x_real_ip", "http_x_forwarded_for", "consumer_name"].map(item => {
+            return <Select.Option value={item}>{item}</Select.Option>
+          })}
+        </Select>
+      </Form.Item>
+      <Form.Item
+        label="rejected_code"
+        name="rejected_code"
+        initialValue={503}
+      >
+        <InputNumber min={200} max={599}></InputNumber>
+      </Form.Item>
+    </Form>
+  );
+}
+
+export default LimitReq;
--- web/src/components/Plugin/UI/plugin.tsx
+++ web/src/components/Plugin/UI/plugin.tsx
@@ -20,6 +20,7 @@ import { Empty } from 'antd';
 import { useIntl } from 'umi';
 
 import BasicAuth from './basic-auth'
+import LimitReq from './limit-req';
 
 type Props = {
   name: string,
@@ -27,7 +28,7 @@ type Props = {
   renderForm: boolean
 }
 
-export const PLUGIN_UI_LIST = ['basic-auth',];
+export const PLUGIN_UI_LIST = ['basic-auth', 'limit-req'];
 
 export const PluginForm: React.FC<Props> = ({ name, renderForm, form }) => {
 
@@ -38,6 +39,8 @@ export const PluginForm: React.FC<Props> = ({ name, renderForm, form }) => {
   switch (name) {
     case 'basic-auth':
       return <BasicAuth form={form} />
+    case 'limit-req':
+      return <LimitReq form={form} />
     default:
       return null;
   }
