caadfc090126e51a0de377b380682b11357cebfb
guoqqqi <979918879@qq.com>
2021-04-10 18:01:03 +0800

    fix: CI

--- web/cypress/support/commands.js
+++ web/cypress/support/commands.js
@@ -73,7 +73,7 @@ Cypress.Commands.add('configurePlugins', (cases) => {
             cy.get(domSelector.selectJSON).click();
           }
         });
-        
+
         cy.get(domSelector.drawer, { timeout }).within(() => {
           cy.get(domSelector.switch).click({
             force: true,
