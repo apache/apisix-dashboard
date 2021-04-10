8143999a0b1bf5781d9d42bab257acc01c19b412
litesun <7sunmiao@gmail.com>
2021-04-10 16:09:51 +0800

    fix: ci

--- web/cypress/integration/pluginTemplate/create-plugin-template-with-route.spec.js
+++ web/cypress/integration/pluginTemplate/create-plugin-template-with-route.spec.js
@@ -40,10 +40,9 @@ context('Create PluginTemplate Binding To Route', () => {
 
     cy.visit('/routes/list');
     cy.contains('Create').click();
-    cy.get(this.domSelector.empty).should('be.visible');
 
-    // FIXME: https://github.com/cypress-io/cypress/issues/7306
-    cy.wait(500);
+    // NOTE: make sure all components rerender done
+    cy.contains('Next').click().click();
     cy.get(this.domSelector.name).type(this.data.routeName);
     cy.contains('Next').click();
     cy.get(this.domSelector.nodes_0_host).type(this.data.ip1);
