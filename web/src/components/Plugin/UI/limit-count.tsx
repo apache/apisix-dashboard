8a86b65b5ee2415a43b6303ef0c5f4658f681950
litesun <7sunmiao@gmail.com>
2021-04-11 17:39:40 +0800

    feat: update i18n

--- web/src/components/Plugin/UI/limit-count.tsx
+++ web/src/components/Plugin/UI/limit-count.tsx
@@ -48,11 +48,13 @@ const removeBtnStyle = {
 };
 
 const RedisForm: React.FC = () => {
+  const { formatMessage } = useIntl();
+
   return (<>
     <Form.Item
       label="redis_host"
       name="redis_host"
-      tooltip='When using the redis policy, this property specifies the address of the Redis server.'
+      tooltip={formatMessage({ id: 'component.pluginForm.limit-count.redis_host.tooltip' })}
       required
     >
       <Input />
@@ -60,14 +62,14 @@ const RedisForm: React.FC = () => {
     <Form.Item
       label="redis_port"
       name="redis_port"
-      tooltip='When using the redis policy, this property specifies the port of the Redis server.'
+      tooltip={formatMessage({ id: 'component.pluginForm.limit-count.redis_port.tooltip' })}
     >
       <InputNumber min={1} />
     </Form.Item>
     <Form.Item
       label="redis_password"
       name="redis_password"
-      tooltip='When using the redis policy, this property specifies the password of the Redis server.'
+      tooltip={formatMessage({ id: 'component.pluginForm.limit-count.redis_password.tooltip' })}
       required
     >
       <Input />
@@ -75,14 +77,14 @@ const RedisForm: React.FC = () => {
     <Form.Item
       label="redis_database"
       name="redis_database"
-      tooltip='When using the redis policy, this property specifies the database you selected of the Redis server, and only for non Redis cluster mode (single instance mode or Redis public cloud service that provides single entry).'
+      tooltip={formatMessage({ id: 'component.pluginForm.limit-count.redis_database.tooltip' })}
     >
       <InputNumber min={0} />
     </Form.Item>
     <Form.Item
       label="redis_timeout"
       name="redis_timeout"
-      tooltip='When using the redis policy, this property specifies the timeout in milliseconds of any command submitted to the Redis server.'
+      tooltip={formatMessage({ id: 'component.pluginForm.limit-count.redis_timeout.tooltip' })}
     >
       <InputNumber />
     </Form.Item>
@@ -97,7 +99,7 @@ const RedisClusterForm: React.FC<Props> = ({ form }) => {
       <Form.Item
         label="redis_cluster_name"
         name="redis_cluster_name"
-        tooltip='When using redis-cluster policy, this property is the name of Redis cluster service nodes.'
+        tooltip={formatMessage({ id: 'component.pluginForm.limit-count.redis_cluster_name.tooltip' })}
         required
       >
         <Input />
@@ -108,7 +110,7 @@ const RedisClusterForm: React.FC<Props> = ({ form }) => {
             <div>
               <Form.Item
                 label='redis_cluster_nodes'
-                tooltip='When using redis-cluster policy，This property is a list of addresses of Redis cluster service nodes.'
+                tooltip={formatMessage({ id: 'component.pluginForm.limit-count.redis_cluster_nodes.tooltip' })}
                 style={{ marginBottom: 0 }}
               >
                 {fields.map((field, index) => (
@@ -155,6 +157,7 @@ const RedisClusterForm: React.FC<Props> = ({ form }) => {
 
 const LimitCount: React.FC<Props> = ({ form }) => {
   const [policy, setPoicy] = useState<PolicyProps>('local');
+  const { formatMessage } = useIntl()
 
   return (
     <Form
@@ -165,7 +168,7 @@ const LimitCount: React.FC<Props> = ({ form }) => {
       <Form.Item
         label="count"
         name="count"
-        tooltip='the specified number of requests threshold.'
+        tooltip={formatMessage({ id: 'component.pluginForm.limit-count.count.tooltip' })}
         required
       >
         <InputNumber min={1} />
@@ -173,7 +176,7 @@ const LimitCount: React.FC<Props> = ({ form }) => {
       <Form.Item
         label="time_window"
         name="time_window"
-        tooltip='the time window in seconds before the request count is reset.'
+        tooltip={formatMessage({ id: 'component.pluginForm.limit-count.time_window.tooltip' })}
         required
       >
         <InputNumber min={1} />
@@ -181,6 +184,7 @@ const LimitCount: React.FC<Props> = ({ form }) => {
       <Form.Item
         label="key"
         name="key"
+        tooltip={formatMessage({ id: 'component.pluginForm.limit-count.key.tooltip' })}
       >
         <Select>
           {["remote_addr", "server_addr", "http_x_real_ip", "http_x_forwarded_for", "consumer_name", "service_id"].map(item => (<Select.Option value={item}>{item}</Select.Option>))}
@@ -189,13 +193,14 @@ const LimitCount: React.FC<Props> = ({ form }) => {
       <Form.Item
         label="rejected_code"
         name="rejected_code"
-        tooltip='The HTTP status code returned when the request exceeds the threshold is rejected, default 503.'
+        tooltip={formatMessage({ id: 'component.pluginForm.limit-count.rejected_code.tooltip' })}
       >
         <InputNumber min={200} max={599} />
       </Form.Item>
       <Form.Item
         label="policy"
         name="policy"
+        tooltip={formatMessage({ id: 'component.pluginForm.limit-count.policy.tooltip' })}
       >
         <Select onChange={(e: PolicyProps) => { setPoicy(e) }}>
           {["local", "redis", "redis-cluster"].map(item => (<Select.Option value={item}>{item}</Select.Option>))}
