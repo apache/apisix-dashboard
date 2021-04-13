acfa1d816a2798eefc1bd6ccd78a426fc3dc79a2
juzhiyuan <juzhiyuan@apache.org>
2021-04-13 15:25:52 +0800

    chore: update active.type

--- web/src/components/Upstream/locales/en-US.ts
+++ web/src/components/Upstream/locales/en-US.ts
@@ -2,5 +2,7 @@ export default {
   'component.upstream.fields.tls.client_key': 'Client Key',
   'component.upstream.fields.tls.client_cert': 'Client Cert',
 
+  'component.upstream.fields.checks.active.type': 'Type',
+
   'component.upstream.text.others': 'Others'
 }
--- web/src/components/Upstream/locales/zh-CN.ts
+++ web/src/components/Upstream/locales/zh-CN.ts
@@ -2,5 +2,7 @@ export default {
   'component.upstream.fields.tls.client_key': '客户端私钥',
   'component.upstream.fields.tls.client_cert': '客户端证书',
 
+  'component.upstream.fields.checks.active.type': '类型',
+
   'component.upstream.text.others': '其它'
 }
