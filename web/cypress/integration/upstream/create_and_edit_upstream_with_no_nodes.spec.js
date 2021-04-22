60a1289a1c402467072f981078b7fb31220974ef
liuxiran <belovedxixi@126.com>
2021-04-22 23:15:38 +0800

    fix: lint error

--- web/cypress/integration/upstream/create_and_edit_upstream_with_no_nodes.spec.js
+++ web/cypress/integration/upstream/create_and_edit_upstream_with_no_nodes.spec.js
@@ -19,32 +19,32 @@
 context('Create and Delete Upstream', () => {
     beforeEach(() => {
       cy.login();
-  
+
       cy.fixture('selector.json').as('domSelector');
       cy.fixture('data.json').as('data');
     });
-  
+
     it('should create upstream with no nodes', function () {
       cy.visit('/');
       cy.contains('Upstream').click();
       cy.contains('Create').click();
-  
+
       cy.get(this.domSelector.name).type(this.data.upstreamName);
       cy.get(this.domSelector.description).type(this.data.description);
-      
+
       // delete all nodes
       cy.get(this.domSelector.upstreamNodeMinus0).click();
-  
+
       cy.contains('Next').click();
       cy.contains('Submit').click();
       cy.get(this.domSelector.notification).should('contain', this.data.createUpstreamSuccess);
       cy.url().should('contains', 'upstream/list');
     });
-  
+
     it('should configure the upstream with no nodes', function () {
       cy.visit('/');
       cy.contains('Upstream').click();
-  
+
       cy.get(this.domSelector.nameSelector).type(this.data.upstreamName);
       cy.contains('Search').click();
       cy.contains(this.data.upstreamName).siblings().contains('Configure').click();
@@ -52,12 +52,12 @@ context('Create and Delete Upstream', () => {
       cy.get(this.domSelector.upstreamNodeMinus0).should('not.exist');
       cy.contains('Next').click();
       cy.contains('Submit').click();
-      
+
       cy.get(this.domSelector.notification).should('contain', this.data.configureUpstreamSuccess);
       cy.url().should('contains', 'upstream/list');
-      
+
     });
-  
+
     it('should delete the upstream', function () {
       cy.visit('/');
       cy.contains('Upstream').click();
@@ -66,4 +66,3 @@ context('Create and Delete Upstream', () => {
       cy.get(this.domSelector.notification).should('contain', this.data.deleteUpstreamSuccess);
     });
   });
-  
