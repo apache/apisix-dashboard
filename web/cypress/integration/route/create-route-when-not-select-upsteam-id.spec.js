17a54234ba9ff3c51cf1f67d837e5141037a395a
jiemoxiaolong <shishuanglong1989@163.com>
2021-04-29 22:12:05 +0800

    Update web/cypress/integration/route/create-route-when-not-select-upsteam-id.spec.js
    Co-authored-by: guoqqqi <72343596+guoqqqi@users.noreply.github.com>

--- web/cypress/integration/route/create-route-when-not-select-upsteam-id.spec.js
+++ web/cypress/integration/route/create-route-when-not-select-upsteam-id.spec.js
@@ -41,14 +41,7 @@ context('Create Route without Upstream', () => {
     cy.get(this.domSelector.nodes_0_weight).type(this.data.weight);
     cy.contains('Next').click();
     cy.contains('Next').click();
-    cy.get(this.domSelector.nodes_0_host).should('be.disabled','true');
-    cy.get(this.domSelector.nodes_0_port).should('be.disabled','true');
-    cy.get(this.domSelector.nodes_0_weight).should('be.disabled','true');
-    cy.get('#timeout_connect').should('be.disabled','true');
-    cy.get('#timeout_send').should('be.disabled','true');
-    cy.get('#timeout_read').should('be.disabled','true');
-    cy.get('#custom_checks_active').should('be.disabled','true');
-    cy.get('#custom_checks_passive').should('be.disabled','true');
+    cy.get(this.domSelector.input).should('be.disabled');
     cy.contains('Submit').click();
     cy.contains(this.data.submitSuccess).should('be.visible');
     cy.contains('Goto List').click();
