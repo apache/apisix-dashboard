896d0222edc6f8de0a6a26aa16e7217dfe9f0261
iamayushdas <ayush24das@gmail.com>
2021-05-08 07:53:29 +0530

    chore: refactored test create-route-when-not-select-upstream-id.spec.js

--- web/cypress/integration/route/create-route-when-not-select-upsteam-id.spec.js
+++ web/cypress/integration/route/create-route-when-not-select-upsteam-id.spec.js
@@ -17,33 +17,53 @@
 /* eslint-disable no-undef */
 
 context('Create Route without Upstream', () => {
+
+  const selector = {
+    name: '#name',
+    nodes_0_host: '#nodes_0_host',
+    nodes_0_port: '#nodes_0_port',
+    nodes_0_weight: '#nodes_0_weight',
+    input: ':input',
+    nameSelector: '[title=Name]',
+    deleteAlert: '.ant-modal-body',
+    notification: '.ant-notification-notice-message',
+  };
+
+  const data = {
+    routeName: 'test_route',
+    submitSuccess: 'Submit Successfully',
+    ip1: '127.0.0.1',
+    ip2: '127.0.0.2',
+    port: '80',
+    weight: 1,
+    deleteRouteSuccess: 'Delete Route Successfully',
+  };
+
   beforeEach(() => {
     cy.login();
-
-    cy.fixture('selector.json').as('domSelector');
-    cy.fixture('data.json').as('data');
   });
-
 
   it('should create route wittout upstream ', function () {
     cy.visit('/');
-    cy.get('[role=menu]').should('be.visible').within(() => {
-      cy.contains('Route').click();
-    });
+    cy.get('[role=menu]')
+      .should('be.visible')
+      .within(() => {
+        cy.contains('Route').click();
+      });
     cy.contains('Create').click();
 
     cy.contains('Next').click().click();
-    cy.get(this.domSelector.name).type(this.data.routeName);
+    cy.get(selector.name).type(data.routeName);
     cy.contains('Next').click();
 
-    cy.get(this.domSelector.nodes_0_host).clear().type(this.data.ip1);
-    cy.get(this.domSelector.nodes_0_port).type(this.data.port);
-    cy.get(this.domSelector.nodes_0_weight).type(this.data.weight);
+    cy.get(selector.nodes_0_host).clear().type(data.ip1);
+    cy.get(selector.nodes_0_port).type(data.port);
+    cy.get(selector.nodes_0_weight).type(data.weight);
     cy.contains('Next').click();
     cy.contains('Next').click();
-    cy.get(this.domSelector.input).should('be.disabled');
+    cy.get(selector.input).should('be.disabled');
     cy.contains('Submit').click();
-    cy.contains(this.data.submitSuccess).should('be.visible');
+    cy.contains(data.submitSuccess).should('be.visible');
     cy.contains('Goto List').click();
     cy.url().should('contains', 'routes/list');
   });
@@ -51,51 +71,53 @@ context('Create Route without Upstream', () => {
   it('should edit this route ', function () {
     cy.visit('/');
     cy.contains('Route').click();
-    cy.get(this.domSelector.nameSelector).type(this.data.routeName);
+    cy.get(selector.nameSelector).type(data.routeName);
 
     cy.contains('Search').click();
-    cy.contains(this.data.routeName).siblings().contains('Configure').click();
+    cy.contains(data.routeName).siblings().contains('Configure').click();
 
-    cy.get(this.domSelector.name).should('value', this.data.routeName);
+    cy.get(selector.name).should('value', data.routeName);
     cy.contains('Next').click({
-      force: true
+      force: true,
     });
 
     // check if the changes have been saved
-    cy.get(this.domSelector.nodes_0_host).should('value', this.data.ip1);
-    cy.get(this.domSelector.nodes_0_host).clear().type(this.data.ip2);
-    cy.get(this.domSelector.nodes_0_port).type(this.data.port);
-    cy.get(this.domSelector.nodes_0_weight).type(this.data.weight);
+    cy.get(selector.nodes_0_host).should('value', data.ip1);
+    cy.get(selector.nodes_0_host).clear().type(data.ip2);
+    cy.get(selector.nodes_0_port).type(data.port);
+    cy.get(selector.nodes_0_weight).type(data.weight);
     cy.contains('Next').click();
     cy.contains('Next').click();
-    cy.get(this.domSelector.input).should('be.disabled');
+    cy.get(selector.input).should('be.disabled');
     cy.contains('Submit').click();
-    cy.contains(this.data.submitSuccess).should('be.visible');
+    cy.contains(data.submitSuccess).should('be.visible');
     cy.contains('Goto List').click();
     cy.url().should('contains', 'routes/list');
 
     // check if the changes have been saved
-    cy.get(this.domSelector.nameSelector).type(this.data.routeName);
+    cy.get(selector.nameSelector).type(data.routeName);
     cy.contains('Search').click();
 
-    cy.contains(this.data.routeName).siblings().contains('Configure').click();
+    cy.contains(data.routeName).siblings().contains('Configure').click();
     // ensure it has already changed to edit page
-    cy.get(this.domSelector.name).should('value', this.data.routeName);
+    cy.get(selector.name).should('value', data.routeName);
     cy.contains('Next').click({
-      force: true
+      force: true,
     });
-    cy.get(this.domSelector.nodes_0_host).should('value', this.data.ip2);
+    cy.get(selector.nodes_0_host).should('value', data.ip2);
   });
 
   it('should delete this test route', function () {
     cy.visit('/routes/list');
-    cy.get(this.domSelector.nameSelector).type(this.data.routeName);
+    cy.get(selector.nameSelector).type(data.routeName);
     cy.contains('Search').click();
-    cy.contains(this.data.routeName).siblings().contains('More').click();
+    cy.contains(data.routeName).siblings().contains('More').click();
     cy.contains('Delete').click();
-    cy.get(this.domSelector.deleteAlert).should('be.visible').within(() => {
-      cy.contains('OK').click();
-    });
-    cy.get(this.domSelector.notification).should('contain', this.data.deleteRouteSuccess);
+    cy.get(selector.deleteAlert)
+      .should('be.visible')
+      .within(() => {
+        cy.contains('OK').click();
+      });
+    cy.get(selector.notification).should('contain', data.deleteRouteSuccess);
   });
 });
