f43785ddca5533b5961a9aa5641285cc7f1be61e
Ayush das <ayush24das@gmail.com>
2021-05-17 15:36:06 +0530

    chore: refactored test dashboard-smoketest.spec.js (#1852)

--- web/cypress/integration/dashboard/dashboard-smoketest.spec.js
+++ web/cypress/integration/dashboard/dashboard-smoketest.spec.js
@@ -17,17 +17,18 @@
 /* eslint-disable no-undef */
 
 context('dashboard page smoke test', () => {
+  const selector = {
+    pageContent: '.ant-pro-page-container',
+  }
   beforeEach(() => {
     cy.login();
-
-    cy.fixture('selector.json').as('domSelector');
   });
 
   it('should visit dashboard page', function () {
     cy.visit('/');
     cy.contains('Dashboard').click();
     cy.url().should('contains', '/dashboard');
-    cy.get(this.domSelector.pageContent)
+    cy.get(selector.pageContent)
       .children()
       .should('contain', 'Dashboard')
       .and('contain', 'You have not configured Grafana')
