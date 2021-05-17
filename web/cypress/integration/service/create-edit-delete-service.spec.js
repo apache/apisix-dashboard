abdfcac07460eea62c420ef1055d7f2357f7b780
Ayush das <ayush24das@gmail.com>
2021-05-17 07:23:49 +0530

    chore: refactored test create-edit-delete-service.spec.js (#1873)

--- web/cypress/integration/service/create-edit-delete-service.spec.js
+++ web/cypress/integration/service/create-edit-delete-service.spec.js
@@ -19,11 +19,42 @@
 context('Create and Delete Service ', () => {
   const timeout = 5000;
 
+  const selector = {
+    name: '#name',
+    description: '#desc',
+    nodes_0_host: '#nodes_0_host',
+    nodes_0_port: '#nodes_0_port',
+    nodes_0_weight: '#nodes_0_weight',
+    pluginCardBordered: '.ant-card-bordered',
+    disabledSwitcher: '#disable',
+    drawer: '.ant-drawer-content',
+    checkedSwitcher: '.ant-switch-checked',
+    codeMirrorMode: "[data-cy='code-mirror-mode']",
+    selectDropdown: '.ant-select-dropdown',
+    selectJSON: '.ant-select-dropdown [label=JSON]',
+    drawerFooter: '.ant-drawer-footer',
+    codemirrorScroll: '.CodeMirror-scroll',
+    notification: '.ant-notification-notice-message',
+    nameSelector: '[title=Name]',
+  };
+
+  const data = {
+    serviceName: 'test_service',
+    description: 'desc_by_autotest',
+    ip1: '127.0.0.1',
+    basicAuthPlugin: 'basic-auth',
+    createServiceSuccess: 'Create Service Successfully',
+    ip2: '127.0.0.2',
+    serviceName2: 'test_service2',
+    description2: 'description2',
+    editServiceSuccess: 'Configure Service Successfully',
+    deleteServiceSuccess: 'Delete Service Successfully',
+    port0: '7000',
+    weight0: '1',
+  };
+
   beforeEach(() => {
     cy.login();
-
-    cy.fixture('selector.json').as('domSelector');
-    cy.fixture('data.json').as('data');
   });
 
   it('should create service', function () {
@@ -31,61 +62,69 @@ context('Create and Delete Service ', () => {
     cy.contains('Service').click();
     cy.contains('Create').click();
 
-    cy.get(this.domSelector.name).type(this.data.serviceName);
-    cy.get(this.domSelector.description).type(this.data.description);
-    cy.get(this.domSelector.nodes_0_host).click();
-    cy.get(this.domSelector.nodes_0_host).type(this.data.ip1);
-    cy.get(this.domSelector.nodes_0_port).clear().type('7000');
-    cy.get(this.domSelector.nodes_0_weight).clear().type(1);
+    cy.get(selector.name).type(data.serviceName);
+    cy.get(selector.description).type(data.description);
+    cy.get(selector.nodes_0_host).click();
+    cy.get(selector.nodes_0_host).type(data.ip1);
+    cy.get(selector.nodes_0_port).clear().type(data.port0);
+    cy.get(selector.nodes_0_weight).clear().type(data.weight0);
 
     cy.contains('Next').click();
 
-    cy.contains(this.data.basicAuthPlugin).parents(this.domSelector.pluginCardBordered).within(() => {
-      cy.get('button').click({ force: true });
-    });
+    cy.contains(data.basicAuthPlugin)
+      .parents(selector.pluginCardBordered)
+      .within(() => {
+        cy.get('button').click({ force: true });
+      });
 
-    cy.get(this.domSelector.drawer).should('be.visible').within(() => {
-      cy.get(this.domSelector.disabledSwitcher).click();
-      cy.get(this.domSelector.checkedSwitcher).should('exist');
-    });
+    cy.get(selector.drawer)
+      .should('be.visible')
+      .within(() => {
+        cy.get(selector.disabledSwitcher).click();
+        cy.get(selector.checkedSwitcher).should('exist');
+      });
 
-    cy.get(this.domSelector.codeMirrorMode).click();
-    cy.get(this.domSelector.selectDropdown).should('be.visible');
-    cy.get(this.domSelector.selectJSON).click();
+    cy.get(selector.codeMirrorMode).click();
+    cy.get(selector.selectDropdown).should('be.visible');
+    cy.get(selector.selectJSON).click();
     cy.contains('button', 'Submit').click();
-    cy.get(this.domSelector.drawer, { timeout }).should('not.exist');
+    cy.get(selector.drawer, { timeout }).should('not.exist');
 
-    cy.contains(this.data.basicAuthPlugin).parents(this.domSelector.pluginCardBordered).within(() => {
-      cy.get('button').click({ force: true });
-    });
+    cy.contains(data.basicAuthPlugin)
+      .parents(selector.pluginCardBordered)
+      .within(() => {
+        cy.get('button').click({ force: true });
+      });
 
-    cy.get(this.domSelector.drawerFooter).contains('button', 'Delete').click({ force: true });
+    cy.get(selector.drawerFooter).contains('button', 'Delete').click({ force: true });
     cy.contains('button', 'Confirm').click({ force: true });
 
-    cy.contains(this.data.basicAuthPlugin).parents(this.domSelector.pluginCardBordered).within(() => {
-      cy.get('button').click({ force: true });
-    });
+    cy.contains(data.basicAuthPlugin)
+      .parents(selector.pluginCardBordered)
+      .within(() => {
+        cy.get('button').click({ force: true });
+      });
 
-    cy.get(this.domSelector.drawerFooter).contains('button', 'Delete').should('not.exist');
+    cy.get(selector.drawerFooter).contains('button', 'Delete').should('not.exist');
     cy.contains('button', 'Cancel').click({ force: true });
 
     cy.contains('Next').click();
     cy.contains('Submit').click();
-    cy.get(this.domSelector.notification).should('contain', this.data.createServiceSuccess);
+    cy.get(selector.notification).should('contain', data.createServiceSuccess);
   });
 
   it('should view the service', function () {
     cy.visit('/');
     cy.contains('Service').click();
 
-    cy.get(this.domSelector.nameSelector).type(this.data.serviceName);
+    cy.get(selector.nameSelector).type(data.serviceName);
     cy.contains('Search').click();
-    cy.contains(this.data.serviceName).siblings().contains('View').click();
-    cy.get(this.domSelector.drawer).should('be.visible');
+    cy.contains(data.serviceName).siblings().contains('View').click();
+    cy.get(selector.drawer).should('be.visible');
 
-    cy.get(this.domSelector.codemirrorScroll).within(() => {
+    cy.get(selector.codemirrorScroll).within(() => {
       cy.contains('upstream').should('exist');
-      cy.contains(this.data.serviceName).should('exist');
+      cy.contains(data.serviceName).should('exist');
     });
   });
 
@@ -93,45 +132,45 @@ context('Create and Delete Service ', () => {
     cy.visit('/');
     cy.contains('Service').click();
 
-    cy.get(this.domSelector.nameSelector).type(this.data.serviceName);
+    cy.get(selector.nameSelector).type(data.serviceName);
     cy.contains('Search').click();
-    cy.contains(this.data.serviceName).siblings().contains('Configure').click();
+    cy.contains(data.serviceName).siblings().contains('Configure').click();
 
     // Confirm whether the created data is saved.
-    cy.get(this.domSelector.nodes_0_host).should('value', this.data.ip1);
-    cy.get(this.domSelector.description).should('value', this.data.description);
-    cy.get(this.domSelector.name).clear().type(this.data.serviceName2);
-    cy.get(this.domSelector.description).clear().type(this.data.description2);
-    cy.get(this.domSelector.nodes_0_host).click();
-    cy.get(this.domSelector.nodes_0_host).clear().type(this.data.ip2);
+    cy.get(selector.nodes_0_host).should('value', data.ip1);
+    cy.get(selector.description).should('value', data.description);
+    cy.get(selector.name).clear().type(data.serviceName2);
+    cy.get(selector.description).clear().type(data.description2);
+    cy.get(selector.nodes_0_host).click();
+    cy.get(selector.nodes_0_host).clear().type(data.ip2);
     cy.contains('Next').click();
     cy.contains('Next').click();
     cy.contains('Submit').click();
-    cy.get(this.domSelector.notification).should('contain', this.data.editServiceSuccess);
+    cy.get(selector.notification).should('contain', data.editServiceSuccess);
 
     // test view
-    cy.contains(this.data.serviceName2).siblings().contains('View').click();
-    cy.get(this.domSelector.drawer).should('be.visible');
+    cy.contains(data.serviceName2).siblings().contains('View').click();
+    cy.get(selector.drawer).should('be.visible');
 
-    cy.get(this.domSelector.codemirrorScroll).within(() => {
+    cy.get(selector.codemirrorScroll).within(() => {
       cy.contains('upstream').should('exist');
-      cy.contains(this.data.serviceName2).should('exist');
+      cy.contains(data.serviceName2).should('exist');
     });
   });
 
   it('should delete the service', function () {
     // Confirm whether the edited data is saved.
     cy.visit('/service/list');
-    cy.get(this.domSelector.nameSelector).type(this.data.serviceName2);
+    cy.get(selector.nameSelector).type(data.serviceName2);
     cy.contains('Search').click();
-    cy.contains(this.data.serviceName2).siblings().contains('Configure').click();
-    cy.get(this.domSelector.nodes_0_host).should('value', this.data.ip2);
-    cy.get(this.domSelector.description).should('value', this.data.description2);
+    cy.contains(data.serviceName2).siblings().contains('Configure').click();
+    cy.get(selector.nodes_0_host).should('value', data.ip2);
+    cy.get(selector.description).should('value', data.description2);
 
     cy.visit('/');
     cy.contains('Service').click();
-    cy.contains(this.data.serviceName2).siblings().contains('Delete').click();
+    cy.contains(data.serviceName2).siblings().contains('Delete').click();
     cy.contains('button', 'Confirm').click();
-    cy.get(this.domSelector.notification).should('contain', this.data.deleteServiceSuccess);
+    cy.get(selector.notification).should('contain', data.deleteServiceSuccess);
   });
 });
