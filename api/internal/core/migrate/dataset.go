74c256e4b5462100a792378c91dd09a365db9222
fregie <xiaohao950830@live.com>
2021-06-04 15:06:31 +0800

    remove useless comment

--- api/internal/core/migrate/dataset.go
+++ api/internal/core/migrate/dataset.go
@@ -32,7 +32,6 @@ type DataSet struct {
 	Upstreams     []*entity.Upstream
 	Scripts       []*entity.Script
 	GlobalPlugins []*entity.GlobalPlugins
-	// ServerInfos   []*entity.ServerInfo
 	PluginConfigs []*entity.PluginConfig
 }
 
@@ -45,7 +44,6 @@ func newAllData() *DataSet {
 		Upstreams:     make([]*entity.Upstream, 0),
 		Scripts:       make([]*entity.Script, 0),
 		GlobalPlugins: make([]*entity.GlobalPlugins, 0),
-		// ServerInfos:   make([]*entity.ServerInfo, 0),
 		PluginConfigs: make([]*entity.PluginConfig, 0),
 	}
 }
@@ -94,12 +92,6 @@ func (a *DataSet) rangeData(key store.HubKey, f func(int, interface{}) bool) {
 				break
 			}
 		}
-	// case store.HubKeyServerInfo:
-	// 	for i, v := range a.ServerInfos {
-	// 		if !f(i, v) {
-	// 			break
-	// 		}
-	// 	}
 	case store.HubKeyPluginConfig:
 		for i, v := range a.PluginConfigs {
 			if !f(i, v) {
@@ -126,8 +118,6 @@ func (a *DataSet) Add(obj interface{}) error {
 		a.Scripts = append(a.Scripts, obj)
 	case *entity.GlobalPlugins:
 		a.GlobalPlugins = append(a.GlobalPlugins, obj)
-	// case *entity.ServerInfo:
-	// 	a.ServerInfos = append(a.ServerInfos, obj)
 	case *entity.PluginConfig:
 		a.PluginConfigs = append(a.PluginConfigs, obj)
 	default:
