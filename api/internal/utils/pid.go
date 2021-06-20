6d94496a16ab2151f4004049025a58f21e468e98
Bisakh Mondal <bisakhmondal00@gmail.com>
2021-06-20 11:56:13 +0530

    silly me! we don't need another else block :)

--- api/internal/utils/pid.go
+++ api/internal/utils/pid.go
@@ -29,9 +29,8 @@ func WritePID(filepath string, forceStart bool) error {
 	if pid, err := ReadPID(filepath); err == nil {
 		if !forceStart {
 			return fmt.Errorf("Instance of Manager API maybe running with a pid %d. If not, please run Manager API with '-f' or '--force' flag\n", pid)
-		} else {
-			fmt.Printf("Force starting new instance. Another instance of Manager API maybe running with pid %d\n", pid)
 		}
+		fmt.Printf("Force starting new instance. Another instance of Manager API maybe running with pid %d\n", pid)
 	}
 	pid := os.Getpid()
 	f, err := os.OpenFile(filepath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC|os.O_CREATE, 0600)
