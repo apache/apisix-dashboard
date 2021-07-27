b5ca00943d16215d5f0bcaed12b1d49890b59981
litesun <7sunmiao@gmail.com>
2021-07-27 15:40:28 +0800

    update test

--- web/cypress/integration/consumer/create_and_delete_consumer.spec.js
+++ web/cypress/integration/consumer/create_and_delete_consumer.spec.js
@@ -29,6 +29,7 @@ context('Create and Delete Consumer', () => {
     serviceSelector: '[title=test_service]',
     monacoScroll: '.monaco-scrollable-element',
     monacoViewZones: '.view-zones',
+    notificationCloseIcon: '.ant-notification-close-icon',
   };
 
   const data = {
@@ -73,9 +74,15 @@ context('Create and Delete Consumer', () => {
     cy.get(selector.disabledSwitcher).click();
 
     // edit monaco
-    cy.get(selector.monacoViewZones).should('exist').click({ force: true });
+    cy.get(selector.monacoViewZones).should('exist').click({
+      force: true,
+    });
     cy.window().then((window) => {
-      window.monacoEditor.setValue(JSON.stringify({ key: 'test' }));
+      window.monacoEditor.setValue(
+        JSON.stringify({
+          key: 'test',
+        }),
+      );
       cy.contains('button', 'Submit').click();
     });
     cy.contains('button', 'Next').click();
@@ -120,9 +127,15 @@ context('Create and Delete Consumer', () => {
     });
 
     // edit monaco
-    cy.get(selector.monacoViewZones).should('exist').click({ force: true });
+    cy.get(selector.monacoViewZones).should('exist').click({
+      force: true,
+    });
     cy.window().then((window) => {
-      window.monacoEditor.setValue(JSON.stringify({ key_not_exist: 'test' }));
+      window.monacoEditor.setValue(
+        JSON.stringify({
+          key_not_exist: 'test',
+        }),
+      );
       cy.contains('button', 'Submit').click();
     });
     cy.get(selector.notification).should('contain', data.pluginErrorAlert);
