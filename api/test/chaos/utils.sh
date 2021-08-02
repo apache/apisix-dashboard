deb6ae12169bb3a30d7e6929d442870ed2ec3460
yiyiyimu <wosoyoung@gmail.com>
2021-08-02 19:38:03 -0400

    port forward seems broke
    Signed-off-by: yiyiyimu <wosoyoung@gmail.com>

--- api/test/chaos/utils.sh
+++ api/test/chaos/utils.sh
@@ -17,17 +17,17 @@
 #
 
 port_forward() {
-    nohup kubectl port-forward svc/apisix 9080:9080 >/dev/null 2>&1 &
-    nohup kubectl port-forward svc/apisix 9091:9091 >/dev/null 2>&1 &
-    nohup kubectl port-forward svc/apisix 9443:9443 >/dev/null 2>&1 &
-    nohup kubectl port-forward svc/apisix2 9081:9081 >/dev/null 2>&1 &
-    nohup kubectl port-forward svc/etcd 2379:2379 >/dev/null 2>&1 &
-    nohup kubectl port-forward svc/managerapi 9000:9000 >/dev/null 2>&1 &
-    nohup kubectl port-forward svc/upstream 1980:1980 >/dev/null 2>&1 &
-    nohup kubectl port-forward svc/upstream 1981:1981 >/dev/null 2>&1 &
-    nohup kubectl port-forward svc/upstream 1982:1982 >/dev/null 2>&1 &
-    nohup kubectl port-forward svc/upstream 1983:1983 >/dev/null 2>&1 &
-    nohup kubectl port-forward svc/upstream 1984:1984 >/dev/null 2>&1 &
+    nohup kubectl port-forward svc/apisix 9080:9080 >/tmp/pf1 2>&1 &
+    nohup kubectl port-forward svc/apisix 9091:9091 >/tmp/pf2 2>&1 &
+    nohup kubectl port-forward svc/apisix 9443:9443 >/tmp/pf3 2>&1 &
+    nohup kubectl port-forward svc/apisix2 9081:9081 >/tmp/pf4 2>&1 &
+    nohup kubectl port-forward svc/etcd 2379:2379 >/tmp/pf5 2>&1 &
+    nohup kubectl port-forward svc/managerapi 9000:9000 >/tmp/pf6 2>&1 &
+    nohup kubectl port-forward svc/upstream 1980:1980 >/tmp/pf7 2>&1 &
+    nohup kubectl port-forward svc/upstream 1981:1981 >/tmp/pf8 2>&1 &
+    nohup kubectl port-forward svc/upstream 1982:1982 >/tmp/pf9 2>&1 &
+    nohup kubectl port-forward svc/upstream 1983:1983 >/tmp/pf10 2>&1 &
+    nohup kubectl port-forward svc/upstream 1984:1984 >/tmp/pf11 2>&1 &
 }
 
 kill_port_forward() {
