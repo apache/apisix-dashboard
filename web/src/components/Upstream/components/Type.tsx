2d78713758d349f5a5fe94108263a3303069693d
Baoyuan <baoyuan.top@gmail.com>
2021-08-04 15:31:30 +0800

    fix: modify key required rule

--- web/src/components/Upstream/components/Type.tsx
+++ web/src/components/Upstream/components/Type.tsx
@@ -14,20 +14,20 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import React, { useState } from 'react'
-import { AutoComplete, Form, Select } from 'antd'
-import { useIntl } from 'umi'
-import type { FormInstance } from 'antd/es/form'
+import React, { useState } from 'react';
+import { AutoComplete, Form, Select } from 'antd';
+import { useIntl } from 'umi';
+import type { FormInstance } from 'antd/es/form';
 
-import { HashOnEnum, CommonHashKeyEnum, AlgorithmEnum } from '../constant'
+import { HashOnEnum, CommonHashKeyEnum, AlgorithmEnum } from '../constant';
 
 type Props = {
-  readonly?: boolean
-  form: FormInstance
-}
+  readonly?: boolean;
+  form: FormInstance;
+};
 
-const CHash: React.FC<Pick<Props, 'readonly'>> = ({ readonly }) => {
-  const { formatMessage } = useIntl()
+const CHash: React.FC<Props> = ({ form, readonly }) => {
+  const { formatMessage } = useIntl();
   const [keySearchWord, setKeySearchWord] = useState('');
 
   const handleSearch = (search: string) => {
@@ -35,7 +35,13 @@ const CHash: React.FC<Pick<Props, 'readonly'>> = ({ readonly }) => {
   };
   return (
     <React.Fragment>
-      <Form.Item name="hash_on" rules={[{ required: true }]} label={formatMessage({ id: 'component.upstream.fields.hash_on' })} tooltip={formatMessage({ id: 'component.upstream.fields.hash_on.tooltip' })} initialValue="vars">
+      <Form.Item
+        name="hash_on"
+        rules={[{ required: true }]}
+        label={formatMessage({ id: 'component.upstream.fields.hash_on' })}
+        tooltip={formatMessage({ id: 'component.upstream.fields.hash_on.tooltip' })}
+        initialValue="vars"
+      >
         <Select disabled={readonly}>
           {Object.entries(HashOnEnum).map(([label, value]) => (
             <Select.Option value={value} key={value}>
@@ -44,12 +50,19 @@ const CHash: React.FC<Pick<Props, 'readonly'>> = ({ readonly }) => {
           ))}
         </Select>
       </Form.Item>
-      <Form.Item name="key" rules={[{ required: true }]}
-                 label={formatMessage({ id: 'component.upstream.fields.key' })}
-                 tooltip={formatMessage({ id: 'component.upstream.fields.key.tooltip' })} initialValue="remote_addr">
+      <Form.Item
+        name="key"
+        rules={[{ required: form.getFieldValue('hash_on') !== 'consumer' }]}
+        label={formatMessage({ id: 'component.upstream.fields.key' })}
+        tooltip={formatMessage({ id: 'component.upstream.fields.key.tooltip' })}
+        initialValue="remote_addr"
+      >
         <AutoComplete disabled={readonly} onSearch={handleSearch}>
           {Object.entries(CommonHashKeyEnum)
-            .filter((([label, value]) => label.startsWith(keySearchWord) || value.startsWith(keySearchWord)))
+            .filter(
+              ([label, value]) =>
+                label.startsWith(keySearchWord) || value.startsWith(keySearchWord),
+            )
             .map(([label, value]) => (
               <Select.Option value={value} key={value}>
                 {label}
@@ -58,11 +71,11 @@ const CHash: React.FC<Pick<Props, 'readonly'>> = ({ readonly }) => {
         </AutoComplete>
       </Form.Item>
     </React.Fragment>
-  )
+  );
 };
 
 const Component: React.FC<Props> = ({ readonly, form }) => {
-  const { formatMessage } = useIntl()
+  const { formatMessage } = useIntl();
 
   return (
     <React.Fragment>
@@ -85,13 +98,13 @@ const Component: React.FC<Props> = ({ readonly, form }) => {
       <Form.Item shouldUpdate noStyle>
         {() => {
           if (form.getFieldValue('type') === 'chash') {
-            return <CHash readonly={readonly} />;
+            return <CHash form={form} readonly={readonly} />;
           }
           return null;
         }}
       </Form.Item>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default Component
+export default Component;
