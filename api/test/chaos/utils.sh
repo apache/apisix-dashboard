40e2560d6082cc32546d8ba5f6ae3efe276a49f7
yiyiyimu <wosoyoung@gmail.com>
2021-08-04 19:04:54 -0400

    fix test
    Signed-off-by: yiyiyimu <wosoyoung@gmail.com>

--- api/test/chaos/utils.sh
+++ api/test/chaos/utils.sh
@@ -18,22 +18,22 @@
 
 apply_sed() {
     sed -i -z 's$"http://172.16.238.10:2379"\n    - "http://172.16.238.11:2379"\n    - "http://172.16.238.12:2379"$"http://etcd.default.svc.cluster.local:2379"$g' ./*.*
-    sed -i -e 's$http://172.16.238.50$http://skywalking.default.svc.cluster.local$g' ./*.*
+    sed -i -e 's$172.16.238.50$skywalking.default.svc.cluster.local$g' ./*.*
     sed -i -e 's$127.0.0.1:2379$etcd.default.svc.cluster.local:2379$g' ../../conf/conf.yaml
 }
 
 port_forward() {
-    nohup kubectl port-forward svc/apisix 9080:9080 >/tmp/pf1 2>&1 &
-    nohup kubectl port-forward svc/apisix 9091:9091 >/tmp/pf2 2>&1 &
-    nohup kubectl port-forward svc/apisix 9443:9443 >/tmp/pf3 2>&1 &
-    nohup kubectl port-forward svc/apisix2 9081:9081 >/tmp/pf4 2>&1 &
-    nohup kubectl port-forward svc/etcd 2379:2379 >/tmp/pf5 2>&1 &
-    nohup kubectl port-forward svc/managerapi 9000:9000 >/tmp/pf6 2>&1 &
-    nohup kubectl port-forward svc/upstream 1980:1980 >/tmp/pf7 2>&1 &
-    nohup kubectl port-forward svc/upstream 1981:1981 >/tmp/pf8 2>&1 &
-    nohup kubectl port-forward svc/upstream 1982:1982 >/tmp/pf9 2>&1 &
-    nohup kubectl port-forward svc/upstream 1983:1983 >/tmp/pf10 2>&1 &
-    nohup kubectl port-forward svc/upstream 1984:1984 >/tmp/pf11 2>&1 &
+    nohup kubectl port-forward svc/apisix 9080:9080 >/dev/null 2>&1 &
+    nohup kubectl port-forward svc/apisix 9091:9091 >/dev/null 2>&1 &
+    nohup kubectl port-forward svc/apisix 9443:9443 >/dev/null 2>&1 &
+    nohup kubectl port-forward svc/apisix2 9081:9080 >/dev/null 2>&1 &
+    nohup kubectl port-forward svc/etcd 2379:2379 >/dev/null 2>&1 &
+    nohup kubectl port-forward svc/managerapi 9000:9000 >/dev/null 2>&1 &
+    nohup kubectl port-forward svc/upstream 1980:1980 >/dev/null 2>&1 &
+    nohup kubectl port-forward svc/upstream 1981:1981 >/dev/null 2>&1 &
+    nohup kubectl port-forward svc/upstream 1982:1982 >/dev/null 2>&1 &
+    nohup kubectl port-forward svc/upstream 1983:1983 >/dev/null 2>&1 &
+    nohup kubectl port-forward svc/upstream 1984:1984 >/dev/null 2>&1 &
 }
 
 kill_port_forward() {
