c7dec73b480ce4f6b4550e2f2cb3d091a0e78bf4
bzp2010 <bzp2010@apache.org>
2021-09-06 22:46:25 +0800

    feat: update proto unit test

--- api/internal/handler/proto/proto_test.go
+++ api/internal/handler/proto/proto_test.go
@@ -17,6 +17,8 @@
 package proto
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 	"testing"
@@ -28,6 +30,7 @@ import (
 
 	"github.com/apisix/manager-api/internal/core/entity"
 	"github.com/apisix/manager-api/internal/core/store"
+	"github.com/apisix/manager-api/internal/handler"
 )
 
 type testCase struct {
@@ -40,19 +43,19 @@ type testCase struct {
 	wantRet    interface{}
 }
 
-func TestHandler_Get(t *testing.T) {
+func TestProto_Get(t *testing.T) {
 	tests := []testCase{
 		{
 			caseDesc:   "normal",
 			giveInput:  &GetInput{ID: "test"},
 			wantGetKey: "test",
-			giveRet:    &entity.Proto{
+			giveRet: &entity.Proto{
 				BaseInfo: entity.BaseInfo{
 					ID: "r1",
 				},
 				Content: "test",
 			},
-			wantRet:    &entity.Proto{
+			wantRet: &entity.Proto{
 				BaseInfo: entity.BaseInfo{
 					ID: "r1",
 				},
@@ -91,7 +94,7 @@ func TestHandler_Get(t *testing.T) {
 	}
 }
 
-func TestRoute_List(t *testing.T) {
+func TestProto_List(t *testing.T) {
 	mockData := []*entity.Proto{
 		{
 			BaseInfo: entity.BaseInfo{
@@ -99,7 +102,7 @@ func TestRoute_List(t *testing.T) {
 				CreateTime: 1609340491,
 				UpdateTime: 1609340491,
 			},
-			Desc: "proto1",
+			Desc:    "test",
 			Content: "proto1",
 		},
 		{
@@ -108,7 +111,7 @@ func TestRoute_List(t *testing.T) {
 				CreateTime: 1609340491,
 				UpdateTime: 1609340491,
 			},
-			Desc: "proto2",
+			Desc:    "test",
 			Content: "proto2",
 		},
 		{
@@ -117,7 +120,7 @@ func TestRoute_List(t *testing.T) {
 				CreateTime: 1609340491,
 				UpdateTime: 1609340491,
 			},
-			Desc: "proto3",
+			Desc:    "proto3",
 			Content: "proto3",
 		},
 	}
@@ -153,9 +156,9 @@ func TestRoute_List(t *testing.T) {
 			},
 		},
 		{
-			caseDesc: "list upstream with 'proto1'",
+			caseDesc: "list proto with test desc",
 			giveInput: &ListInput{
-				Desc: "proto1",
+				Desc: "test",
 				Pagination: store.Pagination{
 					PageSize:   10,
 					PageNumber: 10,
@@ -168,8 +171,9 @@ func TestRoute_List(t *testing.T) {
 			wantRet: &store.ListOutput{
 				Rows: []interface{}{
 					mockData[0],
+					mockData[1],
 				},
-				TotalSize: 1,
+				TotalSize: 2,
 			},
 		},
 	}
@@ -205,6 +209,499 @@ func TestRoute_List(t *testing.T) {
 			ctx.SetInput(tc.giveInput)
 			ret, err := h.List(ctx)
 			assert.True(t, getCalled)
+			assert.Equal(t, tc.wantRet, ret)
+			assert.Equal(t, tc.wantErr, err)
+		})
+	}
+}
+
+func TestProto_Create(t *testing.T) {
+	tests := []struct {
+		caseDesc     string
+		getCalled    bool
+		giveInput    *entity.Proto
+		giveRet      interface{}
+		giveErr      error
+		wantInput    *entity.Proto
+		wantErr      error
+		wantRet      interface{}
+		nameExistRet []interface{}
+	}{
+		{
+			caseDesc:  "create success",
+			getCalled: true,
+			giveInput: &entity.Proto{
+				BaseInfo: entity.BaseInfo{
+					ID: "p1",
+				},
+				Desc:    "p1",
+				Content: "p1",
+			},
+			giveRet: &entity.Proto{
+				BaseInfo: entity.BaseInfo{
+					ID: "p1",
+				},
+				Desc:    "p1",
+				Content: "p1",
+			},
+			wantInput: &entity.Proto{
+				BaseInfo: entity.BaseInfo{
+					ID: "p1",
+				},
+				Desc:    "p1",
+				Content: "p1",
+			},
+			wantRet: &entity.Proto{
+				BaseInfo: entity.BaseInfo{
+					ID: "p1",
+				},
+				Desc:    "p1",
+				Content: "p1",
+			},
+			wantErr: nil,
+		},
+		{
+			caseDesc:  "create failed, create return error",
+			getCalled: true,
+			giveInput: &entity.Proto{
+				BaseInfo: entity.BaseInfo{
+					ID: "p1",
+				},
+				Desc:    "p1",
+				Content: "p1",
+			},
+			giveErr: fmt.Errorf("create failed"),
+			wantInput: &entity.Proto{
+				BaseInfo: entity.BaseInfo{
+					ID: "p1",
+				},
+				Desc:    "p1",
+				Content: "p1",
+			},
+			wantErr: fmt.Errorf("create failed"),
+			wantRet: handler.SpecCodeResponse(fmt.Errorf("create failed")),
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.caseDesc, func(t *testing.T) {
+			getCalled := false
+
+			protoStore := &store.MockInterface{}
+			protoStore.On("Create", mock.Anything, mock.Anything).Run(func(args mock.Arguments) {
+				getCalled = true
+				input := args.Get(1).(*entity.Proto)
+				assert.Equal(t, tc.wantInput, input)
+			}).Return(tc.giveRet, tc.giveErr)
+
+			protoStore.On("Get", mock.Anything).Run(func(args mock.Arguments) {
+				getCalled = true
+				assert.Equal(t, tc.wantInput.ID, args.Get(0))
+			}).Return(nil, nil)
+
+			h := Handler{protoStore: protoStore}
+
+			ctx := droplet.NewContext()
+			ctx.SetInput(tc.giveInput)
+			ret, err := h.Create(ctx)
+			assert.True(t, getCalled)
+			assert.Equal(t, tc.wantRet, ret)
+			assert.Equal(t, tc.wantErr, err)
+		})
+	}
+}
+
+func TestProto_Update(t *testing.T) {
+	mockData := []*entity.Proto{
+		{
+			BaseInfo: entity.BaseInfo{
+				ID: "p1",
+			},
+			Content: "p1-origin",
+		},
+	}
+
+	tests := []struct {
+		caseDesc     string
+		getCalled    bool
+		giveInput    *UpdateInput
+		giveErr      error
+		giveRet      interface{}
+		wantInput    *entity.Proto
+		wantErr      error
+		wantRet      interface{}
+		nameExistRet []interface{}
+	}{
+		{
+			caseDesc:  "update success",
+			getCalled: true,
+			giveInput: &UpdateInput{
+				ID: "p1",
+				Proto: entity.Proto{
+					Content: "p1",
+				},
+			},
+			giveRet: &entity.Proto{
+				BaseInfo: entity.BaseInfo{
+					ID: "p1",
+				},
+				Content: "p1",
+			},
+			wantInput: &entity.Proto{
+				BaseInfo: entity.BaseInfo{
+					ID: "p1",
+				},
+				Content: "p1",
+			},
+			wantRet: &entity.Proto{
+				BaseInfo: entity.BaseInfo{
+					ID: "p1",
+				},
+				Content: "p1",
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.caseDesc, func(t *testing.T) {
+			getCalled := false
+			protoStore := &store.MockInterface{}
+			protoStore.On("Update", mock.Anything, mock.Anything, mock.Anything).Run(func(args mock.Arguments) {
+				getCalled = true
+				input := args.Get(1).(*entity.Proto)
+				createIfNotExist := args.Get(2).(bool)
+				assert.Equal(t, tc.wantInput, input)
+				assert.True(t, createIfNotExist)
+			}).Return(tc.giveRet, tc.giveErr)
+
+			protoStore.On("Get", mock.Anything).Run(func(args mock.Arguments) {
+				getCalled = true
+				assert.Equal(t, tc.wantInput.ID, args.Get(0))
+			}).Return(mockData[0], nil)
+
+			h := Handler{protoStore: protoStore}
+			ctx := droplet.NewContext()
+			ctx.SetInput(tc.giveInput)
+			ret, err := h.Update(ctx)
+			assert.Equal(t, tc.getCalled, getCalled)
+			assert.Equal(t, tc.wantRet, ret)
+			assert.Equal(t, tc.wantErr, err)
+		})
+	}
+}
+
+func TestProto_Patch(t *testing.T) {
+	existProto := &entity.Proto{
+		BaseInfo: entity.BaseInfo{
+			ID: "p1",
+		},
+		Content: "p1",
+	}
+
+	patchUpstream := &entity.Proto{
+		BaseInfo: entity.BaseInfo{
+			ID: "p1",
+		},
+		Content: "p2",
+	}
+	patchUpstreamBytes, err := json.Marshal(patchUpstream)
+	assert.Nil(t, err)
+
+	tests := []struct {
+		caseDesc  string
+		getCalled bool
+		giveInput *PatchInput
+		giveErr   error
+		giveRet   interface{}
+		wantInput *entity.Proto
+		wantErr   error
+		wantRet   interface{}
+	}{
+		{
+			caseDesc: "patch success",
+			giveRet: &entity.Proto{
+				BaseInfo: entity.BaseInfo{
+					ID: "p1",
+				},
+				Content: "p2",
+			},
+			giveInput: &PatchInput{
+				ID:      "u1",
+				SubPath: "",
+				Body:    patchUpstreamBytes,
+			},
+			wantInput: &entity.Proto{
+				BaseInfo: entity.BaseInfo{
+					ID: "p1",
+				},
+				Content: "p2",
+			},
+			wantRet: &entity.Proto{
+				BaseInfo: entity.BaseInfo{
+					ID: "p1",
+				},
+				Content: "p2",
+			},
+			getCalled: true,
+		},
+		{
+			caseDesc: "patch success by path",
+			giveInput: &PatchInput{
+				ID:      "p1",
+				SubPath: "/content",
+				Body:    []byte(`"p2"`),
+			},
+			giveRet: &entity.Proto{
+				BaseInfo: entity.BaseInfo{
+					ID: "p1",
+				},
+				Content: "p2",
+			},
+			wantInput: &entity.Proto{
+				BaseInfo: entity.BaseInfo{
+					ID: "p1",
+				},
+				Content: "p2",
+			},
+			wantRet: &entity.Proto{
+				BaseInfo: entity.BaseInfo{
+					ID: "p1",
+				},
+				Content: "p2",
+			},
+			getCalled: true,
+		},
+		{
+			caseDesc: "patch failed, path error",
+			giveInput: &PatchInput{
+				ID:      "u1",
+				SubPath: "error",
+				Body:    []byte("0"),
+			},
+			wantRet: handler.SpecCodeResponse(
+				errors.New("add operation does not apply: doc is missing path: \"error\": missing value")),
+			wantErr: errors.New("add operation does not apply: doc is missing path: \"error\": missing value"),
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.caseDesc, func(t *testing.T) {
+			getCalled := false
+
+			protoStore := &store.MockInterface{}
+			protoStore.On("Get", mock.Anything, mock.Anything).Return(existProto, nil)
+			protoStore.On("Update", mock.Anything, mock.Anything, mock.Anything).Run(func(args mock.Arguments) {
+				getCalled = true
+				input := args.Get(1).(*entity.Proto)
+				createIfNotExist := args.Get(2).(bool)
+				assert.Equal(t, tc.wantInput, input)
+				assert.False(t, createIfNotExist)
+			}).Return(tc.giveRet, tc.giveErr)
+
+			h := Handler{protoStore: protoStore}
+			ctx := droplet.NewContext()
+			ctx.SetInput(tc.giveInput)
+			ret, err := h.Patch(ctx)
+			assert.Equal(t, tc.getCalled, getCalled)
+			assert.Equal(t, tc.wantRet, ret)
+			if tc.wantErr != nil && err != nil {
+				assert.Error(t, tc.wantErr.(error), err.Error())
+			} else {
+				assert.Equal(t, tc.wantErr, err)
+			}
+		})
+	}
+}
+
+func TestProtos_Delete(t *testing.T) {
+	tests := []struct {
+		caseDesc        string
+		giveInput       *BatchDeleteInput
+		giveErr         error
+		wantInput       []string
+		wantErr         error
+		wantRet         interface{}
+		routeMockData   []*entity.Route
+		routeMockErr    error
+		serviceMockData []*entity.Service
+		serviceMockErr  error
+		getCalled       bool
+	}{
+		{
+			caseDesc: "delete success",
+			giveInput: &BatchDeleteInput{
+				IDs: "p1",
+			},
+			wantInput: []string{"p1"},
+			getCalled: true,
+		},
+		{
+			caseDesc: "batch delete success",
+			giveInput: &BatchDeleteInput{
+				IDs: "p1,p2",
+			},
+			wantInput: []string{"p1", "p2"},
+			getCalled: true,
+		},
+		{
+			caseDesc: "delete failed",
+			giveInput: &BatchDeleteInput{
+				IDs: "p1",
+			},
+			giveErr:   fmt.Errorf("delete error"),
+			wantInput: []string{"p1"},
+			wantRet:   handler.SpecCodeResponse(fmt.Errorf("delete error")),
+			wantErr:   fmt.Errorf("delete error"),
+			getCalled: true,
+		},
+		{
+			caseDesc: "delete failed, route is using",
+			giveInput: &BatchDeleteInput{
+				IDs: "p1",
+			},
+			wantInput: []string{"s1"},
+			routeMockData: []*entity.Route{
+				&entity.Route{
+					BaseInfo: entity.BaseInfo{
+						ID:         "r1",
+						CreateTime: 1609746531,
+					},
+					Name: "route1",
+					Desc: "test_route",
+					Plugins: map[string]interface{}{
+						"grpc-transcode": map[string]interface{}{
+							"disable":  false,
+							"proto_id": "p1",
+						},
+					},
+				},
+			},
+			routeMockErr: nil,
+			getCalled:    false,
+			wantRet:      &data.SpecCodeResponse{StatusCode: 400},
+			wantErr:      errors.New("proto used check invalid: route: r1 is using this proto"),
+		},
+		{
+			caseDesc: "delete failed, route list error",
+			giveInput: &BatchDeleteInput{
+				IDs: "p1",
+			},
+			wantInput:     []string{"p1"},
+			routeMockData: nil,
+			routeMockErr:  errors.New("route list error"),
+			wantRet:       handler.SpecCodeResponse(errors.New("route list error")),
+			wantErr:       errors.New("route list error"),
+			getCalled:     false,
+		},
+		{
+			caseDesc: "delete failed, service is using",
+			giveInput: &BatchDeleteInput{
+				IDs: "p1",
+			},
+			wantInput: []string{"s1"},
+			serviceMockData: []*entity.Service{
+				&entity.Service{
+					BaseInfo: entity.BaseInfo{
+						ID:         "s1",
+						CreateTime: 1609746531,
+					},
+					Name: "service1",
+					Plugins: map[string]interface{}{
+						"grpc-transcode": map[string]interface{}{
+							"disable":  false,
+							"proto_id": "p1",
+						},
+					},
+				},
+			},
+			serviceMockErr: nil,
+			getCalled:      false,
+			wantRet:        &data.SpecCodeResponse{StatusCode: 400},
+			wantErr:        errors.New("proto used check invalid: service: s1 is using this proto"),
+		},
+		{
+			caseDesc: "delete failed, service list error",
+			giveInput: &BatchDeleteInput{
+				IDs: "p1",
+			},
+			wantInput:      []string{"p1"},
+			serviceMockErr: errors.New("service list error"),
+			wantRet:        handler.SpecCodeResponse(errors.New("service list error")),
+			wantErr:        errors.New("service list error"),
+			getCalled:      false,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.caseDesc, func(t *testing.T) {
+			getCalled := false
+			protoStore := &store.MockInterface{HubKey: store.HubKeyProto}
+			protoStore.On("BatchDelete", mock.Anything, mock.Anything).Run(func(args mock.Arguments) {
+				getCalled = true
+				input := args.Get(1).([]string)
+				assert.Equal(t, tc.wantInput, input)
+			}).Return(tc.giveErr)
+
+			routeStore := &store.MockInterface{HubKey: store.HubKeyRoute}
+			routeStore.On("List", mock.Anything).Return(func(input store.ListInput) *store.ListOutput {
+				var returnData []interface{}
+				for _, c := range tc.routeMockData {
+					if input.Predicate(c) {
+						if input.Format == nil {
+							returnData = append(returnData, c)
+							continue
+						}
+
+						returnData = append(returnData, input.Format(c))
+					}
+				}
+
+				return &store.ListOutput{
+					Rows:      returnData,
+					TotalSize: len(returnData),
+				}
+			}, tc.routeMockErr)
+
+			serviceStore := &store.MockInterface{HubKey: store.HubKeyService}
+			serviceStore.On("List", mock.Anything).Return(func(input store.ListInput) *store.ListOutput {
+				var returnData []interface{}
+				for _, c := range tc.serviceMockData {
+					if input.Predicate(c) {
+						if input.Format == nil {
+							returnData = append(returnData, c)
+							continue
+						}
+
+						returnData = append(returnData, input.Format(c))
+					}
+				}
+
+				return &store.ListOutput{
+					Rows:      returnData,
+					TotalSize: len(returnData),
+				}
+			}, tc.serviceMockErr)
+
+			consumerStore := &store.MockInterface{HubKey: store.HubKeyConsumer}
+			consumerStore.On("List", mock.Anything).Return(&store.ListOutput{Rows: nil, TotalSize: 0}, nil)
+
+			pluginConfigStore := &store.MockInterface{HubKey: store.HubKeyPluginConfig}
+			pluginConfigStore.On("List", mock.Anything).Return(&store.ListOutput{Rows: nil, TotalSize: 0}, nil)
+
+			globalRuleStore := &store.MockInterface{HubKey: store.HubKeyGlobalRule}
+			globalRuleStore.On("List", mock.Anything).Return(&store.ListOutput{Rows: nil, TotalSize: 0}, nil)
+
+			h := Handler{
+				protoStore:        protoStore,
+				routeStore:        routeStore,
+				serviceStore:      serviceStore,
+				consumerStore:     consumerStore,
+				pluginConfigStore: pluginConfigStore,
+				globalRuleStore:   globalRuleStore,
+			}
+			ctx := droplet.NewContext()
+			ctx.SetInput(tc.giveInput)
+			ret, err := h.BatchDelete(ctx)
+			assert.Equal(t, tc.getCalled, getCalled)
 			assert.Equal(t, tc.wantRet, ret)
 			assert.Equal(t, tc.wantErr, err)
 		})
