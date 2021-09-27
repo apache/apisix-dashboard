8d50e0b43fa72cbb7c5205be95749803db572242
bzp2010 <bzp2010@apache.org>
2021-09-27 14:21:41 +0800

    feat: add service hosts property

--- web/src/pages/Service/locales/en-US.ts
+++ web/src/pages/Service/locales/en-US.ts
@@ -22,6 +22,8 @@ export default {
   'page.service.description':
     'A service consists of a combination of public plugin configuration and upstream target information in a route. Services are associated with Routes and Upstreams, and a service can correspond to a set of upstream nodes and can be bound by multiple routes.',
   'page.service.fields.name.required': 'Please enter service name',
+  'page.service.fields.hosts': 'Hosts',
+  'page.service.fields.hosts.placeholder': 'Please enter service hosts',
   'page.service.create': 'Create Service',
   'page.service.configure': 'Configure Service',
 };
