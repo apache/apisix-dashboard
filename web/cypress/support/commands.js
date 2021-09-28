27e649ad1c3ab103a8833c8bdca6a8f37ff41d29
bzp2010 <bzp2010@apache.org>
2021-09-28 20:37:54 +0800

    feat: try to reduce time cost

--- web/cypress/support/commands.js
+++ web/cypress/support/commands.js
@@ -73,7 +73,7 @@ Cypress.Commands.add('configurePlugin', ({ name, cases }) => {
     .invoke('text')
     .then((text) => {
       if (text === 'Form') {
-        cy.wait(5000);
+        cy.wait(1000);
         cy.get(domSelector.monacoMode).should('be.visible');
         cy.get(domSelector.monacoMode).click();
         cy.get(domSelector.selectDropdown).should('be.visible');
@@ -92,7 +92,7 @@ Cypress.Commands.add('configurePlugin', ({ name, cases }) => {
     .then((text) => {
       if (text === 'Form') {
         // FIXME: https://github.com/cypress-io/cypress/issues/7306
-        cy.wait(5000);
+        cy.wait(1000);
         cy.get(domSelector.monacoMode).should('be.visible');
         cy.get(domSelector.monacoMode).click();
         cy.get(domSelector.selectDropdown).should('be.visible');
