9a7e6e4e49de0bbe8cbff0587e6ae84e232e0d79
bzp2010 <bzp2010@apache.org>
2021-10-08 17:15:39 +0800

    feat: add GetPlugin interface

--- api/internal/core/entity/interface.go
+++ api/internal/core/entity/interface.go
@@ -19,3 +19,27 @@ package entity
 type GetBaseInfo interface {
 	GetBaseInfo() *BaseInfo
 }
+
+type GetPlugins interface {
+	GetPlugins() map[string]interface{}
+}
+
+func (r *Route) GetPlugins() map[string]interface{} {
+	return r.Plugins
+}
+
+func (s *Service) GetPlugins() map[string]interface{} {
+	return s.Plugins
+}
+
+func (c *Consumer) GetPlugins() map[string]interface{} {
+	return c.Plugins
+}
+
+func (g *GlobalPlugins) GetPlugins() map[string]interface{} {
+	return g.Plugins
+}
+
+func (p *PluginConfig) GetPlugins() map[string]interface{} {
+	return p.Plugins
+}
