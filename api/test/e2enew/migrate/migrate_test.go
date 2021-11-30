6cef539ce88f48642a688a03a2c716d863258e3f
Zeping Bai <bzp2010@apache.org>
2021-11-30 09:50:11 +0800

    fix: migrate testcase CI crash (#2228)

--- api/test/e2enew/migrate/migrate_test.go
+++ api/test/e2enew/migrate/migrate_test.go
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package migrate
+package migrate_test
 
 import (
 	"bytes"
@@ -23,9 +23,11 @@ import (
 	"encoding/json"
 	"hash/crc32"
 	"net/http"
+	"time"
 
-	"github.com/onsi/ginkgo"
-	"github.com/onsi/gomega"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
 
 	"github.com/apisix/manager-api/test/e2enew/base"
 )
@@ -53,21 +55,92 @@ type response struct {
 	} `json:"Data"`
 }
 
-var _ = ginkgo.Describe("Migrate", func() {
+var _ = Describe("Migrate", func() {
 	var exportData []byte
 
-	ginkgo.It("prepare config data", prepareConfigData)
-	ginkgo.It("export config success", func() {
+	DescribeTable("Prepare config data",
+		func(tc base.HttpTestCase) {
+			base.RunTestCase(tc)
+		},
+		Entry("create test route", base.HttpTestCase{
+			Object: base.ManagerApiExpect(),
+			Method: http.MethodPut,
+			Path:   "/apisix/admin/routes/r1",
+			Body: `{
+				"name": "route",
+				"uri": "/hello_",
+				"upstream": {
+					"nodes": {
+						"` + base.UpstreamIp + `:1980": 1
+					},
+					"type": "roundrobin"
+				}
+			}`,
+			Headers:      map[string]string{"Authorization": base.GetToken()},
+			ExpectStatus: http.StatusOK,
+		}),
+		Entry("create test upstream", base.HttpTestCase{
+			Object: base.ManagerApiExpect(),
+			Method: http.MethodPut,
+			Path:   "/apisix/admin/upstreams/u1",
+			Body: `{
+				"name": "upstream",
+				"nodes": [
+					{
+						"host": "` + base.UpstreamIp + `",
+						"port": 1980,
+						"weight": 1
+					}
+				],
+				"type": "roundrobin"
+			}`,
+			Headers:      map[string]string{"Authorization": base.GetToken()},
+			ExpectStatus: http.StatusOK,
+		}),
+		Entry("create test service", base.HttpTestCase{
+			Object: base.ManagerApiExpect(),
+			Method: http.MethodPut,
+			Path:   "/apisix/admin/services/s1",
+			Body: `{
+  				"name": "service",
+  				"upstream": {
+    				"nodes": [
+      					{
+        					"host": "` + base.UpstreamIp + `",
+        					"port": 1980,
+        					"weight": 1
+      					},
+      					{
+							"host": "` + base.UpstreamIp + `",
+        					"port": 1981,
+        					"weight": 2
+      					},
+      					{
+        					"host": "` + base.UpstreamIp + `",
+        					"port": 1982,
+        					"weight": 3
+      					}
+    				],
+    				"type": "roundrobin"
+  				}
+			}`,
+			Headers:      map[string]string{"Authorization": base.GetToken()},
+			ExpectStatus: http.StatusOK,
+			Sleep:        time.Second * 1,
+		}),
+	)
+
+	It("export config success", func() {
 		req := base.ManagerApiExpect().GET("/apisix/admin/migrate/export")
 		resp := req.Expect()
 		resp.Status(http.StatusOK)
 		exportData = []byte(resp.Body().Raw())
 		data := exportData[:len(exportData)-checksumLength]
 		checksum := binary.BigEndian.Uint32(exportData[len(exportData)-checksumLength:])
-		gomega.Expect(checksum).Should(gomega.Equal(crc32.ChecksumIEEE(data)))
+		Expect(checksum).Should(Equal(crc32.ChecksumIEEE(data)))
 	})
 
-	ginkgo.It("import config conflict and return", func() {
+	It("import config conflict and return", func() {
 		req := base.ManagerApiExpect().POST("/apisix/admin/migrate/import")
 		buffer := bytes.NewBuffer(exportData)
 		req.WithMultipart().WithForm(map[string]string{"mode": "return"})
@@ -76,14 +149,14 @@ var _ = ginkgo.Describe("Migrate", func() {
 		resp.Status(http.StatusOK)
 		rsp := &response{}
 		err := json.Unmarshal([]byte(resp.Body().Raw()), rsp)
-		gomega.Expect(err).Should(gomega.BeNil())
-		gomega.Expect(rsp.Code).Should(gomega.Equal(20001))
-		gomega.Expect(len(rsp.Data.ConflictItems.Routes)).Should(gomega.Equal(1))
-		gomega.Expect(len(rsp.Data.ConflictItems.Upstreams)).Should(gomega.Equal(1))
-		gomega.Expect(len(rsp.Data.ConflictItems.Services)).Should(gomega.Equal(1))
+		Expect(err).Should(BeNil())
+		Expect(rsp.Code).Should(Equal(20001))
+		Expect(len(rsp.Data.ConflictItems.Routes)).Should(Equal(1))
+		Expect(len(rsp.Data.ConflictItems.Upstreams)).Should(Equal(1))
+		Expect(len(rsp.Data.ConflictItems.Services)).Should(Equal(1))
 	})
 
-	ginkgo.It("import config conflict and skip", func() {
+	It("import config conflict and skip", func() {
 		req := base.ManagerApiExpect().POST("/apisix/admin/migrate/import")
 		buffer := bytes.NewBuffer(exportData)
 		req.WithMultipart().WithForm(map[string]string{"mode": "skip"})
@@ -92,11 +165,11 @@ var _ = ginkgo.Describe("Migrate", func() {
 		resp.Status(http.StatusOK)
 		rsp := &response{}
 		err := json.Unmarshal([]byte(resp.Body().Raw()), rsp)
-		gomega.Expect(err).Should(gomega.BeNil())
-		gomega.Expect(rsp.Code).Should(gomega.Equal(0))
+		Expect(err).Should(BeNil())
+		Expect(rsp.Code).Should(Equal(0))
 	})
 
-	ginkgo.It("import config conflict and overwrite", func() {
+	It("import config conflict and overwrite", func() {
 		req := base.ManagerApiExpect().POST("/apisix/admin/migrate/import")
 		buffer := bytes.NewBuffer(exportData)
 		req.WithMultipart().WithForm(map[string]string{"mode": "overwrite"})
@@ -105,11 +178,11 @@ var _ = ginkgo.Describe("Migrate", func() {
 		resp.Status(http.StatusOK)
 		rsp := &response{}
 		err := json.Unmarshal([]byte(resp.Body().Raw()), rsp)
-		gomega.Expect(err).Should(gomega.BeNil())
-		gomega.Expect(rsp.Code).Should(gomega.Equal(0))
+		Expect(err).Should(BeNil())
+		Expect(rsp.Code).Should(Equal(0))
 	})
 
-	ginkgo.It("request hit route r1", func() {
+	It("request hit route r1", func() {
 		base.RunTestCase(base.HttpTestCase{
 			Object:       base.APISIXExpect(),
 			Method:       http.MethodGet,
@@ -120,9 +193,34 @@ var _ = ginkgo.Describe("Migrate", func() {
 		})
 	})
 
-	ginkgo.It("delete all config", deleteConfigData)
+	DescribeTable("Cleanup config data",
+		func(tc base.HttpTestCase) {
+			base.RunTestCase(tc)
+		},
+		Entry("remove test route", base.HttpTestCase{
+			Object:       base.ManagerApiExpect(),
+			Method:       http.MethodDelete,
+			Path:         "/apisix/admin/routes/r1",
+			Headers:      map[string]string{"Authorization": base.GetToken()},
+			ExpectStatus: http.StatusOK,
+		}),
+		Entry("remove test upstream", base.HttpTestCase{
+			Object:       base.ManagerApiExpect(),
+			Method:       http.MethodDelete,
+			Path:         "/apisix/admin/upstreams/u1",
+			Headers:      map[string]string{"Authorization": base.GetToken()},
+			ExpectStatus: http.StatusOK,
+		}),
+		Entry("remove test service", base.HttpTestCase{
+			Object:       base.ManagerApiExpect(),
+			Method:       http.MethodDelete,
+			Path:         "/apisix/admin/services/s1",
+			Headers:      map[string]string{"Authorization": base.GetToken()},
+			ExpectStatus: http.StatusOK,
+		}),
+	)
 
-	ginkgo.It("delete imported route failed", func() {
+	It("delete imported route failed", func() {
 		base.RunTestCase(base.HttpTestCase{
 			Object:       base.ManagerApiExpect(),
 			Method:       http.MethodDelete,
@@ -132,7 +230,7 @@ var _ = ginkgo.Describe("Migrate", func() {
 		})
 	})
 
-	ginkgo.It("request route r1 not found", func() {
+	It("request route r1 not found", func() {
 		base.RunTestCase(base.HttpTestCase{
 			Object:       base.APISIXExpect(),
 			Method:       http.MethodGet,
@@ -142,7 +240,7 @@ var _ = ginkgo.Describe("Migrate", func() {
 		})
 	})
 
-	ginkgo.It("import config success", func() {
+	It("import config success", func() {
 		req := base.ManagerApiExpect().POST("/apisix/admin/migrate/import")
 		buffer := bytes.NewBuffer(exportData)
 		req.WithMultipart().WithForm(map[string]string{"mode": "return"})
@@ -151,11 +249,11 @@ var _ = ginkgo.Describe("Migrate", func() {
 		resp.Status(http.StatusOK)
 		rsp := &response{}
 		err := json.Unmarshal([]byte(resp.Body().Raw()), rsp)
-		gomega.Expect(err).Should(gomega.BeNil())
-		gomega.Expect(rsp.Code).Should(gomega.Equal(0))
+		Expect(err).Should(BeNil())
+		Expect(rsp.Code).Should(Equal(0))
 	})
 
-	ginkgo.It("request hit route r1", func() {
+	It("request hit route r1", func() {
 		base.RunTestCase(base.HttpTestCase{
 			Object:       base.APISIXExpect(),
 			Method:       http.MethodGet,
@@ -166,7 +264,7 @@ var _ = ginkgo.Describe("Migrate", func() {
 		})
 	})
 
-	ginkgo.It("delete imported route success", func() {
+	It("delete imported route success", func() {
 		base.RunTestCase(base.HttpTestCase{
 			Object:       base.ManagerApiExpect(),
 			Method:       http.MethodDelete,
@@ -176,17 +274,17 @@ var _ = ginkgo.Describe("Migrate", func() {
 		})
 	})
 
-	ginkgo.It("delete imported upstream success", func() {
+	It("delete imported upstream success", func() {
 		base.RunTestCase(base.HttpTestCase{
 			Object:       base.ManagerApiExpect(),
 			Method:       http.MethodDelete,
-			Path:         "/apisix/admin/upstreams/1",
+			Path:         "/apisix/admin/upstreams/u1",
 			Headers:      map[string]string{"Authorization": base.GetToken()},
 			ExpectStatus: http.StatusOK,
 		})
 	})
 
-	ginkgo.It("delete imported service success", func() {
+	It("delete imported service success", func() {
 		base.RunTestCase(base.HttpTestCase{
 			Object:       base.ManagerApiExpect(),
 			Method:       http.MethodDelete,
@@ -197,78 +295,3 @@ var _ = ginkgo.Describe("Migrate", func() {
 	})
 
 })
-
-func prepareConfigData() {
-	headers := map[string]string{
-		"Content-Type":  "application/json",
-		"Authorization": base.GetToken(),
-	}
-	_, statusCode, err := base.HttpPut(base.ManagerAPIHost+"/apisix/admin/routes/r1", headers, `{
-		"name": "route1",
-		"uri": "/hello_",
-		"upstream": {
-			"nodes": {
-				"`+base.UpstreamIp+`:1980": 1
-			},
-			"type": "roundrobin"
-		}
-	}`)
-	gomega.Expect(statusCode).Should(gomega.Equal(http.StatusOK))
-	gomega.Expect(err).Should(gomega.BeNil())
-
-	_, statusCode, err = base.HttpPut(base.ManagerAPIHost+"/apisix/admin/upstreams/1", headers, `{
-		"name": "upstream1",
-		"nodes": [
-			{
-				"host": "`+base.UpstreamIp+`",
-				"port": 1980,
-				"weight": 1
-			}
-		],
-		"type": "roundrobin"
-	}`)
-	gomega.Expect(statusCode).Should(gomega.Equal(http.StatusOK))
-	gomega.Expect(err).Should(gomega.BeNil())
-
-	_, statusCode, err = base.HttpPut(base.ManagerAPIHost+"/apisix/admin/services/s1", headers, `{
-  "name": "testservice",
-  "upstream": {
-    "nodes": [
-      {
-        "host": "`+base.UpstreamIp+`",
-        "port": 1980,
-        "weight": 1
-      },
-      {
-        "host": "`+base.UpstreamIp+`",
-        "port": 1981,
-        "weight": 2
-      },
-      {
-        "host": "`+base.UpstreamIp+`",
-        "port": 1982,
-        "weight": 3
-      }
-    ],
-    "type": "roundrobin"
-  }
-}`)
-	gomega.Expect(statusCode).Should(gomega.Equal(http.StatusOK))
-	gomega.Expect(err).Should(gomega.BeNil())
-}
-
-func deleteConfigData() {
-	headers := map[string]string{
-		"Content-Type":  "application/json",
-		"Authorization": base.GetToken(),
-	}
-	_, statusCode, err := base.HttpDelete(base.ManagerAPIHost+"/apisix/admin/routes/r1", headers)
-	gomega.Expect(statusCode).Should(gomega.Equal(http.StatusOK))
-	gomega.Expect(err).Should(gomega.BeNil())
-	_, statusCode, err = base.HttpDelete(base.ManagerAPIHost+"/apisix/admin/upstreams/1", headers)
-	gomega.Expect(statusCode).Should(gomega.Equal(http.StatusOK))
-	gomega.Expect(err).Should(gomega.BeNil())
-	_, statusCode, err = base.HttpDelete(base.ManagerAPIHost+"/apisix/admin/services/s1", headers)
-	gomega.Expect(statusCode).Should(gomega.Equal(http.StatusOK))
-	gomega.Expect(err).Should(gomega.BeNil())
-}
