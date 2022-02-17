aa6e7a54d7cc1b438a95d44b39dbda9826481276
oil欧呦 <2228586315@qq.com>
2022-02-17 10:04:50 +0800

    fix: fix license content

--- web/src/pages/Proto/components/ProtoDrawer/index.less
+++ web/src/pages/Proto/components/ProtoDrawer/index.less
@@ -1,5 +1,4 @@
 /*
-import { Drawer } from 'antd';
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
