9a158cbdd1ea280cab5da8227dc2bac7843855bb
juxianzheng <874123984@qq.com>
2022-02-17 20:48:14 +0800

    fix: format code

--- web/src/components/Plugin/UI/authz-keycloak.tsx
+++ web/src/components/Plugin/UI/authz-keycloak.tsx
@@ -1,92 +1,108 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import { FormInstance, Input } from "antd";
 import React from "react";
 import { useIntl } from "umi";
 import Form from "antd/es/form";
 
 type Props = {
-    form: FormInstance;
-    schema: Record<string, any> | undefined;
-    ref?: any;
+  form: FormInstance;
+  schema: Record<string, any> | undefined;
+  ref?: any;
 }
 
 const FORM_ITEM_LAYOUT = {
-    labelCol: {
-        span: 7,
-    },
-    wrapperCol: {
-        span: 10,
-    },
+  labelCol: {
+    span: 7,
+  },
+  wrapperCol: {
+    span: 10,
+  },
 };
 
 const AuthzKeycloak: React.FC<Props> = ({ form, schema }) => {
-    const { formatMessage } = useIntl();
-    const properties = schema?.properties
+  const { formatMessage } = useIntl();
+  const properties = schema?.properties
 
-    return (
-        <Form form={form} {...FORM_ITEM_LAYOUT}>
-            <Form.Item
-                name="token_endpoint"
-                label="token_endpoint"
-                rules={[
-                    {
-                        required: true,
-                        message: `${formatMessage({ id: 'component.global.pleaseEnter' })} token_endpoint`,
-                    },
-                ]}
-                initialValue={properties.token_endpoint.default}
-                tooltip={formatMessage({ id: 'component.pauginForm.authz-keycloak.token_endpoint.tooltip' })}
-            >
-                <Input />
-            </Form.Item>
-            <Form.Item
-                name="grant_type"
-                label="grant_type"
-                initialValue={"urn:ietf:params:oauth:grant-type:uma-ticket"}
-                tooltip={formatMessage({ id: 'component.pauginForm.authz-keycloak.grant_type.tooltip' })}
-            >
-                <Input />
-            </Form.Item>
-            <Form.Item
-                name="audience"
-                label="audience"
-                tooltip={formatMessage({ id: 'component.pauginForm.authz-keycloak.audience.tooltip' })}
-            >
-                <Input />
-            </Form.Item>
-            <Form.Item
-                name="permissions"
-                label="permissions"
-                tooltip={formatMessage({ id: 'component.pauginForm.authz-keycloak.permissions.tooltip' })}
-            >
-                <Input />
-            </Form.Item>
-            <Form.Item
-                name="timeout"
-                label="timeout"
-                initialValue={3000}
-                tooltip={formatMessage({ id: 'component.pauginForm.authz-keycloak.timeout.tooltip' })}
-            >
-                <Input />
-            </Form.Item>
-            <Form.Item
-                name="ssl_verify"
-                label="ssl_verify"
-                initialValue={"ture"}
-                tooltip={formatMessage({ id: 'component.pauginForm.authz-keycloak.ssl_verify.tooltip' })}
-            >
-                <Input />
-            </Form.Item>
-            <Form.Item
-                name="policy_enforcement_mode"
-                label="policy_enforcement_mode"
-                initialValue={"ENFORCING"}
-                tooltip={formatMessage({ id: 'component.pauginForm.authz-keycloak.policy_enforcement_mode.tooltip' })}
-            >
-                <Input />
-            </Form.Item>
+  return (
+    <Form form={form} {...FORM_ITEM_LAYOUT}>
+      <Form.Item
+        name="token_endpoint"
+        label="token_endpoint"
+        rules={[
+          {
+            required: true,
+            message: `${formatMessage({ id: 'component.global.pleaseEnter' })} token_endpoint`,
+          },
+        ]}
+        initialValue={properties.token_endpoint.default}
+        tooltip={formatMessage({ id: 'component.pauginForm.authz-keycloak.token_endpoint.tooltip' })}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="grant_type"
+        label="grant_type"
+        initialValue={"urn:ietf:params:oauth:grant-type:uma-ticket"}
+        tooltip={formatMessage({ id: 'component.pauginForm.authz-keycloak.grant_type.tooltip' })}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="audience"
+        label="audience"
+        tooltip={formatMessage({ id: 'component.pauginForm.authz-keycloak.audience.tooltip' })}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="permissions"
+        label="permissions"
+        tooltip={formatMessage({ id: 'component.pauginForm.authz-keycloak.permissions.tooltip' })}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="timeout"
+        label="timeout"
+        initialValue={3000}
+        tooltip={formatMessage({ id: 'component.pauginForm.authz-keycloak.timeout.tooltip' })}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="ssl_verify"
+        label="ssl_verify"
+        initialValue={"ture"}
+        tooltip={formatMessage({ id: 'component.pauginForm.authz-keycloak.ssl_verify.tooltip' })}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="policy_enforcement_mode"
+        label="policy_enforcement_mode"
+        initialValue={"ENFORCING"}
+        tooltip={formatMessage({ id: 'component.pauginForm.authz-keycloak.policy_enforcement_mode.tooltip' })}
+      >
+        <Input />
+      </Form.Item>
 
-        </Form >
-    )
+    </Form >
+  )
 }
 
-export default AuthzKeycloak 
+export default AuthzKeycloak
