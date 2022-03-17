9e1d60ed04dcf4d6284983b53f11f88bd9ebfd73
Nicolas Frankel <nicolas@frankel.ch>
2022-03-17 09:22:52 +0100

    fix: Fix Copyright notice
      From Wikipedia (https://en.wikipedia.org/wiki/Copyright#Copyright_notice):
    
      1) United States law required the use of a copyright notice, consisting of the
      copyright symbol (©, the letter C inside a circle), the abbreviation "Copr.",
      or the word "Copyright", followed by the year of the first publication of the
      work and the name of the copyright holder.
    
      2) In 1989 the United States enacted the Berne Convention Implementation Act,
      amending the 1976 Copyright Act to conform to most of the provisions of the
      Berne Convention. As a result, the use of copyright notices has become
      optional to claim copyright, because the Berne Convention makes copyright
      automatic.

--- web/src/components/Footer/index.tsx
+++ web/src/components/Footer/index.tsx
@@ -20,7 +20,7 @@ import { DefaultFooter } from '@ant-design/pro-layout';
 
 export default () => (
   <DefaultFooter
-    copyright="2021 Apache APISIX"
+    copyright="2019 Apache APISIX"
     links={[
       {
         key: 'GitHub',
