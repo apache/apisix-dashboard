2287f79f030fad9d557daab75a165ee70b8b9cd9
okaybase <75366457+okaybase@users.noreply.github.com>
2022-04-09 22:53:16 +0800

    chore: update the year of copyright and fix typo (#2417)
    * fix typo
    * Update copyright year
    
    Co-authored-by: Bisakh <bisakhmondal00@gmail.com>

--- web/src/components/Footer/index.tsx
+++ web/src/components/Footer/index.tsx
@@ -20,7 +20,7 @@ import { DefaultFooter } from '@ant-design/pro-layout';
 
 export default () => (
   <DefaultFooter
-    copyright="2021 Apache APISIX"
+    copyright={`${new Date().getFullYear()} Apache APISIX`}
     links={[
       {
         key: 'GitHub',
