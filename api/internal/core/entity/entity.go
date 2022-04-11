f6ac1f4ae561f6fbbd3f179f7eeb3bdfc830bb05
tyltr <tylitianrui@126.com>
2022-04-11 11:13:21 +0800

    data  type

--- api/internal/core/entity/entity.go
+++ api/internal/core/entity/entity.go
@@ -133,7 +133,7 @@ type Active struct {
 	Host                   string       `json:"host,omitempty"`
 	Port                   int          `json:"port,omitempty"`
 	HTTPPath               string       `json:"http_path,omitempty"`
-	HTTPSVerifyCertificate string       `json:"https_verify_certificate,omitempty"`
+	HTTPSVerifyCertificate bool         `json:"https_verify_certificate,omitempty"`
 	Healthy                Healthy      `json:"healthy,omitempty"`
 	UnHealthy              UnHealthy    `json:"unhealthy,omitempty"`
 	ReqHeaders             []string     `json:"req_headers,omitempty"`
