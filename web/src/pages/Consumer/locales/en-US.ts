a2fe71d0644927eb71ef3dd06b902ea9f4ce81f6
洋洋 <1071969365@qq.com>
2022-06-06 10:37:21 +0800

    fix: Users can create a Consumer in Dashboard without enabling the plugin (#2442)

--- web/src/pages/Consumer/locales/en-US.ts
+++ web/src/pages/Consumer/locales/en-US.ts
@@ -18,14 +18,12 @@ export default {
   'page.consumer.form.itemRuleMessage.username':
     'Maximum length is 100, only letters, numbers and _ are supported.',
   'page.consumer.form.itemExtraMessage.username': 'Name should be unique',
-  'page.consumer.notification.warning.enableAuthenticationPlugin':
-    'Please enable at least one of the following authentication plugin:',
   'page.consumer.username': 'Name',
   'page.consumer.username.required': "Please enter the Consumer's name",
   'page.consumer.updateTime': 'Update Time',
   'page.consumer.list': 'Consumer List',
   'page.consumer.description':
-    'Consumers are the consumers of Routes, e.g developers, end users, API calls, etc. When creating a consumer, you need to bind at least one Authentication plugin.',
+    'Consumers are the consumers of Routes, e.g developers, end users, API calls, etc.',
   'page.consumer.create': 'Create Consumer',
   'page.consumer.configure': 'Configure Consumer',
 };
