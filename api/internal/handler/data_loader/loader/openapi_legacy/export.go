4b3f2b80a1970ad2485cddff65a6f8b0a7462967
白泽平 <bzp2010@apache.org>
2022-06-10 13:56:25 +0800

    chore: add license header

--- api/internal/handler/data_loader/loader/openapi_legacy/export.go
+++ api/internal/handler/data_loader/loader/openapi_legacy/export.go
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package openapi_legacy
 
 import (
@@ -38,17 +54,17 @@ var (
 )
 
 func (o Loader) Export(data loader.DataSets) (interface{}, error) {
-	routes = make([]*entity.Route, 0)
-	upstreams = make([]*entity.Upstream, 0)
-	services = make([]*entity.Service, 0)
-	for _, route := range data.Routes {
-		routes = append(routes, &route)
+	routes = []*entity.Route{}
+	upstreams = []*entity.Upstream{}
+	services = []*entity.Service{}
+	for i := range data.Routes {
+		routes = append(routes, &data.Routes[i])
 	}
-	for _, upstream := range data.Upstreams {
-		upstreams = append(upstreams, &upstream)
+	for i := range data.Upstreams {
+		upstreams = append(upstreams, &data.Upstreams[i])
 	}
-	for _, service := range data.Services {
-		services = append(services, &service)
+	for i := range data.Services {
+		services = append(services, &data.Services[i])
 	}
 
 	swagger, err := RouteToOpenAPI3()
