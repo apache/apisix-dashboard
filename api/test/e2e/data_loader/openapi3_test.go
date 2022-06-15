4e2d925fc16e18199878864ef338764bf707da6e
白泽平 <bzp2010@apache.org>
2022-06-15 17:00:36 +0800

    chore: add license header

--- api/test/e2e/data_loader/openapi3_test.go
+++ api/test/e2e/data_loader/openapi3_test.go
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package data_loader_test
 
 import (
@@ -37,7 +53,7 @@ var _ = Describe("OpenAPI 3", func() {
 
 			r = r.Get("data")
 			for s, result := range r.Map() {
-				if s == "route" || s == "upstream" {
+				if s == "route" {
 					Expect(result.Get("total").Uint()).To(Equal(uint64(1)))
 					Expect(result.Get("failed").Uint()).To(Equal(uint64(0)))
 				}
@@ -63,7 +79,7 @@ var _ = Describe("OpenAPI 3", func() {
 
 			r = r.Get("data")
 			for s, result := range r.Map() {
-				if s == "route" || s == "upstream" {
+				if s == "route" {
 					Expect(result.Get("total").Uint()).To(Equal(uint64(1)))
 					Expect(result.Get("failed").Uint()).To(Equal(uint64(0)))
 				}
