9e80831bb0116e48a8429dc39725de0f7b4f444a
Feng Han <forestcdkfox@gmail.com>
2022-07-16 11:18:35 +0000

    postman test 1

--- api/test/e2e/data_loader/postman_test.go
+++ api/test/e2e/data_loader/postman_test.go
@@ -4,6 +4,7 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
+	"github.com/tidwall/gjson"
 )
 
 var _ = Describe("Postman Collection v2.1", func() {
@@ -11,8 +12,34 @@ var _ = Describe("Postman Collection v2.1", func() {
 		func(f func()) {
 			f()
 		},
-		Entry("Postman-API101.yaml", func() {
+		Entry("API_101.postman_collection", func() {
+			path, err := filepath.Abs("../../testdata/import/API_101.postman_collection")
+			Expect(err).To(BeNil())
+			req := base.ManagerApiExpect().POST("/apisix/admin/import/routes")
+			req.WithMultipart().WithForm(map[string]string{
+				"type":         "postman",
+				"task_name":    "api_101_postman",
+				"_file":        "API_101.postman_collection",
+				"merge_method": "false",
+			})
+			req.WithMultipart().WithFile("file", path)
+
+			req.WithHeader("Authorization", base.GetToken())
+
+			resp := req.Expect()
+			resp.Status(http.StatusOK)
+			r := gjson.ParseBytes([]byte(resp.Body().Raw()))
+
+			Expect(r.Get("code").Uint()).To(Equal(uint64(0)))
+			r = r.Get("data")
+			r = r.Get("data")
+			for s, result := range r.Map() {
+				if s == "route" {
+					Expect(result.Get("total").Uint()).To(Equal(uint64(1)))
+					Expect(result.Get("failed").Uint()).To(Equal(uint64(0)))
+				}
+			}
+
 		}),
 	)
 })
-
