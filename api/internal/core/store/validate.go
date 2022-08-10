0ea581921e22be18851e10f2429271a385f20acf
Zeping Bai <bzp2010@apache.org>
2022-08-10 15:50:36 +0800

    chore: remove unused validator (#2575)

--- api/internal/core/store/validate.go
+++ api/internal/core/store/validate.go
@@ -18,9 +18,7 @@ package store
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
-	"io/ioutil"
 
 	"github.com/xeipuuv/gojsonschema"
 	"go.uber.org/zap/buffer"
@@ -32,42 +30,6 @@ import (
 
 type Validator interface {
 	Validate(obj interface{}) error
-}
-type JsonSchemaValidator struct {
-	schema *gojsonschema.Schema
-}
-
-func NewJsonSchemaValidator(jsonPath string) (Validator, error) {
-	bs, err := ioutil.ReadFile(jsonPath)
-	if err != nil {
-		return nil, fmt.Errorf("get abs path failed: %s", err)
-	}
-	s, err := gojsonschema.NewSchema(gojsonschema.NewStringLoader(string(bs)))
-	if err != nil {
-		return nil, fmt.Errorf("new schema failed: %s", err)
-	}
-	return &JsonSchemaValidator{
-		schema: s,
-	}, nil
-}
-
-func (v *JsonSchemaValidator) Validate(obj interface{}) error {
-	ret, err := v.schema.Validate(gojsonschema.NewGoLoader(obj))
-	if err != nil {
-		return fmt.Errorf("validate failed: %s", err)
-	}
-
-	if !ret.Valid() {
-		errString := buffer.Buffer{}
-		for i, vErr := range ret.Errors() {
-			if i != 0 {
-				errString.AppendString("\n")
-			}
-			errString.AppendString(vErr.String())
-		}
-		return errors.New(errString.String())
-	}
-	return nil
 }
 
 type APISIXJsonSchemaValidator struct {
--- api/internal/core/store/validate_test.go
+++ api/internal/core/store/validate_test.go
@@ -26,50 +26,6 @@ import (
 	"github.com/apache/apisix-dashboard/api/internal/core/entity"
 )
 
-type TestObj struct {
-	Name  string `json:"name"`
-	Email string `json:"email"`
-	Age   int    `json:"age"`
-}
-
-func TestJsonSchemaValidator_Validate(t *testing.T) {
-	tests := []struct {
-		givePath        string
-		giveObj         interface{}
-		wantNewErr      error
-		wantValidateErr []error
-	}{
-		{
-			givePath: "./test_case.json",
-			giveObj: TestObj{
-				Name:  "lessName",
-				Email: "too long name greater than 10",
-				Age:   12,
-			},
-			wantValidateErr: []error{
-				fmt.Errorf("name: String length must be greater than or equal to 10\nemail: String length must be less than or equal to 10"),
-				fmt.Errorf("email: String length must be less than or equal to 10\nname: String length must be greater than or equal to 10"),
-			},
-		},
-	}
-
-	for _, tc := range tests {
-		v, err := NewJsonSchemaValidator(tc.givePath)
-		if err != nil {
-			assert.Equal(t, tc.wantNewErr, err)
-			continue
-		}
-		err = v.Validate(tc.giveObj)
-		ret := false
-		for _, wantErr := range tc.wantValidateErr {
-			if wantErr.Error() == err.Error() {
-				ret = true
-			}
-		}
-		assert.True(t, ret)
-	}
-}
-
 func TestAPISIXJsonSchemaValidator_Validate(t *testing.T) {
 	validator, err := NewAPISIXJsonSchemaValidator("main.consumer")
 	assert.Nil(t, err)
