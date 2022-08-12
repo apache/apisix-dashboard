bd39c1d5115ed311878696bfbb11c655fb8035bf
SkyeYoung <iskyex@outlook.com>
2022-08-12 08:04:29 +0000

    fix: workflow, eslint

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 module.exports = {
-  extends: [require.resolve('@umijs/fabric/dist/eslint'), 'cypress.config.ts'],
+  extends: [require.resolve('@umijs/fabric/dist/eslint')],
   globals: {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
     page: true,
