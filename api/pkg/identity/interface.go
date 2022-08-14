4a3fd43d3f64d8bd04e3c86154387ee7f75e8f33
jackj-ohn1 <2105753640@qq.com>
2022-08-14 16:27:17 +0800

    feat:add a Non-invasive way to control use power

---
+++ api/pkg/identity/interface.go
@@ -0,0 +1,17 @@
+package identity
+
+type Identifier interface {
+	Check(userId, resource, action string) error
+}
+
+type ReplaceIdentifier struct {
+	self *Identifier
+}
+
+func (dr *ReplaceIdentifier) ReplaceIdentifier(i Identifier) {
+	dr.self = &i
+}
+
+func (dr ReplaceIdentifier) GetIdentifier() Identifier {
+	return *dr.self
+}
