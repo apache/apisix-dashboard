9ce3ddc7989808c7d0aa2ddef697c57448c89d89
白泽平 <bzp2010@apache.org>
2022-08-16 17:25:58 +0800

    feat: add IAM filter

---
+++ api/internal/filter/iam/iam.go
@@ -0,0 +1,33 @@
+package iam
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	iamDef "github.com/apache/apisix-dashboard/api/pkg/iam"
+)
+
+var (
+	access iamDef.Access
+)
+
+func IAM() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if access != nil {
+			identity := c.MustGet("identity").(string)
+			err := access.Check(identity, c.Request.URL.Path, c.Request.Method)
+			if err != nil {
+				c.AbortWithStatus(http.StatusForbidden)
+				return
+			}
+		}
+
+		c.Next()
+	}
+}
+
+// SetAccessImplementation provides a function that allows developers to replace the built-in access control implementation
+func SetAccessImplementation(impl iamDef.Access) {
+	access = impl
+}
