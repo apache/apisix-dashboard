27c2c91de439ffb1d50c65b55007bb8c55985a70
白泽平 <bzp2010@apache.org>
2022-08-16 17:23:44 +0800

    chore: clean unused codes

--- api/pkg/iam/interface.go
+++ api/pkg/iam/interface.go
@@ -1,17 +1,6 @@
 package iam
 
-type Identifier interface {
-	Check(userId, resource, action string) error
-}
-
-type ReplaceIdentifier struct {
-	self *Identifier
-}
-
-func (dr *ReplaceIdentifier) ReplaceIdentifier(i Identifier) {
-	dr.self = &i
-}
-
-func (dr ReplaceIdentifier) GetIdentifier() Identifier {
-	return *dr.self
+// Access interface defines the pattern of functions required for access control in the IAM
+type Access interface {
+	Check(identity, resource, action string) error
 }
