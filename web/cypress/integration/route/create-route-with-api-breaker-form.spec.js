fe842f5c791a0b6cf48d37aff09c1ecc1727d0b8
房森 <72308243+FangSen9000@users.noreply.github.com>
2022-08-17 23:39:43 +0800

    Update create-route-with-api-breaker-form.spec.js

--- web/cypress/integration/route/create-route-with-api-breaker-form.spec.js
+++ web/cypress/integration/route/create-route-with-api-breaker-form.spec.js
@@ -49,6 +49,7 @@ context('Create and delete route with api-breaker form', () => {
   it('should create route with api-breaker form', function () {
     cy.visit('/');
     cy.contains('Route').click();
+    cy.wait(2000);
     cy.get(selector.empty).should('be.visible');
     cy.contains('Create').click();
     cy.contains('Next').click().click();
@@ -110,6 +111,7 @@ context('Create and delete route with api-breaker form', () => {
 
   it('should delete the route', function () {
     cy.visit('/routes/list');
+    cy.wait(2000);
 
     cy.get(selector.name).clear().type('routeName');
     cy.contains('Search').click();
