a05094258da66847e9a81836345236cffde932cd
房森 <72308243+FangSen9000@users.noreply.github.com>
2022-08-17 23:17:14 +0800

    Update create-consumer-with-api-breaker-plugin-form.spec.js

--- web/cypress/integration/consumer/create-consumer-with-api-breaker-plugin-form.spec.js
+++ web/cypress/integration/consumer/create-consumer-with-api-breaker-plugin-form.spec.js
@@ -50,6 +50,7 @@ context('Create and delete consumer with api-breaker plugin form', () => {
   it('creates consumer with api-breaker form', function () {
     cy.visit('/');
     cy.contains('Consumer').click();
+    cy.wait(3000);
     cy.get(selector.empty).should('be.visible');
     cy.contains('Create').click();
     // basic information
@@ -110,6 +111,7 @@ context('Create and delete consumer with api-breaker plugin form', () => {
 
   it('delete the consumer', function () {
     cy.visit('/consumer/list');
+    cy.wait(3000);
     cy.contains(data.consumerName).should('be.visible').siblings().contains('Delete').click();
     cy.contains('button', 'Confirm').click();
     cy.get(selector.notification).should('contain', data.deleteConsumerSuccess);
