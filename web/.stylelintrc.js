cb0a45ff40dd2a16f597e46f47503b57055b0b4c
房森 <72308243+FangSen9000@users.noreply.github.com>
2022-08-24 21:42:42 +0800

    Update .stylelintrc.js

--- web/.stylelintrc.js
+++ web/.stylelintrc.js
@@ -16,7 +16,6 @@
  */
 module.exports = {
   extends: [
-    'stylelint-config-standard',
     'stylelint-config-css-modules',
     'stylelint-config-rational-order',
     'stylelint-config-prettier',
