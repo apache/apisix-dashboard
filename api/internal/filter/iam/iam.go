77eccb1a763b6cb88d4d12ff29ee5b827c1c4429
jackj-ohn1 <2105753640@qq.com>
2022-08-25 15:35:21 +0800

    chore:add constant method instead of method of string

--- api/internal/filter/iam/iam.go
+++ api/internal/filter/iam/iam.go
@@ -1,10 +1,27 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package iam
 
 import (
 	"net/http"
-
+	
 	"github.com/gin-gonic/gin"
-
+	
 	"github.com/apache/apisix-dashboard/api/internal/config"
 	iamDef "github.com/apache/apisix-dashboard/api/pkg/iam"
 	"github.com/apache/apisix-dashboard/api/pkg/iam/demo"
@@ -22,7 +39,7 @@ func Filter(cfg config.Config) gin.HandlerFunc {
 		access = demo.Access{}
 		accessLock = true
 	}
-
+	
 	return func(c *gin.Context) {
 		if access != nil && c.Request.URL.Path != "/apisix/admin/user/login" {
 			identity := c.MustGet("identity").(string)
@@ -32,7 +49,7 @@ func Filter(cfg config.Config) gin.HandlerFunc {
 				return
 			}
 		}
-
+		
 		c.Next()
 	}
 }
--- api/internal/filter/iam/iam_test.go
+++ api/internal/filter/iam/iam_test.go
@@ -1,8 +1,26 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package iam
 
 import (
 	"errors"
 	"github.com/apache/apisix-dashboard/api/internal/config"
+	"github.com/apache/apisix-dashboard/api/pkg/iam"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"net/http"
@@ -30,24 +48,26 @@ func TestFilter(t *testing.T) {
 	r.DELETE("/apisix/admin/stream_routes/:ids", func(ctx *gin.Context) {})
 	r.GET("/*path", func(ctx *gin.Context) {})
 	r.POST("/success", func(ctx *gin.Context) {})
-
-	w := performRequest(r, "POST", "/apisix/admin/user/login")
+	
+	w := performRequest(r, http.MethodPost, "/apisix/admin/user/login")
 	assert.Equal(t, http.StatusOK, w.Code)
-
-	w = performRequest(r, "PUT", "/apisix/admin/global_rules/12")
+	
+	w = performRequest(r, http.MethodDelete, "/apisix/admin/global_rules/12")
 	assert.Equal(t, http.StatusForbidden, w.Code)
-
-	w = performRequest(r, "DELETE", "/apisix/admin/stream_routes/67")
+	
+	w = performRequest(r, http.MethodDelete, "/apisix/admin/stream_routes/67")
 	assert.Equal(t, http.StatusForbidden, w.Code)
-
-	w = performRequest(r, "GET", "/apisix/admin/ssl/98")
+	
+	w = performRequest(r, http.MethodGet, "/apisix/admin/ssl/98")
 	assert.Equal(t, http.StatusOK, w.Code)
-
-	w = performRequest(r, "POST", "/success")
+	
+	w = performRequest(r, http.MethodPost, "/success")
 	assert.Equal(t, http.StatusOK, w.Code)
 }
 
 type test struct{}
+
+var _ iam.Access = test{}
 
 func (test) Check(identity, resource, action string) error {
 	return errors.New("no permission")
@@ -67,13 +87,13 @@ func TestSetAccessImplementation(t *testing.T) {
 	r.POST("/apisix/admin/user/login", func(ctx *gin.Context) {})
 	r.PUT("/apisix/admin/route/:id", func(ctx *gin.Context) {})
 	r.DELETE("/apisix/admin/upstream", func(ctx *gin.Context) {})
-
-	w := performRequest(r, "POST", "/apisix/admin/user/login")
+	
+	w := performRequest(r, http.MethodPost, "/apisix/admin/user/login")
 	assert.Equal(t, http.StatusOK, w.Code)
-
-	w = performRequest(r, "PUT", "/apisix/admin/route/2")
+	
+	w = performRequest(r, http.MethodPut, "/apisix/admin/route/2")
 	assert.Equal(t, http.StatusForbidden, w.Code)
-
-	w = performRequest(r, "DELETE", "/apisix/admin/upstream")
+	
+	w = performRequest(r, http.MethodDelete, "/apisix/admin/upstream")
 	assert.Equal(t, http.StatusForbidden, w.Code)
 }
