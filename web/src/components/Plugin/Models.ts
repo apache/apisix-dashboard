dea9f69809540848bd0893cad356a42382972585
房森 <72308243+FangSen9000@users.noreply.github.com>
2022-08-29 16:08:59 +0800

    Update Models.ts

--- web/src/components/Plugin/Models.ts
+++ web/src/components/Plugin/Models.ts
@@ -14,14 +14,40 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+ 
+/**
+ * Model List of PluginType of Monaco editor
+ */
+ 
 import { Uri, editor } from "monaco-editor";
-import*as modelCode from './modelCode';
+import*as modelCode from './modelCode'
 
+/**
+* Model type is authentication as fllows:
+*/
 export const authzcasbinModel = editor.createModel(
     modelCode.authzcasbin,
     "json",
     Uri.parse("file:authz-casbin")
   );
+
+export const authzkeycloakModel = editor.createModel(
+  modelCode.authzkeycloak,
+  "json",
+  Uri.parse("file:authz-keycloak")
+);
+
+export const forwardauthModel = editor.createModel(
+  modelCode.forwardauth,
+  "json",
+  Uri.parse("file:forward-auth")
+);
+
+export const hmacauthModel = editor.createModel(
+  modelCode.hmacauth,
+  "json",
+  Uri.parse("file:hmac-auth")
+);
 
 export const opaModel = editor.createModel(
   modelCode.opa,
@@ -29,8 +55,173 @@ export const opaModel = editor.createModel(
   Uri.parse("file:opa")
 );
 
-export const authzkeycloakModel = editor.createModel(
-  modelCode.authzkeycloak,
+export const openidconnectModel = editor.createModel(
+  modelCode.openidconnect,
   "json",
-  Uri.parse("file:authz-keycloak")
+  Uri.parse("file:openid-connect")
 );
+
+/**
+* Model type is security as fllows:
+*/
+
+export const csrfModel = editor.createModel(
+  modelCode.csrf,
+  "json",
+  Uri.parse("file:csrf")
+);
+
+export const iprestrictionModel = editor.createModel(
+  modelCode.iprestriction,
+  "json",
+  Uri.parse("file:ip-restriction")
+);
+
+export const uarestrictionModel = editor.createModel(
+  modelCode.uarestriction,
+  "json",
+  Uri.parse("file:ua-restriction")
+);
+
+export const uriblockerModel = editor.createModel(
+  modelCode.uriblocker,
+  "json",
+  Uri.parse("file:uri-blocker")
+);
+
+/**
+* Model type is traffic as fllows:
+*/
+
+export const clientcontrolModel = editor.createModel(
+  modelCode.clientcontrol,
+  "json",
+  Uri.parse("file:client-control")
+);
+
+export const trafficsplitModel = editor.createModel(
+  modelCode.trafficsplit,
+  "json",
+  Uri.parse("file:traffic-split")
+);
+
+/**
+* Model type is serverless as fllows:
+*/
+
+export const awslambdaModel = editor.createModel(
+  modelCode.awslambda,
+  "json",
+  Uri.parse("file:aws-lambda")
+);
+
+export const azurefunctionsModel = editor.createModel(
+  modelCode.azurefunctions,
+  "json",
+  Uri.parse("file:azure-functions")
+);
+
+export const openwhiskModel = editor.createModel(
+  modelCode.openwhisk,
+  "json",
+  Uri.parse("file:openwhisk")
+);
+
+/**
+* Model type is observability as fllows:
+*/
+
+export const clickhouseloggerModel = editor.createModel(
+  modelCode.clickhouselogger,
+  "json",
+  Uri.parse("file:clickhouse-logger")
+);
+
+export const fileloggerModel = editor.createModel(
+  modelCode.filelogger,
+  "json",
+  Uri.parse("file:file-logger")
+);
+
+export const googlecloudloggingModel = editor.createModel(
+  modelCode.googlecloudlogging,
+  "json",
+  Uri.parse("file:google-cloud-logging")
+);
+
+export const httploggerModel = editor.createModel(
+  modelCode.httplogger,
+  "json",
+  Uri.parse("file:http-logger")
+);
+
+export const kafkaloggerModel = editor.createModel(
+  modelCode.kafkalogger,
+  "json",
+  Uri.parse("file:kafka-logger")
+);
+
+export const logglyModel = editor.createModel(
+  modelCode.loggly,
+  "json",
+  Uri.parse("file:loggly")
+);
+
+export const rocketmqloggerModel = editor.createModel(
+  modelCode.rocketmqlogger,
+  "json",
+  Uri.parse("file:rocketmq-logger")
+);
+
+export const skywalkingModel = editor.createModel(
+  modelCode.skywalking,
+  "json",
+  Uri.parse("file:sky-walking")
+);
+
+export const slsloggerModel = editor.createModel(
+  modelCode.slslogger,
+  "json",
+  Uri.parse("file:sls-logger")
+);
+
+export const splunkhecloggingModel = editor.createModel(
+  modelCode.splunkheclogging,
+  "json",
+  Uri.parse("file:splunk-hec-logging")
+);
+
+export const syslogModel = editor.createModel(
+  modelCode.syslog,
+  "json",
+  Uri.parse("file:syslog")
+);
+
+export const tcploggerModel = editor.createModel(
+  modelCode.tcplogger,
+  "json",
+  Uri.parse("file:tcp-logger")
+);
+
+export const zipkinModel = editor.createModel(
+  modelCode.zipkin,
+  "json",
+  Uri.parse("file:zipkin")
+);
+
+/**
+* Model type is other as fllows:
+*/
+
+export const extpluginprereqModel = editor.createModel(
+  modelCode.extpluginprereq,
+  "json",
+  Uri.parse("file:ext-plugin-pre-req")
+);
+
+export const realipModel = editor.createModel(
+  modelCode.realip,
+  "json",
+  Uri.parse("file:real-ip")
+);
+
