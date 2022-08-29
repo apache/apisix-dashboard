0246d88e643c287a39557de3e31ce628b85102c8
房森 <72308243+FangSen9000@users.noreply.github.com>
2022-08-29 16:09:48 +0800

    Update modelCode.ts

--- web/src/components/Plugin/modelCode.ts
+++ web/src/components/Plugin/modelCode.ts
@@ -14,6 +14,49 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/**
+ * Model Code List of Models of Monaco editor
+ */
+
+/**
+* Model code of authentication type as fllows:
+*/
+
+export const authzcasbin = `
+{
+    "model_path":
+    "policy_path":
+    "username":
+}
+`;
+
+export const authzkeycloak =`
+{
+    "token_endpoint":
+    "permissions":
+    "audience":
+}
+`;
+
+export const forwardauth =`
+{
+    "uri":
+    "request_headers":
+    "upstream_headers":
+    "client_headers":
+}
+`;
+
+export const hmacauth =`
+{
+    "access_key":
+    "secret_key":
+    "clock_skew":
+    "signed_headers":
+}
+`;
+
 export const opa = `
 {
     "type":
@@ -43,18 +86,273 @@ export const opa = `
 }
 `;
 
-export const authzcasbin = `
+export const openidconnect =`
 {
-  "model_path":
-  "policy_path":
-  "username":
+    "client_id":
+    "client_secret":
+    "discovery":
+    "introspection_endpoint":
+    "bearer_only": true,
+    "realm":
+    "introspection_endpoint_auth_method":
 }
 `;
 
-export const authzkeycloak =`
+/**
+* Model code of security type as fllows:
+*/
+
+export const csrf =`
+{
+    "key":
+}
+`;
+
+export const iprestriction =`
+{
+    "whitelist":
+}
+`;
+
+export const uarestriction =`
+{
+    "bypass_missing":
+    "allowlist":
+    "denylist":
+}
+`;
+
+export const uriblocker =`
+{
+    "block_rules":
+}
+`;
+
+/**
+* Model code of traffic type as fllows:
+*/
+
+export const clientcontrol =`
+{
+    "max_body_size":
+}
+`;
+
+export const trafficsplit =`
+{
+    "rules": [
+        {
+            "weighted_upstreams": [
+                {
+                    "upstream": {
+                        "name": "upstream_A",
+                        "type": "roundrobin",
+                        "nodes": {
+                            "127.0.0.1:1981":10
+                        },
+                        "timeout": {
+                            "connect": 15,
+                            "send": 15,
+                            "read": 15
+                        }
+                    },
+                    "weight": 1
+                },
+                {
+                    "weight": 1
+                }
+            ]
+        }
+    ]
+}
+`;
+ 
+/**
+* Model code of serverless type as fllows:
+*/
+ 
+export const awslambda =`
 {
   "token_endpoint":
   "permissions":
   "audience":
+}
+`;
+
+export const azurefunctions =`
+{
+    "azure-functions": {
+        "function_uri":
+        "authorization": {
+            "apikey":
+        }
+    }
+}
+`;
+
+export const openwhisk =`
+{
+    "api_host":
+    "service_token":
+    "namespace":
+    "action":
+}
+`;
+
+/**
+* Model code of observability type as fllows:
+*/
+ 
+export const clickhouselogger =`
+{
+    "user":
+    "password":
+    "database":
+    "logtable":
+    "endpoint_addr":
+}
+`;
+
+export const filelogger =`
+{
+    "path":
+}
+`;
+
+export const googlecloudlogging =`
+{
+    "auth_config":{
+        "project_id":
+        "private_key":
+        "token_uri":
+        "scopes":
+        "entries_uri":
+    },
+    "resource":{
+        "type":
+    },
+    "log_id":
+    "inactive_timeout":
+    "max_retry_count":
+    "buffer_duration":
+    "retry_delay":
+    "batch_max_size":
+}
+`;
+
+export const httplogger =`
+{
+    "uri":
+}
+`;
+
+export const kafkalogger =`
+{
+    "broker_list" :
+    "kafka_topic" :
+    "key" :
+    "batch_max_size":
+    "name":
+}
+`;
+
+export const loggly =`
+{
+    "nameserver_list":
+    "topic":
+    "batch_max_size":
+    "name":
+}
+`;
+
+export const rocketmqlogger =`
+{
+  "token_endpoint":
+  "permissions":
+  "audience":
+}
+`;
+
+export const skywalking =`
+{
+    "sample_ratio":
+}
+`;
+
+export const skywalkinglogger =`
+{
+    "endpoint_addr":
+}
+`;
+
+export const slslogger =`
+{
+    "host":
+    "port":
+    "project":
+    "logstore":
+    "access_key_id":
+    "access_key_secret":
+    "timeout":
+}
+`;
+
+export const splunkheclogging =`
+{
+    "endpoint":{
+        "uri":
+        "token":
+        "channel":
+        "timeout":
+    },
+    "buffer_duration":
+    "max_retry_count":
+    "retry_delay":
+    "inactive_timeout":
+    "batch_max_size":
+}
+`;
+
+export const syslog =`
+{
+    "host":
+    "port":
+    "flush_limit":
+}
+`;
+
+export const tcplogger =`
+{
+    "host":
+    "port":
+    "tls":
+    "batch_max_size":
+    "name":
+}
+`;
+
+export const zipkin =`
+{
+    "endpoint":
+    "sample_ratio":
+    "service_name":
+    "server_addr":
+}
+`;
+
+/**
+* Model code of other type as fllows:
+*/
+
+export const extpluginprereq =`
+{
+    "conf":
+}
+`;
+
+export const realip =`
+{
+    "source":
+    "trusted_addresses":
 }
 `;
