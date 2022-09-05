ba1c0db64a7ecfcf1a7b0c55189927053ae11ad5
房森 <72308243+FangSen9000@users.noreply.github.com>
2022-09-05 19:05:09 +0800

    Update create-delete-in-drawer-plugin.cy.js

--- web/cypress/e2e/plugin/create-delete-in-drawer-plugin.cy.js
+++ web/cypress/e2e/plugin/create-delete-in-drawer-plugin.cy.js
@@ -292,12 +292,12 @@ context('Delete Plugin List with the Drawer', () => {
       cy.contains(item)
         .parents(selector.pluginCardBordered)
         .within(() => {
-          cy.get('button').click();
+          cy.get('button', 'Enable').click();
         });
       cy.get(selector.drawer)
         .should('be.visible')
         .within(() => {
-          cy.get(selector.checkedSwitcher).should('exist');
+          cy.get(selector.disabledSwitcher).should('exist');
         });
       cy.wait(timeout);
       cy.contains('button', 'Delete').click({ force: true });
