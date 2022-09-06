39e25f6803c3a51743a5f788bd1155cdd2fd7f32
房森 <72308243+FangSen9000@users.noreply.github.com>
2022-09-06 19:56:24 +0800

    Update service-edit-service-with-upstream.cy.js

--- web/cypress/e2e/rest/service-edit-service-with-upstream.cy.js
+++ web/cypress/e2e/rest/service-edit-service-with-upstream.cy.js
@@ -96,7 +96,10 @@ context('Edit Service with Upstream', () => {
 
     cy.wait(500);
     cy.get(selector.upstreamSelector).click();
-    cy.contains('.ant-select-item-option-content', 'Custom').click({force: true});
+    cy.contains('.ant-select-item-option-content', 'Custom')
+      .click({
+        force: true,
+      })
     cy.get(selector.nodes_0_host).should('not.be.disabled').clear().type(data.ip2);
     cy.get(selector.nodes_0_port).type(data.port);
     cy.get(selector.nodes_0_weight).type(data.weight);
