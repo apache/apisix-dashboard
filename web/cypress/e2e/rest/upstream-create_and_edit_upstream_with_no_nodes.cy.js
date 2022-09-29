63c2819144ed2d105a0556e1e2d0800cabec6878
房森 <72308243+FangSen9000@users.noreply.github.com>
2022-09-29 21:48:49 +0800

    Update upstream-create_and_edit_upstream_with_no_nodes.cy.js

--- web/cypress/e2e/rest/upstream-create_and_edit_upstream_with_no_nodes.cy.js
+++ web/cypress/e2e/rest/upstream-create_and_edit_upstream_with_no_nodes.cy.js
@@ -16,20 +16,32 @@
  */
 /* eslint-disable no-undef */
 
-context('Create and Delete Upstream', () => {
+context('Edit Service with Upstream', () => {
   const selector = {
+    empty: '.ant-empty-normal',
     name: '#name',
     description: '#desc',
-    upstreamNodeMinus0: '[data-cy=upstream-node-minus-0]',
+    nodes_0_host: '#submitNodes_0_host',
+    nodes_0_port: '#submitNodes_0_port',
+    nodes_0_weight: '#submitNodes_0_weight',
     notification: '.ant-notification-notice-message',
-    nameSelector: '[title=Name]',
+    upstreamSelector: '[data-cy=upstream_selector]',
+    input: ':input',
+    nameSearch: '[title=Name]',
   };
 
   const data = {
     upstreamName: 'test_upstream',
     description: 'desc_by_autotest',
+    ip1: '127.0.0.1',
     createUpstreamSuccess: 'Create Upstream Successfully',
-    configureUpstreamSuccess: 'Configure Upstream Successfully',
+    serviceName: 'test_service',
+    createServiceSuccess: 'Create Service Successfully',
+    ip2: '127.0.0.2',
+    port: '80',
+    weight: 1,
+    editServiceSuccess: 'Configure Service Successfully',
+    deleteServiceSuccess: 'Delete Service Successfully',
     deleteUpstreamSuccess: 'Delete Upstream Successfully',
   };
 
@@ -37,42 +49,73 @@ context('Create and Delete Upstream', () => {
     cy.login();
   });
 
-  it('should create upstream with no nodes', function () {
-    cy.visit('/');
-    cy.contains('Upstream').click();
+  it('should create a test upstream', function () {
+    cy.visit('/upstream/list');
+    cy.get(selector.empty).should('be.visible');
     cy.contains('Create').click();
-
     cy.get(selector.name).type(data.upstreamName);
-    cy.get(selector.description).type(data.description);
-
-    // delete all nodes
-    cy.get(selector.upstreamNodeMinus0).click();
-
+    cy.get(selector.nodes_0_host).type(data.ip1);
+    cy.get(selector.nodes_0_port).clear().type('7000');
+    cy.get(selector.nodes_0_weight).clear().type(1);
     cy.contains('Next').click();
     cy.contains('Submit').click();
     cy.get(selector.notification).should('contain', data.createUpstreamSuccess);
     cy.url().should('contains', 'upstream/list');
   });
 
-  it('should configure the upstream with no nodes', function () {
+  it('should create a test service', function () {
     cy.visit('/');
-    cy.contains('Upstream').click();
+    cy.get('.ant-empty').should('be.visible');
+    cy.contains('Service').click();
+    cy.get(selector.empty).should('be.visible');
+    cy.contains('Create').click();
+    cy.get(selector.name).type(data.serviceName);
+    cy.get(selector.description).type(data.description);
+    cy.get(selector.upstreamSelector).click();
+    cy.contains(data.upstreamName).click();
+    cy.get(selector.input).should('be.disabled');
 
-    cy.get(selector.nameSelector).type(data.upstreamName);
-    cy.contains('Search').click();
-    cy.contains(data.upstreamName).siblings().contains('Configure').click();
-
-    cy.get(selector.upstreamNodeMinus0).should('not.exist');
-    cy.contains('button', 'Next').should('not.be.disabled').click();
-    cy.contains('Submit').click({
-      force: true,
-    });
-
-    cy.get(selector.notification).should('contain', data.configureUpstreamSuccess);
-    cy.url().should('contains', 'upstream/list');
+    cy.contains('Next').click();
+    cy.contains('Submit').click();
+    cy.get(selector.notification).should('contain', data.createServiceSuccess);
   });
 
-  it('should delete the upstream', function () {
+  it('should edit the service', function () {
+    cy.visit('/service/list');
+
+    cy.get(selector.nameSearch).type(data.serviceName);
+    cy.contains('Search').click();
+    cy.contains(data.serviceName).siblings().contains('Configure').click();
+
+    cy.contains(data.upstreamName).click();
+    cy.wait(500);
+    cy.contains('.ant-select-item-option-content','Custom').click();
+    cy.get(selector.nodes_0_host)
+      .click({
+        force: true,
+      })
+      .should('value', data.ip1);
+    cy.get(selector.input).should('be.disabled');
+
+    cy.wait(500);
+    cy.get(selector.upstreamSelector).click();
+    cy.get(selector.nodes_0_host).should('not.be.disabled').clear().type(data.ip2);
+    cy.get(selector.nodes_0_port).type(data.port);
+    cy.get(selector.nodes_0_weight).type(data.weight);
+    cy.contains('Next').click();
+    cy.contains('Next').click();
+    cy.contains('Submit').click();
+    cy.get(selector.notification).should('contain', data.editServiceSuccess);
+  });
+
+  it('should delete this service and upstream', function () {
+    cy.visit('/service/list');
+    cy.get(selector.nameSearch).type(data.serviceName);
+    cy.contains('Search').click();
+    cy.contains(data.serviceName).siblings().contains('Delete').click();
+    cy.contains('button', 'Confirm').click();
+    cy.get(selector.notification).should('contain', data.deleteServiceSuccess);
+
     cy.visit('/');
     cy.contains('Upstream').click();
     cy.contains(data.upstreamName).siblings().contains('Delete').click();
