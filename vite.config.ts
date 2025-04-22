e8fe462e5e584bfaf3e5e8813e9288296fe412af
Skye Young <iskyex@outlook.com>
2025-04-22 18:36:37 +0800

    chore: replace @vitejs/plugin-react with @vitejs/plugin-react-swc

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import postcssPresetMantine from 'postcss-preset-mantine';
