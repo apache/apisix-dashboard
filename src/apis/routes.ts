8d9a67e7fcf559195671342b3d4245687480ba9f
Skye Young <iskyex@outlook.com>
2025-04-30 15:20:44 +0800

    fix: type

--- src/apis/routes.ts
+++ src/apis/routes.ts
@@ -1,7 +1,7 @@
 import type { RoutePostType } from '@/components/form-slice/FormPartRoute/schema';
 import { API_ROUTES } from '@/config/constant';
 import { req } from '@/config/req';
-import type { A6Type } from '@/types/schema/apisix';
+import type { APISIXType } from '@/types/schema/apisix';
 import type { PageSearchType } from '@/types/schema/pageSearch';
 import { queryOptions } from '@tanstack/react-query';
 
@@ -11,7 +11,7 @@ export const getRouteListQueryOptions = (props: PageSearchType) => {
     queryKey: ['routes', page, pageSize],
     queryFn: () =>
       req
-        .get<unknown, A6Type['RespRouteList']>(API_ROUTES, {
+        .get<unknown, APISIXType['RespRouteList']>(API_ROUTES, {
           params: { page, page_size: pageSize },
         })
         .then((v) => v.data),
@@ -19,4 +19,4 @@ export const getRouteListQueryOptions = (props: PageSearchType) => {
 };
 
 export const postRouteReq = (data: RoutePostType) =>
-  req.post<unknown, A6Type['RespRouteDetail']>(API_ROUTES, data);
+  req.post<unknown, APISIXType['RespRouteDetail']>(API_ROUTES, data);
