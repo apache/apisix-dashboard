ffdb18e62475de911a58dda529722aa2f5b65e9b
Skye Young <iskyex@outlook.com>
2025-05-06 10:48:06 +0800

    feat: consumer schema

--- src/types/schema/apisix/index.ts
+++ src/types/schema/apisix/index.ts
@@ -6,9 +6,11 @@ import { APISIXRoutes } from './routes';
 import { APISIXStreamRoutes } from './stream_routes';
 import { APISIXUpstreams } from './upstreams';
 import { APISIXPluginMetadata } from './plugin_metadata';
+import { APISIXConsumers } from './consumers';
 export type { APISIXType } from './type';
 export const APISIX = {
   ...APISIXCommon,
+  ...APISIXConsumers,
   ...APISIXUpstreams,
   ...APISIXRoutes,
   ...APISIXStreamRoutes,
