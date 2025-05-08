a094b97e944356c39802681e8c88597976adac62
Skye Young <iskyex@outlook.com>
2025-05-08 18:58:52 +0800

    chore

--- src/config/constant.ts
+++ src/config/constant.ts
@@ -1,7 +1,5 @@
 export const BASE_PATH = '/ui';
 export const API_HEADER_KEY = 'X-API-KEY';
-export const LOCAL_STORAGE_ADMIN_KEY = 'apisix-admin-key';
-
 export const API_PREFIX = '/apisix/admin';
 export const API_ROUTES = '/routes';
 export const API_STREAM_ROUTES = '/stream_routes';
