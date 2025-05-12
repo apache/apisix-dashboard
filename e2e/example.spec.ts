f4733b68640520c6287726dd0dc68fbe2244c941
Skye Young <iskyex@outlook.com>
2025-05-12 23:13:15 +0800

    feat: e2e config

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -14,17 +14,23 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {
-  Button,
-  type ButtonProps,
-  type PolymorphicComponentProps,
-} from '@mantine/core';
-import { useFormContext, useFormState } from 'react-hook-form';
+import { expect, test } from '@playwright/test';
 
-export const FormSubmitBtn = (
-  props: PolymorphicComponentProps<'button', ButtonProps>
-) => {
-  const form = useFormContext();
-  const { isSubmitting } = useFormState(form);
-  return <Button type="submit" loading={isSubmitting} {...props} />;
-};
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(
+    page.getByRole('heading', { name: 'Installation' })
+  ).toBeVisible();
+});
