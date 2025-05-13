aa9f48bf872f2e2433923bc94f28b714da573479
Skye Young <iskyex@outlook.com>
2025-05-13 10:46:44 +0800

    fix:

--- playwright.config.ts
+++ playwright.config.ts
@@ -65,16 +65,6 @@ export default defineConfig({
     //   use: { ...devices['Desktop Safari'] },
     // },
 
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
     /* Test against branded browsers. */
     // {
     //   name: 'Microsoft Edge',
