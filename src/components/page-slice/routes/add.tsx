fcf58f7898a100b70efb201a703a9b2adc75e3d5
Skye Young <iskyex@outlook.com>
2025-06-05 11:38:36 +0800

    refactor: mv routes' components to page-slice

---
+++ src/components/page-slice/routes/add.tsx
@@ -0,0 +1,83 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { zodResolver } from '@hookform/resolvers/zod';
+import { notifications } from '@mantine/notifications';
+import { useMutation } from '@tanstack/react-query';
+import { useRouter } from '@tanstack/react-router';
+import { FormProvider, useForm } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+
+import { postRouteReq } from '@/apis/routes';
+import { FormSubmitBtn } from '@/components/form/Btn';
+import { FormPartRoute } from '@/components/form-slice/FormPartRoute';
+import {
+  RoutePostSchema,
+  type RoutePostType,
+} from '@/components/form-slice/FormPartRoute/schema';
+import { FormTOCBox } from '@/components/form-slice/FormSection';
+import PageHeader from '@/components/page/PageHeader';
+import { req } from '@/config/req';
+import { produceRmUpstreamWhenHas } from '@/utils/form-producer';
+import { pipeProduce } from '@/utils/producer';
+
+const RouteAddForm = () => {
+  const { t } = useTranslation();
+  const router = useRouter();
+
+  const postRoute = useMutation({
+    mutationFn: (d: RoutePostType) =>
+      postRouteReq(req, pipeProduce(produceRmUpstreamWhenHas('service_id'))(d)),
+    async onSuccess(res) {
+      notifications.show({
+        message: t('info.add.success', { name: t('routes.singular') }),
+        color: 'green',
+      });
+      await router.navigate({
+        to: '/routes/detail/$id',
+        params: { id: res.data.value.id },
+      });
+    },
+  });
+
+  const form = useForm({
+    resolver: zodResolver(RoutePostSchema),
+    shouldUnregister: true,
+    shouldFocusError: true,
+    mode: 'all',
+  });
+
+  return (
+    <FormProvider {...form}>
+      <form onSubmit={form.handleSubmit((d) => postRoute.mutateAsync(d))}>
+        <FormPartRoute />
+        <FormSubmitBtn>{t('form.btn.add')}</FormSubmitBtn>
+      </form>
+    </FormProvider>
+  );
+};
+
+export const RouteAdd = () => {
+  const { t } = useTranslation();
+  return (
+    <>
+      <PageHeader title={t('info.add.title', { name: t('routes.singular') })} />
+      <FormTOCBox>
+        <RouteAddForm />
+      </FormTOCBox>
+    </>
+  );
+};
--- src/routes/routes/index.tsx
+++ src/routes/routes/index.tsx
@@ -14,120 +14,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import type { ProColumns } from '@ant-design/pro-components';
-import { ProTable } from '@ant-design/pro-components';
 import { createFileRoute } from '@tanstack/react-router';
-import { useMemo } from 'react';
-import { useTranslation } from 'react-i18next';
 
-import { getRouteListQueryOptions, useRouteList } from '@/apis/hooks';
-import { DeleteResourceBtn } from '@/components/page/DeleteResourceBtn';
-import PageHeader from '@/components/page/PageHeader';
-import { ToAddPageBtn, ToDetailPageBtn } from '@/components/page/ToAddPageBtn';
-import { AntdConfigProvider } from '@/config/antdConfigProvider';
-import { API_ROUTES } from '@/config/constant';
+import { getRouteListQueryOptions } from '@/apis/hooks';
+import { RouteIndex } from '@/components/page-slice/routes/list';
 import { queryClient } from '@/config/global';
-import type { APISIXType } from '@/types/schema/apisix';
 import { pageSearchSchema } from '@/types/schema/pageSearch';
 
-const RouteList = () => {
-  const { data, isLoading, refetch, pagination } = useRouteList();
-  const { t } = useTranslation();
-
-  const columns = useMemo<ProColumns<APISIXType['RespRouteItem']>[]>(() => {
-    return [
-      {
-        dataIndex: ['value', 'id'],
-        title: 'ID',
-        key: 'id',
-        valueType: 'text',
-      },
-      {
-        dataIndex: ['value', 'name'],
-        title: t('form.basic.name'),
-        key: 'name',
-        valueType: 'text',
-      },
-      {
-        dataIndex: ['value', 'desc'],
-        title: t('form.basic.desc'),
-        key: 'desc',
-        valueType: 'text',
-      },
-      {
-        dataIndex: ['value', 'uri'],
-        title: 'URI',
-        key: 'uri',
-        valueType: 'text',
-      },
-      {
-        title: t('table.actions'),
-        valueType: 'option',
-        key: 'option',
-        width: 120,
-        render: (_, record) => [
-          <ToDetailPageBtn
-            key="detail"
-            to="/routes/detail/$id"
-            params={{ id: record.value.id }}
-          />,
-          <DeleteResourceBtn
-            key="delete"
-            name={t('routes.singular')}
-            target={record.value.id}
-            api={`${API_ROUTES}/${record.value.id}`}
-            onSuccess={refetch}
-          />,
-        ],
-      },
-    ];
-  }, [t, refetch]);
-
-  return (
-    <AntdConfigProvider>
-      <ProTable
-        columns={columns}
-        dataSource={data.list}
-        rowKey="id"
-        loading={isLoading}
-        search={false}
-        options={false}
-        pagination={pagination}
-        cardProps={{ bodyStyle: { padding: 0 } }}
-        toolbar={{
-          menu: {
-            type: 'inline',
-            items: [
-              {
-                key: 'add',
-                label: (
-                  <ToAddPageBtn
-                    key="add"
-                    label={t('info.add.title', { name: t('routes.singular') })}
-                    to="/routes/add"
-                  />
-                ),
-              },
-            ],
-          },
-        }}
-      />
-    </AntdConfigProvider>
-  );
-};
-
-function RouteComponent() {
-  const { t } = useTranslation();
-  return (
-    <>
-      <PageHeader title={t('sources.routes')} />
-      <RouteList />
-    </>
-  );
-}
-
 export const Route = createFileRoute('/routes/')({
-  component: RouteComponent,
+  component: RouteIndex,
   validateSearch: pageSearchSchema,
   loaderDeps: ({ search }) => search,
   loader: ({ deps }) =>
