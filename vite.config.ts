992990969ffc8c34b37e29636b75d65541978169
Roland Schaer <schaer@begasoft.ch>
2025-08-15 09:05:51 +0000

    feat(i18n): add support for german

--- vite.config.ts
+++ vite.config.ts
@@ -76,7 +76,7 @@ export default defineConfig({
       semicolons: false,
     }),
     i18nProgress({
-      langs: ['en', 'zh'],
+      langs: ['en', 'de', 'zh'],
       baseLang: 'en',
       getTranslationDir: (lang) => `./src/locales/${lang}`,
     }),
