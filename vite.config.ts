984a872beb0a30292f72ab7c595dc7000036600a
YYYoung <iskyex@outlook.com>
2025-09-02 15:34:19 +0800

    fix(form/Editor): only use the bundled monaco editor (#3169)

--- vite.config.ts
+++ vite.config.ts
@@ -63,6 +63,21 @@ export default defineConfig({
       },
     }),
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'monaco-editor-vendor': ['monaco-editor', '@monaco-editor/react'],
+          'antd-vendor': [
+            'antd',
+            '@ant-design/pro-components',
+            '@ant-design/v5-patch-for-react-19',
+          ],
+        },
+      },
+    },
+  },
   plugins: [
     tsconfigPaths(),
     UnpluginIcons({
